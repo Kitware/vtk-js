915a8a7c8af0f4584d82dce6af5101bff7275863
Ken Martin <ken.martin@kitware.com>
2016-05-12 13:15:27 -0400

    added OpenGLHelper class
    a simple container class

---
+++ Sources/Rendering/OpenGL/Helper/index.js
@@ -0,0 +1,64 @@
+import * as macro from '../../../macro';
+import VertexArrayObject from '../VertexArrayObject';
+import IndexBufferObject from '../IndexBufferObject';
+import ShaderProgram from '../ShaderProgram';
+
+// ----------------------------------------------------------------------------
+// vtkOpenGLHelper methods
+// ----------------------------------------------------------------------------
+
+export function helper(publicAPI, model) {
+  // Set our className
+  model.classHierarchy.push('vtkOpenGLHelper');
+}
+
+// ----------------------------------------------------------------------------
+// Object factory
+// ----------------------------------------------------------------------------
+
+const DEFAULT_VALUES = {
+  program: null,
+  shaderSourceTime: null,
+  VAO: null,
+  attributeUpdateTime: null,
+  IBO: null,
+};
+
+// ----------------------------------------------------------------------------
+
+export function extend(publicAPI, model, initialValues = {}) {
+  Object.assign(model, DEFAULT_VALUES, initialValues);
+
+  // Build VTK API
+  macro.obj(publicAPI, model);
+
+  model.program = ShaderProgram.newInstance();
+  model.VAO = VertexArrayObject.newInstance();
+  model.IBO = IndexBufferObject.newInstance();
+
+  model.shaderSourceTime = {};
+  macro.obj(model.shaderSourceTime);
+
+  model.shaderSourceTime = {};
+  macro.obj(model.shaderSourceTime);
+
+
+  macro.setGet(publicAPI, model, [
+    'program',
+    'shaderSourceTime',
+    'VAO',
+    'attributeUpdateTime',
+    'IBO',
+  ]);
+
+  // Object methods
+  helper(publicAPI, model);
+}
+
+// ----------------------------------------------------------------------------
+
+export const newInstance = macro.newInstance(extend);
+
+// ----------------------------------------------------------------------------
+
+export default { newInstance, extend };
