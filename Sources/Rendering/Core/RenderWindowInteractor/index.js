6e47c959ac60a48b43dc483db0038032b6efd533
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-05-20 12:26:46 -0600

    fix(KeyPress): Fix key press event

--- Sources/Rendering/Core/RenderWindowInteractor/index.js
+++ Sources/Rendering/Core/RenderWindowInteractor/index.js
@@ -118,14 +118,14 @@ function vtkRenderWindowInteractor(publicAPI, model) {
   publicAPI.bindEvents = (canvas, document) => {
     model.canvas = canvas;
     canvas.addEventListener('mousedown', publicAPI.handleMouseDown);
-    canvas.addEventListener('keypress', publicAPI.handleKeyPress);
+    document.querySelector('body').addEventListener('keypress', publicAPI.handleKeyPress);
     canvas.addEventListener('mouseup', publicAPI.handleMouseUp);
     canvas.addEventListener('mousemove', publicAPI.handleMouseMove);
   };
 
   publicAPI.unbindEvents = (canvas, document) => {
     canvas.removeEventListener('mousedown', publicAPI.handleMouseDown);
-    canvas.removeEventListener('keypress', publicAPI.handleKeyPress);
+    document.querySelector('body').removeEventListener('keypress', publicAPI.handleKeyPress);
     canvas.removeEventListener('mouseup', publicAPI.handleMouseUp);
     canvas.removeEventListener('mousemove', publicAPI.handleMouseMove);
   };
