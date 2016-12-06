d5ea3d26769c342fdf3ee388b4ad6f5118c8b01d
Aron Helser <aron.helser@kitware.com>
2016-12-06 17:02:02 -0500

    fix(macro): add testing for get/set macro methods
    Still todo: enum get/set, more array length testing.

--- Sources/tests.js
+++ Sources/tests.js
@@ -3,3 +3,4 @@ import './Filters/Sources/ConeSource/test/testCone';
 import './Rendering/Core/Mapper/test/testEdgeVisibility';
 import './Rendering/OpenGL/PolyDataMapper/test/testInterpolateScalarsBeforeMapping';
 import './Testing/testSerialization';
+import './Testing/testMacro';
