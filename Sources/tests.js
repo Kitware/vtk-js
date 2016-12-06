d982a66507b0ae696119712b307fe773eccaaa4d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-06 13:30:52 -0700

    test(testInterpolateScalarsBeforeMapping): Fix API call with new class hierachy

--- Sources/tests.js
+++ Sources/tests.js
@@ -1,5 +1,5 @@
 import './Filters/General/WarpScalar/test/testWarp';
-// import './Filters/Sources/ConeSource/test/testCone';
-// import './Rendering/Core/Mapper/test/testEdgeVisibility';
-// import './Rendering/OpenGL/PolyDataMapper/test/testInterpolateScalarsBeforeMapping';
+import './Filters/Sources/ConeSource/test/testCone';
+import './Rendering/Core/Mapper/test/testEdgeVisibility';
+import './Rendering/OpenGL/PolyDataMapper/test/testInterpolateScalarsBeforeMapping';
 import './Testing/testSerialization';
