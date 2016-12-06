f06281570cfe87ca057e5079e606b25bee236657
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-06 16:31:32 -0700

    fix(StructuredData): Expose only getDataDescriptionFromExtent() method

--- Sources/Common/DataModel/StructuredData/index.js
+++ Sources/Common/DataModel/StructuredData/index.js
@@ -1,20 +1,8 @@
 import { VTK_STRUCTURED_TYPE } from './Constants';
 
-export function setExtent(minI, maxI, minJ, maxJ, minK, maxK, model) {
-  let dataDescription = VTK_STRUCTURED_TYPE.VTK_EMPTY;
-  const inExt = [minI, maxI, minJ, maxJ, minK, maxK];
-  const ext = [].concat(model.extent);
-
-  if (inExt[0] === ext[0] && inExt[1] === ext[1] &&
-      inExt[2] === ext[2] && inExt[3] === ext[3] &&
-      inExt[4] === ext[4] && inExt[5] === ext[5]) {
-    return VTK_STRUCTURED_TYPE.VTK_UNCHANGED;
-  }
-
+export function getDataDescriptionFromExtent(inExt) {
   let dataDim = 0;
   for (let i = 0; i < 3; ++i) {
-    ext[(i * 2) + 0] = inExt[(i * 2) + 0];
-    ext[(i * 2) + 1] = inExt[(i * 2) + 1];
     if (inExt[i * 2] < inExt[(i * 2) + 1]) {
       dataDim++;
     }
@@ -25,30 +13,26 @@ export function setExtent(minI, maxI, minJ, maxJ, minK, maxK, model) {
   }
 
   if (dataDim === 3) {
-    dataDescription = VTK_STRUCTURED_TYPE.VTK_XYZ_GRID;
+    return VTK_STRUCTURED_TYPE.VTK_XYZ_GRID;
   } else if (dataDim === 2) {
     if (inExt[0] === inExt[1]) {
-      dataDescription = VTK_STRUCTURED_TYPE.VTK_YZ_PLANE;
+      return VTK_STRUCTURED_TYPE.VTK_YZ_PLANE;
     } else if (inExt[2] === inExt[3]) {
-      dataDescription = VTK_STRUCTURED_TYPE.VTK_XZ_PLANE;
-    } else {
-      dataDescription = VTK_STRUCTURED_TYPE.VTK_XY_PLANE;
+      return VTK_STRUCTURED_TYPE.VTK_XZ_PLANE;
     }
+    return VTK_STRUCTURED_TYPE.VTK_XY_PLANE;
   } else if (dataDim === 1) {
     if (inExt[0] < inExt[1]) {
-      dataDescription = VTK_STRUCTURED_TYPE.VTK_X_LINE;
+      return VTK_STRUCTURED_TYPE.VTK_X_LINE;
     } else if (inExt[2] < inExt[3]) {
-      dataDescription = VTK_STRUCTURED_TYPE.VTK_Y_LINE;
-    } else {
-      dataDescription = VTK_STRUCTURED_TYPE.VTK_Z_LINE;
+      return VTK_STRUCTURED_TYPE.VTK_Y_LINE;
     }
-  } else {
-    dataDescription = VTK_STRUCTURED_TYPE.VTK_SINGLE_POINT;
+    return VTK_STRUCTURED_TYPE.VTK_Z_LINE;
   }
 
-  return dataDescription;
+  return VTK_STRUCTURED_TYPE.VTK_SINGLE_POINT;
 }
 
 export default {
-  setExtent,
+  getDataDescriptionFromExtent,
 };
