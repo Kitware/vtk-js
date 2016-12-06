dd168ba98326d9d705c3bbbdeac67be504f4fa15
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-06 16:32:47 -0700

    fix(DataModel): Register classes into DataModel module

--- Sources/Common/DataModel/index.js
+++ Sources/Common/DataModel/index.js
@@ -1,11 +1,19 @@
 import vtkBoundingBox from './BoundingBox';
 import vtkDataSet from './DataSet';
 import vtkDataSetAttributes from './DataSetAttributes';
+import vtkImageData from './ImageData';
 import vtkPlane from './Plane';
+import vtkPointSet from './PointSet';
+import vtkPolyData from './PolyData';
+import vtkStructuredData from './StructuredData';
 
 export default {
   vtkBoundingBox,
   vtkDataSet,
   vtkDataSetAttributes,
+  vtkImageData,
   vtkPlane,
+  vtkPointSet,
+  vtkPolyData,
+  vtkStructuredData,
 };
