3e89a15f60927627fc233202d7814d41f0e78920
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-07 09:24:52 -0700

    fix(Filters): update sources and code cleanup

--- Sources/Filters/Sources/ImageGridSource/index.js
+++ Sources/Filters/Sources/ImageGridSource/index.js
@@ -128,7 +128,7 @@ export function extend(publicAPI, model, initialValues = {}) {
 
 // ----------------------------------------------------------------------------
 
-export const newInstance = macro.newInstance(extend);
+export const newInstance = macro.newInstance(extend, 'vtkImageGridSource');
 
 // ----------------------------------------------------------------------------
 
