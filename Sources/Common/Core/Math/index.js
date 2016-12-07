6b03703e6b5281deb27e4bc4f4fa21e31665787d
Tristan Wright <tristan.wright@kitware.com>
2016-12-07 15:41:24 -0700

    style(Core): spelling of insure -> ensure
    closes #41

--- Sources/Common/Core/Math/index.js
+++ Sources/Common/Core/Math/index.js
@@ -727,7 +727,7 @@ function jacobiN(a, n, w, v) {
       }
     }
   }
-  // insure eigenvector consistency (i.e., Jacobi can compute vectors that
+  // ensure eigenvector consistency (i.e., Jacobi can compute vectors that
   // are negative of one another (.707,.707,0) and (-.707,-.707,0). This can
   // reek havoc in hyperstreamline/other stuff. We will select the most
   // positive eigenvector.
