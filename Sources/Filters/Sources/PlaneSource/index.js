c640579538842f59d11dc715aeba91a16127d1bd
Will Schroeder <will.schroeder@kitware.com>
2016-12-07 11:32:14 -0500

    feat(PlaneSource): Add plane source

---
+++ Sources/Filters/Sources/PlaneSource/index.js
@@ -0,0 +1,131 @@
+import * as macro     from '../../../macro';
+import vtkPolyData    from '../../../Common/DataModel/PolyData';
+import vtkDataArray   from '../../../Common/Core/DataArray';
+import vtkMath        from '../../../Common/Core/Math';
+
+
+/* global window */
+
+// ----------------------------------------------------------------------------
+// vtkPlaneSource methods
+// ----------------------------------------------------------------------------
+
+export function vtkPlaneSource(publicAPI, model) {
+  // Set our className
+  model.classHierarchy.push('vtkPlaneSource');
+
+  function requestData(inData, outData) {
+    if (model.deleted) {
+      return;
+    }
+
+    // Check input
+    const pd = vtkPolyData.newInstance();
+    let v10 = new Float32Array(3);
+    let v20 = new Float32Array(3);
+    let n = new Float32Array(3);
+    vtkMath.subtract(model.point1,model.origin,v10);
+    vtkMath.subtract(model.point2,model.origin,v20);
+    vtkMath.cross(v10,v20,n);
+    if ( vtkMath.norm(n) <= 0.0 ) {
+      vtkWarningMacro('Bad plane definition');
+      return;
+    }
+
+    // hand create a plane with special scalars
+    const xres = model.xResolution;
+    const yres = model.yResolution;
+    const numPts = (xres + 1) * (yres + 1);
+    const numPolys = xres * yres;
+
+    // Points
+    const points = new Float32Array(numPts * 3);
+    pd.getPoints().setData(points, 3);
+
+    // Cells
+    const polys = new Uint32Array(5 * numPolys);
+    pd.getPolys().setData(polys, 1);
+
+    // Texture coords
+    const tcData = new Float32Array(numPts * 2);
+    const tcoords = vtkDataArray.newInstance({numberOfComponents: 2, values: tcData, name: 'TextureCoordinates'})
+    pd.getPointData().addArray(tcoords);
+
+
+    let tc = new Float32Array(2)
+    let idx = 0;
+    for (let j = 0; j < (yres + 1); j++) {
+      tc[1] = j / yres;
+      for (let i = 0; i < (xres + 1); i++) {
+        tc[0] = i / xres;
+
+        points[(idx * 3)] = model.origin[0] + tc[0]*v10[0] + tc[1]*v20[0];
+        points[(idx * 3) + 1] = model.origin[1] + tc[0]*v10[1] + tc[1]*v20[1];
+        points[(idx * 3) + 2] = model.origin[2] + tc[0]*v10[2] + tc[1]*v20[2];
+
+        tcData[(idx * 2)] = tc[0];
+        tcData[(idx * 2) + 1] = tc[1];
+
+        idx++;
+      }
+    }
+
+    // Generate polygon connectivity
+    //
+    idx = 0;
+    for (let j=0; j < yres; j++)
+    {
+      for (let i=0; i < xres; i++)
+      {
+        polys[(idx * 5) + 0] = 4;
+        polys[(idx * 5) + 1] = i + j * (xres + 1);
+        polys[(idx * 5) + 2] = polys[(idx * 5) + 1] + 1;
+        polys[(idx * 5) + 3] = polys[(idx * 5) + 1] + xres + 2;
+        polys[(idx * 5) + 4] = polys[(idx * 5) + 1] + xres + 1;
+      }
+    }
+}
+
+  // Expose methods
+  publicAPI.requestData = requestData;
+}
+
+// ----------------------------------------------------------------------------
+// Object factory
+// ----------------------------------------------------------------------------
+
+const DEFAULT_VALUES = {
+  xResolution: 10,
+  yResolution: 10,
+  origin: [0, 0, 0],
+  point1: [1.0, 0.0, 0.0],
+  point2: [0.0, 1.0, 0.0],
+};
+
+// ----------------------------------------------------------------------------
+
+export function extend(publicAPI, model, initialValues = {}) {
+  Object.assign(model, DEFAULT_VALUES, initialValues);
+
+  // Build VTK API
+  macro.obj(publicAPI, model);
+  macro.setGet(publicAPI, model, [
+    'xResolution',
+    'yResolution',
+  ]);
+  macro.setGetArray(publicAPI, model, [
+    'origin',
+    'point1',
+    'point2',
+  ], 3);
+  macro.algo(publicAPI, model, 0, 1);
+  vtkPlaneSource(publicAPI, model);
+}
+
+// ----------------------------------------------------------------------------
+
+export const newInstance = macro.newInstance(extend);
+
+// ----------------------------------------------------------------------------
+
+export default { newInstance, extend };
---
+++ Sources/Filters/Sources/PlaneSource/test/testPlane.js
@@ -0,0 +1,47 @@
+import test from 'tape-catch';
+
+import vtkOpenGLRenderWindow from '../../../../Rendering/OpenGL/RenderWindow';
+import vtkRenderWindow from '../../../../Rendering/Core/RenderWindow';
+import vtkRenderer from '../../../../Rendering/Core/Renderer';
+import vtkPlaneSource from '../../../../Filters/Sources/PlaneSource';
+import vtkActor from '../../../../Rendering/Core/Actor';
+import vtkMapper from '../../../../Rendering/Core/Mapper';
+
+import baseline from './baseline.png';
+import testUtils from '../../../../Testing/testUtils';
+
+/* global document */
+
+test.onlyIfWebGL('Test vtkPlaneSource Rendering', (t) => {
+  t.ok('rendering', 'vtkPlaneSource Rendering');
+
+  // Create some control UI
+  const container = document.querySelector('body');
+  const renderWindowContainer = document.createElement('div');
+  container.appendChild(renderWindowContainer);
+
+  // create what we will view
+  const renderWindow = vtkRenderWindow.newInstance();
+  const renderer = vtkRenderer.newInstance();
+  renderWindow.addRenderer(renderer);
+  renderer.setBackground(0.32, 0.34, 0.43);
+
+  const actor = vtkActor.newInstance();
+  renderer.addActor(actor);
+
+  const mapper = vtkMapper.newInstance();
+  actor.setMapper(mapper);
+
+  const PlaneSource = vtkPlaneSource.newInstance({ xResolution: 5, yResolution: 10 });
+  mapper.setInputConnection(PlaneSource.getOutputPort());
+
+  // now create something to view it, in this case webgl
+  const glwindow = vtkOpenGLRenderWindow.newInstance();
+  glwindow.setContainer(renderWindowContainer);
+  renderWindow.addView(glwindow);
+  glwindow.setSize(400, 400);
+
+  const image = glwindow.captureImage();
+
+  testUtils.compareImages(image, [baseline], 'Filters/Sources/PlaneSource/', t);
+});
