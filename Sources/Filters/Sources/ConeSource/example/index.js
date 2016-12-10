c33892603a82e8b1eb583045bf1431ce2341df35
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-10 10:57:01 -0700

    fix(ConeSourceExample): Update code to use full screen render window and simplify code

--- Sources/Filters/Sources/ConeSource/example/index.js
+++ Sources/Filters/Sources/ConeSource/example/index.js
@@ -1,72 +1,61 @@
+import vtkFullScreenRenderWindow  from '../../../../../Sources/Testing/FullScreenRenderWindow';
+
 import vtkActor                   from '../../../../../Sources/Rendering/Core/Actor';
-import vtkCamera                  from '../../../../../Sources/Rendering/Core/Camera';
 import vtkConeSource              from '../../../../../Sources/Filters/Sources/ConeSource';
 import vtkMapper                  from '../../../../../Sources/Rendering/Core/Mapper';
-import vtkOpenGLRenderWindow      from '../../../../../Sources/Rendering/OpenGL/RenderWindow';
-import vtkRenderer                from '../../../../../Sources/Rendering/Core/Renderer';
-import vtkRenderWindow            from '../../../../../Sources/Rendering/Core/RenderWindow';
-import vtkRenderWindowInteractor  from '../../../../../Sources/Rendering/Core/RenderWindowInteractor';
 
 import controlPanel from './controlPanel.html';
 
-// Create some control UI
-const rootContainer = document.querySelector('body');
-rootContainer.innerHTML = controlPanel;
-const renderWindowContainer = document.querySelector('.renderwidow');
-// ----------------------
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
 
-const ren = vtkRenderer.newInstance();
-ren.setBackground(0.32, 0.34, 0.43);
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({ background: [0, 0, 0] });
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
 
-const renWin = vtkRenderWindow.newInstance();
-renWin.addRenderer(ren);
-
-const glwindow = vtkOpenGLRenderWindow.newInstance();
-glwindow.setContainer(renderWindowContainer);
-renWin.addView(glwindow);
-
-const iren = vtkRenderWindowInteractor.newInstance();
-iren.setView(glwindow);
-
-const actor = vtkActor.newInstance();
-ren.addActor(actor);
-
-const mapper = vtkMapper.newInstance();
-actor.setMapper(mapper);
-
-const cam = vtkCamera.newInstance();
-ren.setActiveCamera(cam);
-cam.setFocalPoint(0, 0, 0);
-cam.setPosition(0, 0, 4);
-cam.setClippingRange(0.1, 50.0);
+// ----------------------------------------------------------------------------
+// Example code
+// ----------------------------------------------------------------------------
 
 const coneSource = vtkConeSource.newInstance();
+const actor = vtkActor.newInstance();
+const mapper = vtkMapper.newInstance();
+
+actor.setMapper(mapper);
 mapper.setInputConnection(coneSource.getOutputPort());
 
-iren.initialize();
-iren.bindEvents(renderWindowContainer, document);
-iren.start();
+renderer.addActor(actor);
+renderer.resetCamera();
+renderWindow.render();
 
-// ----- JavaScript UI -----
+// -----------------------------------------------------------
+// UI control handling
+// -----------------------------------------------------------
+
+fullScreenRenderer.addController(controlPanel);
 
 ['height', 'radius', 'resolution'].forEach((propertyName) => {
   document.querySelector(`.${propertyName}`).addEventListener('input', (e) => {
     const value = Number(e.target.value);
     coneSource.set({ [propertyName]: value });
-    renWin.render();
+    renderWindow.render();
   });
 });
 
 document.querySelector('.capping').addEventListener('change', (e) => {
   const capping = !!(e.target.checked);
   coneSource.set({ capping });
-  renWin.render();
+  renderWindow.render();
 });
 
-// ----- Console play ground -----
+// -----------------------------------------------------------
+// Make some variables global so that you can inspect and
+// modify objects in your browser's developer console:
+// -----------------------------------------------------------
 
 global.coneSource = coneSource;
 global.mapper = mapper;
 global.actor = actor;
-global.renderer = ren;
-global.renderWindow = renWin;
+global.renderer = renderer;
+global.renderWindow = renderWindow;
