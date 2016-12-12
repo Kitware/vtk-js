a3a0c79f5cc46a5abf3b0b034f72049e502b74fe
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-12 12:00:31 -0700

    test(tests): Update test list while reordering them in alphabetical order

--- Sources/tests.js
+++ Sources/tests.js
@@ -1,10 +1,11 @@
 // import './Common/Core/LookupTable/test/testCategoricalColors';
 import './Common/DataModel/DataSetAttributes/test/testDataSetAttributes';
+import './Filters/General/Calculator/test/testCalculator';
+import './Filters/General/WarpScalar/test/testWarp';
 import './Filters/Sources/ConeSource/test/testCone';
 import './Filters/Sources/LineSource/test/testLine';
 import './Filters/Sources/PlaneSource/test/testPlane';
-import './Filters/General/WarpScalar/test/testWarp';
-import './Filters/General/Calculator/test/testCalculator';
+import './Filters/Sources/PointSource/test/testPointSource';
 import './Rendering/Core/ColorTransferFunction/test/testColorTransferFunction';
 import './Rendering/Core/Mapper/test/testEdgeVisibility';
 import './Rendering/OpenGL/PolyDataMapper/test/testInterpolateScalarsBeforeMapping';
