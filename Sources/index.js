f931c1c909c7e0e44731884a49e98eb132a15dee
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-12-13 11:07:30 -0700

    fix(vtk): Fix generated library to be usable as external script

--- Sources/index.js
+++ Sources/index.js
@@ -3,9 +3,12 @@ import Filters      from './Filters';
 import IO           from './IO';
 import Rendering    from './Rendering';
 
-export {
-  Common,
-  Filters,
-  IO,
-  Rendering,
-};
+import vtk from './vtk';
+
+vtk.Common = Common;
+vtk.Filters = Filters;
+vtk.IO = IO;
+vtk.Rendering = Rendering;
+
+/* eslint-disable */
+module.exports = vtk;
