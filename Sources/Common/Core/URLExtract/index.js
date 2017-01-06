898fcd2ed1dc417b33b367d42db818c79451628e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-01-06 14:46:26 -0700

    fix(URLExtract): Add URL extract helper

---
+++ Sources/Common/Core/URLExtract/index.js
@@ -0,0 +1,41 @@
+function identity(i) {
+  return i;
+}
+
+function toNativeType(str) {
+  if (str === null || str === 'null') {
+    return null;
+  } else if (str === 'true') {
+    return true;
+  } else if (str === 'false') {
+    return false;
+  } else if (str === undefined || str === 'undefined') {
+    return undefined;
+  } else if (str === '' || isNaN(str)) {
+    return str;
+  }
+  return parseFloat(str);
+}
+
+function extractURLParameters(castToNativeType = true, query = window.location.search) {
+  const summary = {};
+  const convert = castToNativeType ? toNativeType : identity;
+  const queryTokens = (query || '')
+    .replace(/#.*/, '') // remove hash query
+    .replace('?', '') // Remove ? from the head
+    .split('&'); // extract token pair
+
+  queryTokens.forEach((token) => {
+    const [key, value] = token.split('=').map(s => decodeURIComponent(s));
+    if (key) {
+      summary[key] = value ? convert(value) : true;
+    }
+  });
+
+  return summary;
+}
+
+export default {
+  toNativeType,
+  extractURLParameters,
+};
