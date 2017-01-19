071df6b234d885a19f7be054476a1a77ccbe9a6d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-01-19 16:49:46 -0700

    chore(XMLConverter): Add chemistry-mapper json generator

---
+++ Utilities/XMLConverter/chemistry-mapper/README.md
@@ -0,0 +1,1 @@
+xml2json -i $PWD/Utilities/XMLConverter/chemistry/elements.xml -o $PWD/Utilities/XMLConverter/chemistry-mapper/elements.json -p chemistry-mapper
---
+++ Utilities/XMLConverter/chemistry-mapper/post-process.js
@@ -0,0 +1,18 @@
+var preProcessing = require('../chemistry/post-process.js');
+
+const valueToExtract = ['radiusCovalent', 'elementColor'];
+
+module.exports = function (xml) {
+  var atoms = {};
+  var fullJSON = preProcessing(xml);
+  fullJSON.atoms.forEach(function (a) {
+    var values = valueToExtract.map(function (key) {
+      return a[key];
+    });
+    // Remove atoms that have no radiusCovalent
+    if (values[0]) {
+      atoms[a.id] = values;
+    }
+  });
+  return atoms;
+};
