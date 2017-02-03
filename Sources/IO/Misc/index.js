19683dab5e9f2c5fbb90d0105199b17dd18ccb25
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-02-02 17:36:50 -0700

    fix(ObjReader): Add OBJ reader

--- Sources/IO/Misc/index.js
+++ Sources/IO/Misc/index.js
@@ -1,7 +1,9 @@
 import vtkElevationReader    from './ElevationReader';
+import vtkObjReader          from './ObjReader';
 import vtkPDBReader          from './PDBReader';
 
 export default {
   vtkElevationReader,
+  vtkObjReader,
   vtkPDBReader,
 };
