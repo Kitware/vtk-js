948086086ae854cf58f2c6ed8ca00199c12df320
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-02-02 17:36:21 -0700

    fix(ElevationReader): Make sure to register name of the reader for vtk factory

--- Sources/IO/Misc/ElevationReader/index.js
+++ Sources/IO/Misc/ElevationReader/index.js
@@ -162,7 +162,7 @@ export function extend(publicAPI, model, initialValues = {}) {
 
 // ----------------------------------------------------------------------------
 
-export const newInstance = macro.newInstance(extend);
+export const newInstance = macro.newInstance(extend, 'vtkElevationReader');
 
 // ----------------------------------------------------------------------------
 
