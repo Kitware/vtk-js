86b9b01771f43d656675067799202d29e56044ec
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2017-02-06 14:37:29 -0500

    feat(multipleBonds): MoleculeToRepresentation allow multiple bond display
    Based on the order array from a vtkMolecule class, display the suitable number
    of bonds between atoms
    Scale bonds to always fit to the atom

--- Sources/Filters/General/MoleculeToRepresentation/index.js
+++ Sources/Filters/General/MoleculeToRepresentation/index.js
@@ -15,6 +15,9 @@ const { vtkErrorMacro } = macro;
 const ATOMS = {};
 atomElem.atoms.forEach((a) => { ATOMS[a.atomicNumber] = a; });
 
+const bondPositionData = [];
+const bondScaleData = [];
+const bondOrientationData = [];
 
 // ----------------------------------------------------------------------------
 // vtkMoleculeToRepresentation methods
@@ -23,6 +26,19 @@ atomElem.atoms.forEach((a) => { ATOMS[a.atomicNumber] = a; });
 export function vtkMoleculeToRepresentation(publicAPI, model) {
   // Set our className
   model.classHierarchy.push('vtkMoleculeToRepresentation');
+
+  function addBond(position, orientation, length, radius = model.bondRadius) {
+    bondScaleData.push(length);
+    bondScaleData.push(radius);
+
+    bondOrientationData.push(orientation[0]);
+    bondOrientationData.push(orientation[1]);
+    bondOrientationData.push(orientation[2]);
+
+    bondPositionData.push(position[0]);
+    bondPositionData.push(position[1]);
+    bondPositionData.push(position[2]);
+  }
 
   publicAPI.requestData = (inData, outData) => {
     // input
@@ -78,11 +94,7 @@ export function vtkMoleculeToRepresentation(publicAPI, model) {
     const covalentArray = [];
     const colorArray = [];
 
-    const bondPositionData = [];
-    const bondScaleData = [];
-    const bondOrientationData = [];
-
-    vtkDebugMacro('Checking for bonds with tolerance ', model.tolerance);
+    // vtkDebugMacro(['Checking for bonds with tolerance ', model.tolerance]);
 
     // go through each points and fill from elements.json
     /* eslint-disable no-continue */
@@ -97,9 +109,9 @@ export function vtkMoleculeToRepresentation(publicAPI, model) {
         colorArray.push(ATOMS[atomicNumber[i]].elementColor[2]);
       }
 
-      // skip atoms specified by hideElement
+      // skip atoms specified by hideElements
       // model.hideHydrogen = false; // show hydrogen
-      if (model.hideElement.includes(ATOMS[atomicNumber[i]].id)) {
+      if (model.hideElements.includes(ATOMS[atomicNumber[i]].id)) {
         continue;
       }
 
@@ -169,8 +181,8 @@ export function vtkMoleculeToRepresentation(publicAPI, model) {
       const j = bondIndex[(index * 2) + 1];
 
       // Do not append if i or j belong to element to not display
-      if (model.hideElement.includes(ATOMS[atomicNumber[i]].id) ||
-        model.hideElement.includes(ATOMS[atomicNumber[j]].id)) {
+      if (model.hideElements.includes(ATOMS[atomicNumber[i]].id) ||
+        model.hideElements.includes(ATOMS[atomicNumber[j]].id)) {
         continue;
       }
 
@@ -184,29 +196,64 @@ export function vtkMoleculeToRepresentation(publicAPI, model) {
       diff[2] -= pointsArray[iPtsIdx + 2];
       const diffsq = (diff[0] * diff[0]) + (diff[1] * diff[1]) + (diff[2] * diff[2]);
 
-      // offset between center of SphereJ (resp. SphereI) and the start of the bond
-      const bondRadiusSq = model.bondRadius * model.bondRadius;
       const radiusJsq = radiusArray[j] * model.atomicRadiusScaleFactor * radiusArray[j] * model.atomicRadiusScaleFactor;
       const radiusIsq = radiusArray[i] * model.atomicRadiusScaleFactor * radiusArray[i] * model.atomicRadiusScaleFactor;
-      const offsetJ = Math.sqrt(radiusJsq - bondRadiusSq);
-      const offsetI = Math.sqrt(radiusIsq - bondRadiusSq);
+      let bondDelta = (2 + model.deltaBondFactor) * model.bondRadius; // distance between 2 bonds
 
-      // Display one bond
-      if (bondOrder[index] >= 1) {
-        bondScaleData.push(Math.sqrt(diffsq) - offsetJ - offsetI);
-        bondScaleData.push(model.bondRadius);
+      // scale bonds if total distance from bonds is bigger than 2r*factor with r = min(r_i, r_j)
+      const r = Math.min(radiusArray[i] * model.atomicRadiusScaleFactor, radiusArray[j] * model.atomicRadiusScaleFactor);
+      const t = ((bondOrder[index] - 1) * bondDelta) + (2 * model.bondRadius);
+      if (t > 2 * r * 0.6) {
+        model.bondRadius *= ((2 * r * 0.6) / t);
+        // recompute bondDelta
+        bondDelta = (2 + model.deltaBondFactor) * model.bondRadius; // distance between 2 bonds
+      }
 
-        const vectUnitJI = [diff[0] / Math.sqrt(diffsq),
+      // Display multiple bond
+      // loop such as 0 11 22 if odd order / 00 11 22 33 if even order
+      // To make:     0 22 44 66 88 ...      11 33 55 77 ....
+      // because the offset has to be:
+      // (with bd= bondDelta. Note the minus is added just before creating bondPos)
+      //   - odd order: 0 2bd/2 -2bd/2 4bd/2 -4bd/2 ...
+      //   - even order:  1bd/2 -1bd/2 3bd/2 -3bd/2 ...
+      // Then, to transform loop to offset we have:
+      //   - odd order: x * 2 <=> x * 2 + 1 - 1
+      //   - even order: x * 2 + 1
+      // (with x the loop <=> floor(k/2))
+      const oddOrEven = ((bondOrder[index]) % 2); // zero if even order / one if odd order
+      for (let k = oddOrEven; k < bondOrder[index] + oddOrEven; k++) {
+        // dist from center to bond depending of number of bond
+        let offset = ((((Math.floor(k / 2) * 2) + 1) - oddOrEven) * bondDelta) / 2;
+
+        // offset between center of SphereJ (resp. SphereI) and the start of the bond
+        const offsetJ = Math.sqrt(radiusJsq - Math.pow(model.bondRadius + offset, 2));
+        const offsetI = Math.sqrt(radiusIsq - Math.pow(model.bondRadius + offset, 2));
+        const vectUnitJI = [
+          diff[0] / Math.sqrt(diffsq),
           diff[1] / Math.sqrt(diffsq),
           diff[2] / Math.sqrt(diffsq)];
+        const vectUnitJIperp = [0, 0, 0];
+        // Search perp to vectUnitJI: find axis != 0 to create vectUnitJIperp such as dot(vectUnitJIperp,vectUnitJI) = 0
+        for (let coord = 0; coord < 3; coord++) {
+          if (Math.abs(vectUnitJI[coord]) < 0.000001) {
+            continue;
+          }
+          vectUnitJIperp[coord] = (-((vectUnitJI[(coord + 2) % 3] * vectUnitJI[(coord + 2) % 3]) +
+            (vectUnitJI[(coord + 1) % 3] * vectUnitJI[(coord + 1) % 3])) / vectUnitJI[coord]);
+          vectUnitJIperp[(coord + 1) % 3] = (vectUnitJI[(coord + 1) % 3]);
+          vectUnitJIperp[(coord + 2) % 3] = (vectUnitJI[(coord + 2) % 3]);
+          vtkMath.normalize(vectUnitJIperp);
+          break;
+        }
 
-        bondOrientationData.push(vectUnitJI[0]);
-        bondOrientationData.push(vectUnitJI[1]);
-        bondOrientationData.push(vectUnitJI[2]);
+        offset *= Math.pow(-1, k % 2);
+        const bondPos = [
+          pointsArray[jPtsIdx] - (((offsetJ - offsetI) * vectUnitJI[0]) / 2.0) - (diff[0] / 2.0) + (offset * vectUnitJIperp[0]),
+          pointsArray[jPtsIdx + 1] - (((offsetJ - offsetI) * vectUnitJI[1]) / 2.0) - (diff[1] / 2.0) + (offset * vectUnitJIperp[1]),
+          pointsArray[jPtsIdx + 2] - (((offsetJ - offsetI) * vectUnitJI[2]) / 2.0) - (diff[2] / 2.0) + (offset * vectUnitJIperp[2])];
+        const bondLenght = Math.sqrt(diffsq) - offsetJ - offsetI;
 
-        bondPositionData.push(pointsArray[jPtsIdx] - (((offsetJ - offsetI) * vectUnitJI[0]) / 2.0) - (diff[0] / 2.0));
-        bondPositionData.push(pointsArray[jPtsIdx + 1] - (((offsetJ - offsetI) * vectUnitJI[1]) / 2.0) - (diff[1] / 2.0));
-        bondPositionData.push(pointsArray[jPtsIdx + 2] - (((offsetJ - offsetI) * vectUnitJI[2]) / 2.0) - (diff[2] / 2.0));
+        addBond(bondPos, vectUnitJI, bondLenght);
       }
     }
 
@@ -225,16 +272,11 @@ export function vtkMoleculeToRepresentation(publicAPI, model) {
 
     StickData.getPoints().setData(bondPositionData, 3);
 
-    const test = true;
-    if (test) {
-      const stickScales = vtkDataArray.newInstance({ numberOfComponents: 2, values: bondScaleData, name: 'stickScales' });
-      StickData.getPointData().addArray(stickScales);
-    }
+    const stickScales = vtkDataArray.newInstance({ numberOfComponents: 2, values: bondScaleData, name: 'stickScales' });
+    StickData.getPointData().addArray(stickScales);
 
-    if (test) {
-      const orientation = vtkDataArray.newInstance({ numberOfComponents: 3, values: bondOrientationData, name: 'orientation' });
-      StickData.getPointData().addArray(orientation);
-    }
+    const orientation = vtkDataArray.newInstance({ numberOfComponents: 3, values: bondOrientationData, name: 'orientation' });
+    StickData.getPointData().addArray(orientation);
 
     // Update output
     outData[0] = SphereData;
@@ -253,8 +295,9 @@ const DEFAULT_VALUES = {
   tolerance: 0.45,
   atomicRadiusScaleFactor: 0.3,
   bondRadius: 0.075,
+  deltaBondFactor: 0.6,
   radiusType: 'radiusVDW',
-  hideElement: [],
+  hideElements: [],
 };
 
 // ----------------------------------------------------------------------------
@@ -267,7 +310,7 @@ export function extend(publicAPI, model, initialValues = {}) {
   macro.setGet(publicAPI, model, [
     'sphereScaleArrayName',
     'radiusType',
-    'hideElement',
+    'hideElements',
   ]);
   macro.algo(publicAPI, model, 1, 2);
   vtkMoleculeToRepresentation(publicAPI, model);
