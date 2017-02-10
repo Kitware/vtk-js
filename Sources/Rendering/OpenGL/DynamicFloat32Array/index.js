eccff6f5ef5ea08c9031d923d38b15bd51a4400e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-02-10 08:43:18 -0700

    fix(DynamicFloat32Array): Better code

--- Sources/Rendering/OpenGL/DynamicFloat32Array/index.js
+++ Sources/Rendering/OpenGL/DynamicFloat32Array/index.js
@@ -17,35 +17,34 @@ export default class DynamicFloat32Array {
   }
 
   pushBytes(r, g, b, a = 255) {
-    const offset = this.floats.length * 4;
-    const bytes = Uint8Array.from([r, g, b, a]);
-    this.bytes.push({ offset, bytes });
+    const data = new DataView(new ArrayBuffer(8));
+    data.setUint32(0, this.floats.length * 4);
+    data.setUint8(4, r);
+    data.setUint8(5, g);
+    data.setUint8(6, b);
+    data.setUint8(7, a);
+
     this.floats.push(0); // place holder
+    this.bytes.push(data);
   }
 
   pushBytesFromArray(buffer, offset = 0, length = 4, fillValue = 255) {
-    const bytes = new Uint8Array(4);
-    bytes[0] = (length > 0) ? buffer[offset + 0] : fillValue;
-    bytes[1] = (length > 1) ? buffer[offset + 1] : fillValue;
-    bytes[2] = (length > 2) ? buffer[offset + 2] : fillValue;
-    bytes[3] = (length > 3) ? buffer[offset + 3] : fillValue;
+    const data = new DataView(new ArrayBuffer(8));
+    data.setUint32(0, this.floats.length * 4);
+    data.setUint8(4, (length > 0) ? buffer[offset + 0] : fillValue);
+    data.setUint8(5, (length > 1) ? buffer[offset + 1] : fillValue);
+    data.setUint8(6, (length > 2) ? buffer[offset + 2] : fillValue);
+    data.setUint8(7, (length > 3) ? buffer[offset + 3] : fillValue);
 
-    this.bytes.push({
-      offset: this.floats.length * 4,
-      bytes,
-    });
     this.floats.push(0); // place holder
+    this.bytes.push(data);
   }
 
   getFrozenArray() {
     const array = Float32Array.from(this.floats);
     const view = new DataView(array.buffer);
     this.bytes.forEach((entry) => {
-      const { offset, bytes } = entry;
-      view.setUint8(offset + 0, bytes[0]);
-      view.setUint8(offset + 1, bytes[1]);
-      view.setUint8(offset + 2, bytes[2]);
-      view.setUint8(offset + 3, bytes[3]);
+      view.setFloat32(entry.getUint32(0), entry.getFloat32(4));
     });
     return array;
   }
