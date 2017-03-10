a073d0bd626433616e57f50c00e9de48cdffa556
Ken Martin <ken.martin@kitware.com>
2017-03-10 13:08:11 -0500

    perf(Rendering): make VolumeMapper handle LOD
    With this change by default VolumeMapper will adjust
    its rendering speed to hit the desired update rate.

--- Sources/Rendering/Core/VolumeMapper/index.js
+++ Sources/Rendering/Core/VolumeMapper/index.js
@@ -36,6 +36,7 @@ const DEFAULT_VALUES = {
   sampleDistance: 1.0,
   imageSampleDistance: 1.0,
   maximumSamplesPerRay: 1000,
+  autoAdjustSampleDistances: true,
 };
 
 // ----------------------------------------------------------------------------
@@ -52,6 +53,7 @@ export function extend(publicAPI, model, initialValues = {}) {
     'sampleDistance',
     'imageSampleDistance',
     'maximumSamplesPerRay',
+    'autoAdjustSampleDistances',
   ]);
 
   // Object methods
