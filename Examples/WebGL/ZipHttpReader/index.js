f58b541d110ec45796ea8acab0ce9ec98cef1f60
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-04-06 09:58:36 -0600

    doc(ZipHttpReader): Add example on how to use zipped datsaet

---
+++ Examples/WebGL/ZipHttpReader/index.js
@@ -0,0 +1,76 @@
+import vtkColorTransferFunction   from 'vtk.js/Sources/Rendering/Core/ColorTransferFunction';
+import vtkFullScreenRenderWindow  from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
+import vtkHttpDataSetReader       from 'vtk.js/Sources/IO/Core/HttpDataSetReader';
+import vtkPiecewiseFunction       from 'vtk.js/Sources/Common/DataModel/PiecewiseFunction';
+import vtkVolume                  from 'vtk.js/Sources/Rendering/Core/Volume';
+import vtkVolumeMapper            from 'vtk.js/Sources/Rendering/Core/VolumeMapper';
+
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
+
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({ background: [0, 0, 0] });
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
+
+// ----------------------------------------------------------------------------
+// Example code
+// ----------------------------------------------------------------------------
+// Server is not sending the .gz and whith the compress header
+// Need to fetch the true file name and uncompress it locally
+// ----------------------------------------------------------------------------
+
+const reader = vtkHttpDataSetReader.newInstance();
+
+const actor = vtkVolume.newInstance();
+const mapper = vtkVolumeMapper.newInstance();
+mapper.setSampleDistance(0.7);
+actor.setMapper(mapper);
+
+// create color and opacity transfer functions
+const ctfun = vtkColorTransferFunction.newInstance();
+ctfun.addRGBPoint(200.0, 0.4, 0.2, 0.0);
+ctfun.addRGBPoint(2000.0, 1.0, 1.0, 1.0);
+const ofun = vtkPiecewiseFunction.newInstance();
+ofun.addPoint(200.0, 0.0);
+ofun.addPoint(1200.0, 0.5);
+ofun.addPoint(3000.0, 0.8);
+actor.getProperty().setRGBTransferFunction(0, ctfun);
+actor.getProperty().setScalarOpacity(0, ofun);
+actor.getProperty().setScalarOpacityUnitDistance(0, 4.5);
+actor.getProperty().setInterpolationTypeToLinear();
+actor.getProperty().setUseGradientOpacity(0, true);
+actor.getProperty().setGradientOpacityMinimumValue(0, 15);
+actor.getProperty().setGradientOpacityMinimumOpacity(0, 0.0);
+actor.getProperty().setGradientOpacityMaximumValue(0, 100);
+actor.getProperty().setGradientOpacityMaximumOpacity(0, 1.0);
+actor.getProperty().setShade(true);
+actor.getProperty().setAmbient(0.2);
+actor.getProperty().setDiffuse(0.7);
+actor.getProperty().setSpecular(0.3);
+actor.getProperty().setSpecularPower(8.0);
+
+mapper.setInputConnection(reader.getOutputPort());
+
+reader.setUrl('https://data.kitware.com/api/v1/file/58e665158d777f16d095fc2e/download', { fullPath: true, compression: 'zip' }).then(() => {
+  reader.loadData().then(() => {
+    renderer.addVolume(actor);
+    renderer.resetCamera();
+    renderer.getActiveCamera().zoom(1.5);
+    renderer.getActiveCamera().elevation(70);
+    renderWindow.render();
+  });
+});
+
+// -----------------------------------------------------------
+// Make some variables global so that you can inspect and
+// modify objects in your browser's developer console:
+// -----------------------------------------------------------
+
+global.source = reader;
+global.mapper = mapper;
+global.actor = actor;
+global.ctfun = ctfun;
+global.ofun = ofun;
+global.renderer = renderer;
+global.renderWindow = renderWindow;
