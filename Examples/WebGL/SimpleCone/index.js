072f0ebe3bec78b4e017c76e2481cced8f34159f
Ken Martin <ken.martin@kitware.com>
2017-05-23 11:28:27 -0400

    docs(Example): provide a simple minimal example
    Added SimpleCone as a very minimal example of using vtk.js

---
+++ Examples/WebGL/SimpleCone/index.js
@@ -0,0 +1,58 @@
+import vtkActor                  from 'vtk.js/Sources/Rendering/Core/Actor';
+import vtkConeSource             from 'vtk.js/Sources/Filters/Sources/ConeSource';
+import vtkMapper                 from 'vtk.js/Sources/Rendering/Core/Mapper';
+import vtkOpenGLRenderWindow     from 'vtk.js/Sources/Rendering/OpenGL/RenderWindow';
+import vtkRenderWindow           from 'vtk.js/Sources/Rendering/Core/RenderWindow';
+import vtkRenderWindowInteractor from 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
+import vtkRenderer               from 'vtk.js/Sources/Rendering/Core/Renderer';
+
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
+
+const renderWindow = vtkRenderWindow.newInstance();
+const renderer = vtkRenderer.newInstance({ background: [0.2, 0.3, 0.4] });
+renderWindow.addRenderer(renderer);
+
+// ----------------------------------------------------------------------------
+// Simple pipeline ConeSource --> Mapper --> Actor
+// ----------------------------------------------------------------------------
+
+const coneSource = vtkConeSource.newInstance({ height: 1.0 });
+
+const mapper = vtkMapper.newInstance();
+mapper.setInputConnection(coneSource.getOutputPort());
+
+const actor = vtkActor.newInstance();
+actor.setMapper(mapper);
+
+// ----------------------------------------------------------------------------
+// Add the actor to the renderer and set the camera based on it
+// ----------------------------------------------------------------------------
+
+renderer.addActor(actor);
+renderer.resetCamera();
+
+// ----------------------------------------------------------------------------
+// Use OpenGL as the backend to view the all this
+// ----------------------------------------------------------------------------
+
+const openglRenderWindow = vtkOpenGLRenderWindow.newInstance();
+renderWindow.addView(openglRenderWindow);
+
+// ----------------------------------------------------------------------------
+// Create a div section to put this into
+// ----------------------------------------------------------------------------
+
+const container = document.createElement('div');
+document.querySelector('body').appendChild(container);
+openglRenderWindow.setContainer(container);
+
+// ----------------------------------------------------------------------------
+// Setup an interactor to handle mouse events
+// ----------------------------------------------------------------------------
+
+const interactor = vtkRenderWindowInteractor.newInstance();
+interactor.setView(openglRenderWindow);
+interactor.initialize();
+interactor.bindEvents(container);
