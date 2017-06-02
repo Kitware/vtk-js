331e47b348fdb9322647979ed43e95ac5ca92df5
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-06-02 16:38:37 -0600

    fix(rules): Ensure eslint path in vtkRules

--- Utilities/config/rules.js
+++ Utilities/config/rules.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
+const eslintrcPath = path.join(__dirname, '../../.eslintrc.js');
+
 module.exports = [
-  { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre', options: { configFile: '.eslintrc.js' } },
+  { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre', options: { configFile: eslintrcPath } },
   { test: /\.(png|jpg)$/, use: 'url-loader?limit=8192' },
   { test: /\.html$/, loader: 'html-loader' },
   { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
@@ -18,7 +22,7 @@ module.exports = [
       { loader: 'babel-loader', options: { presets: ['es2015'] } },
       { loader: 'string-replace-loader',
         options: {
-          multiple: [{ search: 'test.onlyIfWebGL', replace: process.env.TRAVIS ? 'test.skip' : 'test', flags: 'g' }]
+          multiple: [{ search: 'test.onlyIfWebGL', replace: process.env.TRAVIS ? 'test.skip' : 'test', flags: 'g' }],
         },
       },
     ],
