aa352d3aa316aa214e9701c6f887982743b88d14
laurenn.lam@kitware.com <laurenn.lam@kitware.com>
2017-08-18 16:23:41 +0200

    style(OpenGLPolyDataMapper): Replace 'bounnds' model attribute by 'bound'

--- Sources/Rendering/OpenGL/PolyDataMapper/index.js
+++ Sources/Rendering/OpenGL/PolyDataMapper/index.js
@@ -1393,10 +1393,10 @@ function vtkOpenGLPolyDataMapper(publicAPI, model) {
 
   publicAPI.computeBounds = (ren, actor) => {
     if (!publicAPI.getInput()) {
-      vtkMath.uninitializeBounds(model.Bounds);
+      vtkMath.uninitializeBounds(model.bounds);
       return;
     }
-    model.bounnds = publicAPI.getInput().getBounds();
+    model.bounds = publicAPI.getInput().getBounds();
   };
 
   publicAPI.updateBufferObjects = (ren, actor) => {
