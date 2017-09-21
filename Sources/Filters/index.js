2f9c81f308c68e31375e76eae7ec1112731e206f
Alexis Girault <alexis.girault@kitware.com>
2017-09-21 15:36:59 +0200

    feat(Cutter): add vtkCutter class
    Co-Authored-by: Laurenn Lam <laurenn.lam@kitware.com>

--- Sources/Filters/index.js
+++ Sources/Filters/index.js
@@ -1,10 +1,12 @@
 import Cornerstone from './Cornerstone';
+import Core from './Core';
 import General from './General';
 import Sources from './Sources';
 import Texture from './Texture';
 
 export default {
   Cornerstone,
+  Core,
   General,
   Sources,
   Texture,
