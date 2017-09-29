f215994cf660dcb7ae4db9719efdbdab7217f566
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-09-29 17:45:18 -0400

    docs(ImageMarchingCubes): Cleanup example

--- Sources/Filters/General/ImageMarchingCubes/example/index.js
+++ Sources/Filters/General/ImageMarchingCubes/example/index.js
@@ -1,13 +1,9 @@
-import vtkFullScreenRenderWindow  from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
-// import macro                      from 'vtk.js/Sources/macro';
-// import vtk                        from 'vtk.js/Sources/vtk';
 import vtkActor                   from 'vtk.js/Sources/Rendering/Core/Actor';
-import vtkCamera                  from 'vtk.js/Sources/Rendering/Core/Camera';
-// import vtkDataArray               from 'vtk.js/Sources/Common/Core/DataArray';
-import vtkMapper                  from 'vtk.js/Sources/Rendering/Core/Mapper';
-import vtkSphere                  from 'vtk.js/Sources/Common/DataModel/Sphere';
-import vtkSampleFunction          from 'vtk.js/Sources/Imaging/Hybrid/SampleFunction';
+import vtkFullScreenRenderWindow  from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
 import vtkImageMarchingCubes      from 'vtk.js/Sources/Filters/General/ImageMarchingCubes';
+import vtkMapper                  from 'vtk.js/Sources/Rendering/Core/Mapper';
+import vtkSampleFunction          from 'vtk.js/Sources/Imaging/Hybrid/SampleFunction';
+import vtkSphere                  from 'vtk.js/Sources/Common/DataModel/Sphere';
 
 import controlPanel from './controller.html';
 
@@ -15,7 +11,7 @@ import controlPanel from './controller.html';
 // Standard rendering code setup
 // ----------------------------------------------------------------------------
 
-const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({ background: [1, 0.5, 0.5] });
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
 const renderer = fullScreenRenderer.getRenderer();
 const renderWindow = fullScreenRenderer.getRenderWindow();
 
@@ -25,22 +21,17 @@ const renderWindow = fullScreenRenderer.getRenderWindow();
 const actor = vtkActor.newInstance();
 renderer.addActor(actor);
 
-const mapper = vtkMapper.newInstance({ interpolateScalarBeforeMapping: true });
+const mapper = vtkMapper.newInstance();
 actor.setMapper(mapper);
 
-const cam = vtkCamera.newInstance();
-renderer.setActiveCamera(cam);
-cam.setFocalPoint(0, 0, 0);
-cam.setPosition(0, 0, 10);
-
 // Build pipeline
-const imp = vtkSphere.newInstance({ center: [0.0, 0.0, 0.0], radius: 0.5 });
-const sample = vtkSampleFunction.newInstance({ implicitFunction: imp, sampleDimensions: [50, 50, 50], modelBounds: [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5] });
-const mc = vtkImageMarchingCubes.newInstance({ contourValue: 0.0 });
+const sphere = vtkSphere.newInstance({ center: [0.0, 0.0, 0.0], radius: 0.5 });
+const sample = vtkSampleFunction.newInstance({ implicitFunction: sphere, sampleDimensions: [50, 50, 50], modelBounds: [-0.5, 0.5, -0.5, 0.5, -0.5, 0.5] });
+const marchingCube = vtkImageMarchingCubes.newInstance({ contourValue: 0.0 });
 
 // Connect the pipeline proper
-mc.setInputConnection(sample.getOutputPort());
-mapper.setInputConnection(mc.getOutputPort());
+marchingCube.setInputConnection(sample.getOutputPort());
+mapper.setInputConnection(marchingCube.getOutputPort());
 
 // ----------------------------------------------------------------------------
 // UI control handling
@@ -48,23 +39,23 @@ mapper.setInputConnection(mc.getOutputPort());
 fullScreenRenderer.addController(controlPanel);
 
 // Define the isosurface value
-document.querySelector('.isoValue').addEventListener('change', (e) => {
+document.querySelector('.isoValue').addEventListener('input', (e) => {
   const value = Number(e.target.value);
-  mc.set({ contourValue: value });
+  marchingCube.setContourValue(value);
   renderWindow.render();
 });
 
 // Define the volume resolution
-document.querySelector('.volumeResolution').addEventListener('change', (e) => {
+document.querySelector('.volumeResolution').addEventListener('input', (e) => {
   const value = Number(e.target.value);
-  sample.set({ sampleDimensions: [value, value, value] });
+  sample.setSampleDimensions(value, value, value);
   renderWindow.render();
 });
 
 // Define the sphere radius
-document.querySelector('.sphereRadius').addEventListener('change', (e) => {
+document.querySelector('.sphereRadius').addEventListener('input', (e) => {
   const value = Number(e.target.value);
-  imp.set({ radius: value });
+  sphere.setRadius(value);
   renderWindow.render();
 });
 
@@ -79,6 +70,6 @@ renderWindow.render();
 // -----------------------------------------------------------
 
 global.source = sample;
-global.filter = mc;
+global.filter = marchingCube;
 global.mapper = mapper;
 global.actor = actor;
