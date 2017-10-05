3c6722630c81b4168c330c9a738b6848cf3baadb
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-10-05 16:04:53 -0600

    fix(macro): Add debounce utility function

--- Sources/macro.js
+++ Sources/macro.js
@@ -672,6 +672,31 @@ export function traverseInstanceTree(instance, extractFunction, accumulator = []
 }
 
 // ----------------------------------------------------------------------------
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return (...args) => {
+    var context = this;
+    var later = () => {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+}
+
+// ----------------------------------------------------------------------------
 // Default export
 // ----------------------------------------------------------------------------
 
@@ -700,4 +725,5 @@ export default {
   vtkInfoMacro,
   vtkLogMacro,
   vtkWarningMacro,
+  debounce,
 };
