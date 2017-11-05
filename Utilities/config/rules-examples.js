377beefa10c120a3ca914b2362376881336bf354
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-11-05 11:54:31 -0700

    fix(build): Add rules for svg + mcss

--- Utilities/config/rules-examples.js
+++ Utilities/config/rules-examples.js
@@ -1,16 +1,7 @@
-const autoprefixer = require('autoprefixer');
-
 module.exports = [
   { test: /\.(png|jpg)$/, use: 'url-loader?limit=81920', exclude: /test[^\.]*\.(png|jpg)$/ },
   { test: /\.html$/, loader: 'html-loader' },
   { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-  { test: /\.mcss$/,
-    use: [
-      { loader: 'style-loader' },
-      { loader: 'css-loader', options: { localIdentName: '[sha512:hash:base32]-[name]-[local]', modules: true } },
-      { loader: 'postcss-loader', options: { plugins: () => [autoprefixer('last 3 version', 'ie >= 10')] } },
-    ],
-  },
   { test: /\.cjson$/, loader: 'hson-loader' },
   { test: /\.hson$/, loader: 'hson-loader' },
 ];
--- Utilities/config/rules-vtk.js
+++ Utilities/config/rules-vtk.js
@@ -1,8 +1,22 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = [
   { test: /\.glsl$/i, loader: 'shader-loader' },
   { test: /\.js$/,
     use: [
       { loader: 'babel-loader', options: { presets: ['es2015'] } },
+    ],
+  },
+  { test: /\.mcss$/,
+    use: [
+      { loader: 'style-loader' },
+      { loader: 'css-loader', options: { localIdentName: '[sha512:hash:base32:5]-[name]-[local]', modules: true } },
+      { loader: 'postcss-loader', options: { plugins: () => [autoprefixer('last 3 version', 'ie >= 10')] } },
+    ],
+  },
+  { test: /\.svg$/,
+    use: [
+      { loader: 'raw-loader' },
     ],
   },
 ];
