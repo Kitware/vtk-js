39ea40e97c28ed4e441307854e140d94fdebb44c
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-11-05 12:52:19 -0700

    fix(build): Icons path issue

--- Sources/Interaction/UI/Icons/index.js
+++ Sources/Interaction/UI/Icons/index.js
@@ -1,7 +1,7 @@
-import Contrast from './Contrast';
-import Logo from './Logo';
-import Spacing from './Spacing';
-import Tint from './Tint';
+import Contrast from './Contrast.svg';
+import Logo     from './Logo.svg';
+import Spacing  from './Spacing.svg';
+import Tint     from './Tint.svg';
 
 export default {
   Contrast,
