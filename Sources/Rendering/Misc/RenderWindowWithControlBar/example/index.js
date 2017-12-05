e60d0f98a5ba5f2caf4962084f5e4611ac406911
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-12-05 09:02:30 -0700

    fix(RenderWindowWithControlBar): RenderWindow with associated toolbar

---
+++ Sources/Rendering/Misc/RenderWindowWithControlBar/example/index.js
@@ -0,0 +1,82 @@
+import vtkRenderWindowWithControlBar from 'vtk.js/Sources/Rendering/Misc/RenderWindowWithControlBar';
+import vtkSlider                     from 'vtk.js/Sources/Interaction/UI/Slider';
+
+import vtkActor      from 'vtk.js/Sources/Rendering/Core/Actor';
+import vtkConeSource from 'vtk.js/Sources/Filters/Sources/ConeSource';
+import vtkMapper     from 'vtk.js/Sources/Rendering/Core/Mapper';
+
+// Define container size/position
+const body = document.querySelector('body');
+const rootContainer = document.createElement('div');
+rootContainer.style.position = 'relative';
+rootContainer.style.width = '500px';
+rootContainer.style.height = '500px';
+body.appendChild(rootContainer);
+
+// Create render window inside container
+const renderWindow = vtkRenderWindowWithControlBar.newInstance({ controlSize: 25 });
+renderWindow.setContainer(rootContainer);
+
+// Add some content to the renderer
+const coneSource = vtkConeSource.newInstance();
+const mapper = vtkMapper.newInstance();
+const actor = vtkActor.newInstance();
+
+mapper.setInputConnection(coneSource.getOutputPort());
+actor.setMapper(mapper);
+
+renderWindow.getRenderer().addActor(actor);
+renderWindow.getRenderer().resetCamera();
+renderWindow.getRenderWindow().render();
+
+// Set control bar to be red so we can see it + layout setup...
+renderWindow.getControlContainer().style.background = '#eee';
+renderWindow.getControlContainer().style.display = 'flex';
+renderWindow.getControlContainer().style.alignItems = 'stretch';
+renderWindow.getControlContainer().style.justifyContent = 'stretch';
+renderWindow.getControlContainer().innerHTML = `
+  <button alt="Left"   title="Left"   value="left">L</button>
+  <button alt="Top"    title="Top"    value="top">T</button>
+  <button alt="Right"  title="Right"  value="right">R</button>
+  <button alt="Bottom" title="Bottom" value="bottom">B</button>
+  <div class="js-slider"></div>
+`;
+
+// Add slider to the control bar
+const sliderContainer = renderWindow.getControlContainer().querySelector('.js-slider');
+sliderContainer.style.flex = '1';
+sliderContainer.style.position = 'relative';
+sliderContainer.style.minWidth = '25px';
+sliderContainer.style.minHeight = '25px';
+
+const slider = vtkSlider.newInstance();
+slider.generateValues(6, 60, 55);
+slider.setValue(6);
+slider.setContainer(sliderContainer);
+slider.onValueChange((resolution) => {
+  coneSource.set({ resolution });
+  renderWindow.getRenderWindow().render();
+});
+
+function updateSizeAndOrientation() {
+  renderWindow.resize();
+  slider.resize();
+  renderWindow.getControlContainer().style.flexFlow = slider.getOrientation() ? 'row' : 'column';
+  setTimeout(slider.resize, 0);
+}
+updateSizeAndOrientation();
+
+// Handle UI to change bar location
+function onClick(e) {
+  renderWindow.setControlPosition(e.target.value);
+  updateSizeAndOrientation();
+}
+
+const list = document.querySelectorAll('button');
+let count = list.length;
+while (count--) {
+  list[count].style.width = '25px';
+  list[count].style.height = '25px';
+  list[count].style.flex = 'none';
+  list[count].addEventListener('click', onClick);
+}
