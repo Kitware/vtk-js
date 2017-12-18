2eb93cca41de3e16aae9d0d586ac82a949f191b5
Forrest Li <forrest.li@kitware.com>
2017-12-18 12:21:13 -0500

    fix(OrientationMarkerWidget): Add OrientationMarkerWidget

---
+++ Sources/Interaction/Widgets/OrientationMarkerWidget/example/index.js
@@ -0,0 +1,54 @@
+import 'vtk.js/Sources/favicon';
+
+import vtkFullScreenRenderWindow  from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
+import vtkActor                   from 'vtk.js/Sources/Rendering/Core/Actor';
+import vtkConeSource              from 'vtk.js/Sources/Filters/Sources/ConeSource';
+import vtkMapper                  from 'vtk.js/Sources/Rendering/Core/Mapper';
+import vtkOrientationMarkerWidget from 'vtk.js/Sources/Interaction/Widgets/OrientationMarkerWidget';
+import vtkAnnotatedCubeActor      from 'vtk.js/Sources/Rendering/Core/AnnotatedCubeActor/index.js';
+
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
+
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({ background: [0.2, 0.2, 0.2] });
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
+
+// ----------------------------------------------------------------------------
+// Example code
+// ----------------------------------------------------------------------------
+
+// create cone
+const coneSource = vtkConeSource.newInstance();
+const actor = vtkActor.newInstance();
+const mapper = vtkMapper.newInstance();
+
+actor.setMapper(mapper);
+mapper.setInputConnection(coneSource.getOutputPort());
+
+renderer.addActor(actor);
+
+// create axes
+const axes = vtkAnnotatedCubeActor.newInstance();
+axes.setResolution(400);
+axes.setFontStyle('bold');
+axes.setFontColor('black');
+axes.setEdgeThickness(0.1);
+axes.setXPlusFaceProperty({ text: '+X', faceColor: '#ff0000' });
+axes.setXMinusFaceProperty({ text: '-X', faceColor: '#ffff00' });
+axes.setYPlusFaceProperty({ text: '+Y', faceColor: '#00ff00' });
+axes.setYMinusFaceProperty({ text: '-Y', faceColor: '#00ffff' });
+axes.setZPlusFaceProperty({ text: '+Z', faceColor: '#ff00ff' });
+axes.setZMinusFaceProperty({ text: '-Z', faceColor: '#0000ff' });
+
+// create orientation widget
+const orientationWidget = vtkOrientationMarkerWidget.newInstance({
+  actor: axes,
+  interactor: renderWindow.getInteractor(),
+});
+orientationWidget.setEnabled(true);
+orientationWidget.setViewportSize(0.15);
+
+renderer.resetCamera();
+renderWindow.render();
---
+++ Sources/Interaction/Widgets/OrientationMarkerWidget/index.js
@@ -0,0 +1,164 @@
+import macro       from 'vtk.js/Sources/macro';
+import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
+
+// ----------------------------------------------------------------------------
+// vtkOrientationMarkerWidget
+// ----------------------------------------------------------------------------
+
+const CORNERS = {
+  TOP_LEFT: 'TOP_LEFT',
+  TOP_RIGHT: 'TOP_RIGHT',
+  BOTTOM_LEFT: 'BOTTOM_LEFT',
+  BOTTOM_RIGHT: 'BOTTOM_RIGHT',
+};
+
+const VIEWPORTS = {
+  TOP_LEFT: size => [0, (1 - size), size, 1],
+  TOP_RIGHT: size => [(1 - size), (1 - size), 1, 1],
+  BOTTOM_LEFT: size => [0, 0, size, size],
+  BOTTOM_RIGHT: size => [(1 - size), 0, 1, size],
+};
+
+function vtkOrientationMarkerWidget(publicAPI, model) {
+  // Set our className
+  model.classHierarchy.push('vtkOrientationMarkerWidget');
+
+  // Private variables
+
+  const selfRenderer = vtkRenderer.newInstance();
+
+  // private methods
+
+  function updateMarkerOrientation() {
+    const currentCamera = model.interactor.findPokedRenderer().getActiveCamera();
+
+    if (!currentCamera) {
+      return;
+    }
+
+    // window.camera = currentCamera;
+    const state = currentCamera.get('position', 'focalPoint', 'viewUp');
+    selfRenderer.getActiveCamera().set(state);
+    selfRenderer.resetCamera();
+  }
+
+  function getViewport() {
+    return VIEWPORTS[model.viewportCorner](model.viewportSize);
+  }
+
+  // public methods
+
+  /**
+   * Enables/Disables the orientation marker.
+   */
+  publicAPI.setEnabled = (enabling) => {
+    if (enabling) {
+      if (model.enabled) {
+        return;
+      }
+
+      if (!model.actor) {
+        console.error('Must set actor before enabling orientation marker.');
+        return;
+      }
+
+      if (!model.interactor) {
+        console.error('Must set interactor before enabling orientation marker.');
+        return;
+      }
+
+      const renderWindow = model.interactor.findPokedRenderer().getRenderWindow();
+      renderWindow.addRenderer(selfRenderer);
+      if (renderWindow.getNumberOfLayers() < 2) {
+        renderWindow.setNumberOfLayers(2);
+      }
+      selfRenderer.setLayer(renderWindow.getNumberOfLayers() - 1);
+      selfRenderer.setInteractive(false);
+
+      selfRenderer.addViewProp(model.actor);
+      model.actor.setVisibility(true);
+
+      selfRenderer.setViewport(...getViewport());
+
+      model.interactor.onAnimation(updateMarkerOrientation);
+
+      model.enabled = true;
+    } else {
+      model.enabled = false;
+
+      // TODO interactor.offAnimation() or something similar
+
+      model.actor.setVisibility(false);
+      selfRenderer.removeViewProp(model.actor);
+
+      const renderWindow = model.interactor.findPokedRenderer().getRenderWindow();
+      if (renderWindow) {
+        renderWindow.removeRenderer(selfRenderer);
+      }
+    }
+  };
+
+  /**
+   * Sets the viewport corner.
+   */
+  publicAPI.setViewportCorner = (corner) => {
+    model.viewportCorner = corner;
+    if (model.enabled) {
+      selfRenderer.setViewport(...getViewport());
+    }
+  };
+
+  /**
+   * Sets the viewport size.
+   */
+  publicAPI.setViewportSize = (sizeFactor) => {
+    model.viewportSize = Math.min(1, Math.max(0, sizeFactor));
+    if (model.enabled) {
+      selfRenderer.setViewport(...getViewport());
+    }
+  };
+}
+
+// ----------------------------------------------------------------------------
+// Object factory
+// ----------------------------------------------------------------------------
+
+export const DEFAULT_VALUES = {
+  actor: null,
+  interactor: null,
+  viewportCorner: CORNERS.BOTTOM_LEFT,
+  viewportSize: 0.2,
+};
+
+// ----------------------------------------------------------------------------
+
+export function extend(publicAPI, model, initialValues = {}) {
+  Object.assign(model, DEFAULT_VALUES, initialValues);
+
+  // Build VTK API
+  macro.obj(publicAPI, model);
+
+  macro.get(publicAPI, model, [
+    'enabled',
+    'viewportCorner',
+    'viewportSize',
+  ]);
+
+  // NOTE: setting these while the widget is enabled will
+  // not update the widget.
+  macro.setGet(publicAPI, model, [
+    'actor',
+    'interactor',
+  ]);
+
+  // Object methods
+  vtkOrientationMarkerWidget(publicAPI, model);
+}
+
+// ----------------------------------------------------------------------------
+
+export const newInstance = macro.newInstance(extend, 'vtkOrientationMarkerWidget');
+
+// ----------------------------------------------------------------------------
+
+export default Object.assign({ newInstance, extend }, CORNERS);
