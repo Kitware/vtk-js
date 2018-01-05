a99d9780dee7c4257c4474b53d9be3df980ea104
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-01-05 11:59:30 -0700

    chore(conf): Update config format

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-   printWidth: 80,
-   singleQuote: true,
-   trailingComma: 'es5',
-   arrowParens: 'always',
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
 };
