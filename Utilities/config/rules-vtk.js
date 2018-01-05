47a9a963d025c9cfc96e8ce52bfa33200cf86280
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-01-05 08:40:36 -0700

    build(travis): Try to remove canvas dep

--- Utilities/config/rules-vtk.js
+++ Utilities/config/rules-vtk.js
@@ -2,21 +2,39 @@ const autoprefixer = require('autoprefixer');
 
 module.exports = [
   { test: /\.glsl$/i, loader: 'shader-loader' },
-  { test: /\.js$/,
+  {
+    test: /\.js$/,
     use: [
-      { loader: 'babel-loader', options: { presets: ['env'] } },
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          // presets: [['env', { targets: { browsers: 'last 2 versions' } }]],
+        },
+      },
     ],
   },
-  { test: /\.mcss$/,
+  {
+    test: /\.mcss$/,
     use: [
       { loader: 'style-loader' },
-      { loader: 'css-loader', options: { localIdentName: '[name]-[local]_[sha512:hash:base32:5]', modules: true } },
-      { loader: 'postcss-loader', options: { plugins: () => [autoprefixer('last 2 version', 'ie >= 10')] } },
+      {
+        loader: 'css-loader',
+        options: {
+          localIdentName: '[name]-[local]_[sha512:hash:base32:5]',
+          modules: true,
+        },
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: () => [autoprefixer('last 2 version', 'ie >= 10')],
+        },
+      },
     ],
   },
-  { test: /\.svg$/,
-    use: [
-      { loader: 'raw-loader' },
-    ],
+  {
+    test: /\.svg$/,
+    use: [{ loader: 'raw-loader' }],
   },
 ];
