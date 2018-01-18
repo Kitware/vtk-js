f0debdec3d2d4d7bf79d4fb0cfbd6e22385e37d7
Alexis Girault <alexis.girault@kitware.com>
2018-01-18 16:20:03 -0500

    doc(RangeManipulator): add example for RangeManipulator

---
+++ Sources/Interaction/Manipulators/RangeManipulator/example/index.js
@@ -0,0 +1,81 @@
+import 'vtk.js/Sources/favicon';
+
+import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
+import vtkRTAnalyticSource from 'vtk.js/Sources/Filters/Sources/RTAnalyticSource';
+import vtkImageMapper from 'vtk.js/Sources/Rendering/Core/ImageMapper';
+import vtkImageSlice from 'vtk.js/Sources/Rendering/Core/ImageSlice';
+import vtkInteractorStyleManipulator from 'vtk.js/Sources/Interaction/Style/InteractorStyleManipulator';
+
+import Manipulators from 'vtk.js/Sources/Interaction/Manipulators';
+
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
+
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
+
+// ----------------------------------------------------------------------------
+// Example code
+// ----------------------------------------------------------------------------
+
+const rtSource = vtkRTAnalyticSource.newInstance();
+rtSource.setWholeExtent(0, 200, 0, 200, 0, 200);
+rtSource.setCenter(100, 100, 100);
+rtSource.setStandardDeviation(0.3);
+
+const mapper = vtkImageMapper.newInstance();
+mapper.setInputConnection(rtSource.getOutputPort());
+
+const actor = vtkImageSlice.newInstance();
+actor.getProperty().setColorWindow(100);
+actor.getProperty().setColorLevel(50);
+actor.setMapper(mapper);
+
+const data = rtSource.getOutputData();
+const range = data
+  .getPointData()
+  .getScalars()
+  .getRange();
+const wMin = 1;
+const wMax = range[1] - range[0];
+const wCurrent = actor.getProperty().getColorWindow();
+const wCallback = actor.getProperty().setColorWindow;
+const lMin = range[0];
+const lMax = range[1];
+const lCurrent = actor.getProperty().getColorLevel();
+const lCallback = actor.getProperty().setColorLevel;
+const bounds = data.getBounds();
+const zMin = bounds[4];
+const zMax = bounds[5];
+const zCurrent = mapper.getZSlice();
+const zCallback = mapper.setZSlice;
+
+const rangeManipulator = Manipulators.vtkRangeManipulator.newInstance({
+  button: 1,
+  pinch: true,
+});
+rangeManipulator.setVerticalListener(wMin, wMax, wCurrent, 1, wCallback);
+rangeManipulator.setHorizontalListener(lMin, lMax, lCurrent, 1, lCallback);
+rangeManipulator.setScrollListener(zMin, zMax, zCurrent, 1, zCallback);
+
+const iStyle = vtkInteractorStyleManipulator.newInstance();
+iStyle.addManipulator(rangeManipulator);
+renderWindow.getInteractor().setInteractorStyle(iStyle);
+
+renderer.getActiveCamera().setParallelProjection(true);
+renderer.addActor(actor);
+renderer.resetCamera();
+renderWindow.render();
+
+// -----------------------------------------------------------
+// Make some variables global so that you can inspect and
+// modify objects in your browser's developer console:
+// -----------------------------------------------------------
+
+global.source = rtSource;
+global.mapper = mapper;
+global.actor = actor;
+global.renderer = renderer;
+global.renderWindow = renderWindow;
