59fa9a4ab50d97e08d5d4161b3c18687507073df
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-02-09 11:52:29 -0700

    chore(FPSMonitor): Add FPSMonitor to bundle

--- Sources/Interaction/UI/index.js
+++ Sources/Interaction/UI/index.js
@@ -1,10 +1,12 @@
 import vtkCornerAnnotation from './CornerAnnotation';
+import vtkFPSMonitor from './FPSMonitor';
 import vtkIcons from './Icons';
 import vtkSlider from './Slider';
 import vtkVolumeController from './VolumeController';
 
 export default {
   vtkCornerAnnotation,
+  vtkFPSMonitor,
   vtkIcons,
   vtkSlider,
   vtkVolumeController,
