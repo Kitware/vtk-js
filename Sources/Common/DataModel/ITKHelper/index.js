fcdae198aac39cf2d9deaae58bd4685b0b6e9f2b
Forrest Li <forrest.li@kitware.com>
2018-02-12 15:37:03 -0500

    fix(ITKHelper): Add ITK to VTK js image converter

---
+++ Sources/Common/DataModel/ITKHelper/index.js
@@ -0,0 +1,30 @@
+import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
+import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+
+/**
+ * Converts an itk.js image to a vtk.js image.
+ *
+ * Requires an itk.js image as input.
+ */
+function convertItkToVtkImage(itkImage) {
+  // create VTK image data
+  const imageData = vtkImageData.newInstance({
+    origin: itkImage.origin.slice(),
+    spacing: itkImage.spacing.slice(),
+  });
+  const scalars = vtkDataArray.newInstance({
+    name: 'Scalars',
+    values: itkImage.data,
+    numberOfComponents: itkImage.imageType.components,
+  });
+
+  imageData.setDirection(itkImage.direction.data);
+  imageData.setDimensions(...itkImage.size);
+  imageData.getPointData().setScalars(scalars);
+
+  return imageData;
+}
+
+export default {
+  convertItkToVtkImage,
+};
