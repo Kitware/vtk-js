673f359209ced25dc89c8c5c7e20d71e21868c05
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-02-13 15:21:40 -0700

    fix(GlyphRepresentationProxy): Add glyph representation proxy

--- Sources/Proxy/Representations/index.js
+++ Sources/Proxy/Representations/index.js
@@ -1,10 +1,12 @@
 import vtkGeometryRepresentationProxy from './GeometryRepresentationProxy';
+import vtkGlyphRepresentationProxy from './GlyphRepresentationProxy';
 import vtkMoleculeRepresentationProxy from './MoleculeRepresentationProxy';
 import vtkSliceRepresentationProxy from './SliceRepresentationProxy';
 import vtkVolumeRepresentationProxy from './VolumeRepresentationProxy';
 
 export default {
   vtkGeometryRepresentationProxy,
+  vtkGlyphRepresentationProxy,
   vtkMoleculeRepresentationProxy,
   vtkSliceRepresentationProxy,
   vtkVolumeRepresentationProxy,
