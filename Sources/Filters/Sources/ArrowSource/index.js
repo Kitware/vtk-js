e6ab4b538f35cfc9371c222ef75cdf5beca8b0ca
Patrick O'Leary <patrick.oleary@kitware.com>
2018-02-16 14:30:38 -0700

    fix(ArrowSource): Add ArrowSource direction

--- Sources/Filters/Sources/ArrowSource/index.js
+++ Sources/Filters/Sources/ArrowSource/index.js
@@ -55,19 +55,26 @@ function vtkArrowSource(publicAPI, model) {
     append.setInputData(cylinderPD);
     append.addInputData(conePD);
 
+    const appendPD = append.getOutputData();
+    const appendPts = appendPD.getPoints().getData();
     if (model.invert) {
-      const appendPD = append.getOutputData();
-      const appendPts = appendPD.getPoints().getData();
       // Apply transformation to the arrow
       vtkMatrixBuilder
-        .buildFromDegree()
+        .buildFromRadian()
         .translate(1, 0, 0)
-        .scale(-1, 1, 1)
+        .rotateFromDirections([1, 0, 0], model.direction)
+        .scale(-1, -1, -1)
         .apply(appendPts);
 
       // Update output
       outData[0] = appendPD;
     } else {
+      // Apply transformation to the arrow
+      vtkMatrixBuilder
+        .buildFromRadian()
+        .rotateFromDirections([1, 0, 0], model.direction)
+        .apply(appendPts);
+
       // Update output
       outData[0] = append.getOutputData();
     }
@@ -88,6 +95,7 @@ const DEFAULT_VALUES = {
   shaftResolution: 6,
   shaftRadius: 0.03,
   invert: false,
+  direction: [1.0, 0.0, 0.0],
   pointType: 'Float32Array',
 };
 
@@ -106,6 +114,7 @@ export function extend(publicAPI, model, initialValues = {}) {
     'shaftRadius',
     'invert',
   ]);
+  macro.setGetArray(publicAPI, model, ['direction'], 3);
   macro.algo(publicAPI, model, 0, 1);
   vtkArrowSource(publicAPI, model);
 }
