4df655c6fda1dbe845e42482507a049aabfa3623
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-03-13 15:30:10 -0600

    fix(Cornerstone): Add module to handle cornerstone interoperability

--- Sources/Filters/index.js
+++ Sources/Filters/index.js
@@ -1,8 +1,10 @@
+import Cornerstone from './Cornerstone';
 import General from './General';
 import Sources from './Sources';
 import Texture from './Texture';
 
 export default {
+  Cornerstone,
   General,
   Sources,
   Texture,
