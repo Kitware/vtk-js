c9d61853d9882bd045d2c1e6a939b9adbe77bc3b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-03-27 08:52:07 -0600

    fix(ITKImageReader): Expose ITK reader

--- Sources/IO/Misc/index.js
+++ Sources/IO/Misc/index.js
@@ -1,13 +1,17 @@
 import vtkElevationReader from './ElevationReader';
+import vtkITKImageReader from './ITKImageReader';
 import vtkJSONReader from './JSONReader';
 import vtkMTLReader from './MTLReader';
 import vtkOBJReader from './OBJReader';
 import vtkPDBReader from './PDBReader';
+import vtkSkyboxReader from './SkyboxReader';
 
 export default {
   vtkElevationReader,
+  vtkITKImageReader,
   vtkJSONReader,
   vtkMTLReader,
   vtkOBJReader,
   vtkPDBReader,
+  vtkSkyboxReader,
 };
