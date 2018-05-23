27444f1195dd0beb55030cdf8a01e0fa2d13e4f0
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-05-22 20:38:20 -0600

    fix(JSONNucleoReader): Add new reader

--- Sources/IO/Misc/index.js
+++ Sources/IO/Misc/index.js
@@ -1,5 +1,6 @@
 import vtkElevationReader from './ElevationReader';
 import vtkITKImageReader from './ITKImageReader';
+import vtkJSONNucleoReader from './JSONNucleoReader';
 import vtkJSONReader from './JSONReader';
 import vtkMTLReader from './MTLReader';
 import vtkOBJReader from './OBJReader';
@@ -9,6 +10,7 @@ import vtkSkyboxReader from './SkyboxReader';
 export default {
   vtkElevationReader,
   vtkITKImageReader,
+  vtkJSONNucleoReader,
   vtkJSONReader,
   vtkMTLReader,
   vtkOBJReader,
