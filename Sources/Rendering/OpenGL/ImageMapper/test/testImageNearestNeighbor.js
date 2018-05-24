af80ec37761b1e063ae80d4045a4afda25167558
Matt McCormick <matt.mccormick@kitware.com>
2018-05-24 11:34:26 -0400

    style(prettier): Run prettier on testImageNearestNeighbor.js

--- Sources/Rendering/OpenGL/ImageMapper/test/testImageNearestNeighbor.js
+++ Sources/Rendering/OpenGL/ImageMapper/test/testImageNearestNeighbor.js
@@ -10,64 +10,67 @@ import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
 
 import baseline from './testImageNearestNeighbor.png';
 
-test.onlyIfWebGL('Test ImageMapper with Nearest Neighbor interpolation', (t) => {
-  const gc = testUtils.createGarbageCollector(t);
-  t.ok('rendering', 'vtkOpenGLImageMapper testImage');
+test.onlyIfWebGL(
+  'Test ImageMapper with Nearest Neighbor interpolation',
+  (t) => {
+    const gc = testUtils.createGarbageCollector(t);
+    t.ok('rendering', 'vtkOpenGLImageMapper testImage');
 
-  // Create some control UI
-  const container = document.querySelector('body');
-  const renderWindowContainer = gc.registerDOMElement(
-    document.createElement('div')
-  );
-  container.appendChild(renderWindowContainer);
+    // Create some control UI
+    const container = document.querySelector('body');
+    const renderWindowContainer = gc.registerDOMElement(
+      document.createElement('div')
+    );
+    container.appendChild(renderWindowContainer);
 
-  // create what we will view
-  const renderWindow = gc.registerResource(vtkRenderWindow.newInstance());
-  const renderer = gc.registerResource(vtkRenderer.newInstance());
-  renderWindow.addRenderer(renderer);
-  renderer.setBackground(0.32, 0.34, 0.43);
+    // create what we will view
+    const renderWindow = gc.registerResource(vtkRenderWindow.newInstance());
+    const renderer = gc.registerResource(vtkRenderer.newInstance());
+    renderWindow.addRenderer(renderer);
+    renderer.setBackground(0.32, 0.34, 0.43);
 
-  // ----------------------------------------------------------------------------
-  // Test code
-  // ----------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------
+    // Test code
+    // ----------------------------------------------------------------------------
 
-  const gridSource = gc.registerResource(vtkImageGridSource.newInstance());
-  gridSource.setDataExtent(0, 200, 0, 200, 0, 0);
-  gridSource.setGridSpacing(16, 16, 0);
-  gridSource.setGridOrigin(8, 8, 0);
-  gridSource.setDataDirection(0.866, 0.5, 0, -0.5, 0.866, 0, 0, 0, 1);
+    const gridSource = gc.registerResource(vtkImageGridSource.newInstance());
+    gridSource.setDataExtent(0, 200, 0, 200, 0, 0);
+    gridSource.setGridSpacing(16, 16, 0);
+    gridSource.setGridOrigin(8, 8, 0);
+    gridSource.setDataDirection(0.866, 0.5, 0, -0.5, 0.866, 0, 0, 0, 1);
 
-  const mapper = gc.registerResource(vtkImageMapper.newInstance());
-  mapper.setInputConnection(gridSource.getOutputPort());
+    const mapper = gc.registerResource(vtkImageMapper.newInstance());
+    mapper.setInputConnection(gridSource.getOutputPort());
 
-  const actor = gc.registerResource(vtkImageSlice.newInstance());
-  actor.getProperty().setColorWindow(255);
-  actor.getProperty().setColorLevel(127);
-  actor.getProperty().setInterpolationTypeToNearest();
-  actor.setMapper(mapper);
+    const actor = gc.registerResource(vtkImageSlice.newInstance());
+    actor.getProperty().setColorWindow(255);
+    actor.getProperty().setColorLevel(127);
+    actor.getProperty().setInterpolationTypeToNearest();
+    actor.setMapper(mapper);
 
-  renderer.addActor(actor);
-  renderer.resetCamera();
-  renderWindow.render();
+    renderer.addActor(actor);
+    renderer.resetCamera();
+    renderWindow.render();
 
-  // -----------------------------------------------------------
-  // Make some variables global so that you can inspect and
-  // modify objects in your browser's developer console:
-  // -----------------------------------------------------------
+    // -----------------------------------------------------------
+    // Make some variables global so that you can inspect and
+    // modify objects in your browser's developer console:
+    // -----------------------------------------------------------
 
-  // create something to view it, in this case webgl
-  const glwindow = gc.registerResource(vtkOpenGLRenderWindow.newInstance());
-  glwindow.setContainer(renderWindowContainer);
-  renderWindow.addView(glwindow);
-  glwindow.setSize(400, 400);
+    // create something to view it, in this case webgl
+    const glwindow = gc.registerResource(vtkOpenGLRenderWindow.newInstance());
+    glwindow.setContainer(renderWindowContainer);
+    renderWindow.addView(glwindow);
+    glwindow.setSize(400, 400);
 
-  const image = glwindow.captureImage();
-  testUtils.compareImages(
-    image,
-    [baseline],
-    'Rendering/OpenGL/ImageMapperNearestNeighbor',
-    t,
-    1,
-    gc.releaseResources
-  );
-});
+    const image = glwindow.captureImage();
+    testUtils.compareImages(
+      image,
+      [baseline],
+      'Rendering/OpenGL/ImageMapperNearestNeighbor',
+      t,
+      1,
+      gc.releaseResources
+    );
+  }
+);
