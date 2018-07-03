a9fd77c6c7e677188ecc23e2bc75d733f74ed303
Max Smolens <max.smolens@kitware.com>
2018-07-03 15:17:41 -0400

    refactor(Prop): Simplify hasTexture() implementation

--- Sources/Rendering/Core/Prop/index.js
+++ Sources/Rendering/Core/Prop/index.js
@@ -55,8 +55,7 @@ function vtkProp(publicAPI, model) {
   publicAPI.getSupportsSelection = () => false;
 
   publicAPI.getTextures = () => model.textures;
-  publicAPI.hasTexture = (texture) =>
-    !!model.textures.filter((item) => item === texture).length;
+  publicAPI.hasTexture = (texture) => model.textures.indexOf(texture) !== -1;
   publicAPI.addTexture = (texture) => {
     if (texture && !publicAPI.hasTexture(texture)) {
       model.textures = model.textures.concat(texture);
