8cf6e67babee2ecced39010bcd1f31da35a0a07a
Guilhem Lavaux <lavaux@iap.fr>
2018-07-29 09:39:34 +0300

    fix(Glyph3DMapper): Fix angle units for rotation. Add orientationMode to the model.

--- Sources/Rendering/Core/Glyph3DMapper/index.js
+++ Sources/Rendering/Core/Glyph3DMapper/index.js
@@ -144,9 +144,9 @@ function vtkGlyph3DMapper(publicAPI, model) {
           oArray.getTuple(i, orientation);
           switch (model.orientationMode) {
             case OrientationModes.ROTATION:
-              mat4.rotateZ(z, z, orientation[2] / 3.1415926);
-              mat4.rotateX(z, z, orientation[0] / 3.1415926);
-              mat4.rotateY(z, z, orientation[1] / 3.1415926);
+              mat4.rotateZ(z, z, orientation[2]);
+              mat4.rotateX(z, z, orientation[0]);
+              mat4.rotateY(z, z, orientation[1]);
               break;
 
             case OrientationModes.DIRECTION:
@@ -326,6 +326,7 @@ export function extend(publicAPI, model, initialValues = {}) {
 
   macro.setGet(publicAPI, model, [
     'orient',
+    'orientationMode',
     'orientationArray',
     'scaleArray',
     'scaleFactor',
