92ba6447c092e4d49d2447d6053176fc4eb0fd91
Forrest Li <forrest.li@kitware.com>
2018-08-21 18:10:13 -0400

    Revert using compose

--- Sources/Interaction/Widgets2/SphereState/index.js
+++ Sources/Interaction/Widgets2/SphereState/index.js
@@ -2,24 +2,6 @@ import macro from 'vtk.js/Sources/macro';
 
 // ----------------------------------------------------------------------------
 
-// TODO move to macro
-function prefixName(name, prefix = '') {
-  return prefix ? `${prefix}${macro.capitalize(name)}` : name;
-}
-
-// ----------------------------------------------------------------------------
-
-// TODO move to macro
-function prefixObject(object, prefix = '') {
-  const renamedObj = {};
-  Object.keys(object).forEach((name) => {
-    renamedObj[prefixName(name, prefix)] = object[name];
-  });
-  return renamedObj;
-}
-
-// ----------------------------------------------------------------------------
-
 function vtkSphereState(publicAPI, model) {
   model.classHierarchy.push('vtkSphereState');
 
@@ -40,24 +22,12 @@ const DEFAULT_VALUES = {
 
 // ----------------------------------------------------------------------------
 
-export function compose(
-  publicAPI,
-  model,
-  prefix = '',
-  defaultValues = DEFAULT_VALUES
-) {
-  Object.assign(model, prefixObject(defaultValues, prefix));
-  macro.setGet(publicAPI, model, [prefixName('radius', prefix)]);
-  macro.setGetArray(publicAPI, model, [prefixName('position', prefix)], 3);
-}
-
-// ----------------------------------------------------------------------------
-
 export function extend(publicAPI, model, initialValues = {}) {
   Object.assign(model, DEFAULT_VALUES, initialValues);
 
   macro.obj(publicAPI, model);
-  compose(publicAPI, model);
+  macro.setGet(publicAPI, model, ['radius']);
+  macro.setGetArray(publicAPI, model, ['position'], 3);
 
   vtkSphereState(publicAPI, model);
 }
