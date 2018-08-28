278951c68e21dcb6ddfa3af6cc26ed2e42b0690b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-08-28 17:58:39 -0400

    chore(build): Fix index

--- Sources/Widgets/Widgets3D/index.js
+++ Sources/Widgets/Widgets3D/index.js
@@ -1,11 +1,7 @@
-import vtkBoxWidget from './BoxWidget';
-import vtkHandleWidget2 from './HandleWidget2';
 import vtkImplicitPlaneWidget from './ImplicitPlaneWidget';
 import vtkPolyLineWidget from './PolyLineWidget';
 
 export default {
-  vtkBoxWidget,
-  vtkHandleWidget2,
   vtkImplicitPlaneWidget,
   vtkPolyLineWidget,
 };
