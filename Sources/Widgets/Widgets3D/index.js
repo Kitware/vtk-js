5c9bcdedf5e967cab04465a7e137283dd1995398
Steve Pieper <pieper@isomics.com>
2018-09-20 15:37:58 -0400

    fix(widgets): add exports for PaintWidget

--- Sources/Widgets/Widgets3D/index.js
+++ Sources/Widgets/Widgets3D/index.js
@@ -1,7 +1,9 @@
 import vtkImplicitPlaneWidget from './ImplicitPlaneWidget';
+import vtkPaintWidget from './PaintWidget';
 import vtkPolyLineWidget from './PolyLineWidget';
 
 export default {
   vtkImplicitPlaneWidget,
+  vtkPaintWidget,
   vtkPolyLineWidget,
 };
