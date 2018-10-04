4ba85c0dbfbfee5ebe4f3962d378ade6f19d2730
Brian Helba <brian.helba@kitware.com>
2018-10-04 18:18:16 -0400

    fix: 🐛 Export constants from LookupTableProxy
    This matches what PiecewiseFunctionProxy does.

--- Sources/Proxy/Core/LookupTableProxy/index.js
+++ Sources/Proxy/Core/LookupTableProxy/index.js
@@ -157,4 +157,4 @@ export const newInstance = macro.newInstance(extend, 'vtkLookupTableProxy');
 
 // ----------------------------------------------------------------------------
 
-export default { newInstance, extend };
+export default { newInstance, extend, Mode, Defaults };
