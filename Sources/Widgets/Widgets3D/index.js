ad9a9a9b7678885b4ef8804fda59ac8d61ab16db
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-10-09 16:12:54 -0600

    chore(InteractiveOrientationWidget): Register inside module

--- Sources/Widgets/Widgets3D/index.js
+++ Sources/Widgets/Widgets3D/index.js
@@ -1,9 +1,11 @@
 import vtkImplicitPlaneWidget from './ImplicitPlaneWidget';
+import vtkInteractiveOrientationWidget from './InteractiveOrientationWidget';
 import vtkPaintWidget from './PaintWidget';
 import vtkPolyLineWidget from './PolyLineWidget';
 
 export default {
   vtkImplicitPlaneWidget,
+  vtkInteractiveOrientationWidget,
   vtkPaintWidget,
   vtkPolyLineWidget,
 };
