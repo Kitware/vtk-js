b6601840d2b434cd671fabb602df7fa0eaa53b82
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-10-22 08:43:26 -0600

    fix(XMLPolyDataReader): Register class in the vtk.js bundle
    fix #923

--- Sources/IO/XML/index.js
+++ Sources/IO/XML/index.js
@@ -1,5 +1,9 @@
 import vtkXMLImageDataReader from './XMLImageDataReader';
+import vtkXMLPolyDataReader from './XMLPolyDataReader';
+import vtkXMLReader from './XMLReader';
 
 export default {
   vtkXMLImageDataReader,
+  vtkXMLPolyDataReader,
+  vtkXMLReader,
 };
