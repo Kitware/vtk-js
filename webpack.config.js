eacdce57371d68315cf1a0af26cf086c55056abe
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-10-26 17:27:25 -0600

    chore(webpack): Test out webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,35 @@ const outputPath = path.join(__dirname, './dist');
 const vtkRules = require('./Utilities/config/rules-vtk.js');
 const linterRules = require('./Utilities/config/rules-linter.js');
 
+const plugins = [];
+
+// ----------------------------------------------------------------------------
+// Notifier
+// => Uncomment if you want notification when error or done
+// ----------------------------------------------------------------------------
+// const WebpackNotifierPlugin = require('webpack-notifier');
+// plugins.push(
+//   new WebpackNotifierPlugin({
+//     title: 'Webpack',
+//     excludeWarnings: true,
+//     alwaysNotify: true,
+//   })
+// );
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// Only for dev
+// => Nice dashboard when dev
+// ----------------------------------------------------------------------------
+// const Dashboard = require('webpack-dashboard');
+// const DashboardPlugin = require('webpack-dashboard/plugin');
+// const dashboard = new Dashboard();
+// plugins.push(new DashboardPlugin(dashboard.setData));
+// ----------------------------------------------------------------------------
+
 module.exports = {
   entry,
+  plugins,
   output: {
     path: outputPath,
     filename: 'vtk.js',
