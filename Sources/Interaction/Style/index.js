d8b1537fdbfc1c2ed216a03b42b8c1caaccabe29
Forrest Li <forrest.li@kitware.com>
2018-12-04 15:25:08 -0500

    feat(InteractorStyleMPRSlice): Add MPR slicing interactor style

--- Sources/Interaction/Style/index.js
+++ Sources/Interaction/Style/index.js
@@ -1,9 +1,11 @@
 import vtkInteractorStyleImage from './InteractorStyleImage';
 import vtkInteractorStyleManipulator from './InteractorStyleManipulator';
 import vtkInteractorStyleTrackballCamera from './InteractorStyleTrackballCamera';
+import vtkInteractorStyleMPRSlice from './InteractorStyleMPRSlice';
 
 export default {
   vtkInteractorStyleImage,
   vtkInteractorStyleManipulator,
   vtkInteractorStyleTrackballCamera,
+  vtkInteractorStyleMPRSlice,
 };
