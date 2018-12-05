80a946563b0cd2808ecbb545688212fb192ab7a8
gundam <jiejin2022@163.com>
2018-12-05 14:15:22 +0800

    feat(Cursor3D): Cursor3D is an object that generate a 3D representation of a cursor.
    This cursor3d is the exactly implementation of vtkCursor3D in c++, which is a javascript version.

--- Sources/Filters/Sources/index.js
+++ Sources/Filters/Sources/index.js
@@ -8,6 +8,7 @@ import vtkPointSource from './PointSource';
 import vtkRTAnalyticSource from './RTAnalyticSource';
 import vtkSLICSource from './SLICSource';
 import vtkSphereSource from './SphereSource';
+import vtkCursor3D from './Cursor3D';
 
 export default {
   vtkConcentricCylinderSource,
@@ -20,4 +21,5 @@ export default {
   vtkRTAnalyticSource,
   vtkSLICSource,
   vtkSphereSource,
+  vtkCursor3D,
 };
