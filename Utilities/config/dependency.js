Kitware/vtk-js

commit 5f2439e03981bc00ef5f3c14950782c646119951

fix: Scope build rules to source tree
Fixes #993

I tested this on the geode repo and this alleviated the need for the babel.config workaround (namely `ignore: [/\/core-js/]`).

Utilities/config/dependency.js
@@ -6,12 +6,12 @@ module.exports = {
       rules: [
         {
           test: /\.glsl$/i,
-          include: /vtk\.js/,
+          include: /vtk\.js\/Sources/,
           loader: 'shader-loader',
         },
         {
           test: /\.js$/,
-          include: /vtk\.js/,
+          include: /vtk\.js\/Sources/,
           use: [
             {
               loader: 'babel-loader',
@@ -23,7 +23,7 @@ module.exports = {
         },
         {
           test: /\.worker\.js$/,
-          include: /vtk\.js/,
+          include: /vtk\.js\/Sources/,
           use: [
             {
               loader: 'worker-loader',

commit a2183d6c920d5e7167d43a45d14f06733d7f094f

fix(DependencyWebpackRules): vtk.js source identification on Windows
This addresses a regression from
5f2439e03981bc00ef5f3c14950782c646119951
where sources are ignored for required Webpack loaders on Windows.

Utilities/config/dependency.js
@@ -6,12 +6,12 @@ module.exports = {
       rules: [
         {
           test: /\.glsl$/i,
-          include: /vtk\.js\/Sources/,
+          include: /vtk\.js[\/\\]Sources/,
           loader: 'shader-loader',
         },
         {
           test: /\.js$/,
-          include: /vtk\.js\/Sources/,
+          include: /vtk\.js[\/\\]Sources/,
           use: [
             {
               loader: 'babel-loader',
@@ -23,7 +23,7 @@ module.exports = {
         },
         {
           test: /\.worker\.js$/,
-          include: /vtk\.js\/Sources/,
+          include: /vtk\.js[\/\\]Sources/,
           use: [
             {
               loader: 'worker-loader',