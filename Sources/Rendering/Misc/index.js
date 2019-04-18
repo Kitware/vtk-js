9ed3aa41361d1f34d097b9b68814343ad48ef7fc
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2019-04-18 13:35:29 -0600

    fix(RemoteView): Add vtkRemoteView helper

--- Sources/Rendering/Misc/index.js
+++ Sources/Rendering/Misc/index.js
@@ -1,6 +1,7 @@
 import vtkCanvasView from './CanvasView';
 import vtkFullScreenRenderWindow from './FullScreenRenderWindow';
 import vtkGenericRenderWindow from './GenericRenderWindow';
+import vtkRemoveView from './RemoteView';
 import vtkRenderWindowWithControlBar from './RenderWindowWithControlBar';
 import vtkSynchronizableRenderWindow from './SynchronizableRenderWindow';
 
@@ -8,6 +9,7 @@ export default {
   vtkCanvasView,
   vtkFullScreenRenderWindow,
   vtkGenericRenderWindow,
+  vtkRemoveView,
   vtkRenderWindowWithControlBar,
   vtkSynchronizableRenderWindow,
 };
