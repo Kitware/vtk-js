73c4d9ec275575670aa25ade65b53a8ad93605bf
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2019-04-18 11:04:04 -0600

    fix(CanvasView): Add view that can be used for remote rendering without local GL

--- Sources/Rendering/Misc/index.js
+++ Sources/Rendering/Misc/index.js
@@ -1,9 +1,11 @@
+import vtkCanvasView from './CanvasView';
 import vtkFullScreenRenderWindow from './FullScreenRenderWindow';
 import vtkGenericRenderWindow from './GenericRenderWindow';
 import vtkRenderWindowWithControlBar from './RenderWindowWithControlBar';
 import vtkSynchronizableRenderWindow from './SynchronizableRenderWindow';
 
 export default {
+  vtkCanvasView,
   vtkFullScreenRenderWindow,
   vtkGenericRenderWindow,
   vtkRenderWindowWithControlBar,
