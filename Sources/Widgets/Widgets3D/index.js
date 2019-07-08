9ce6c99b833a1b81bb61aa4b3b03cf687775d14b
Remi <remi.cecchinato@kitware.com>
2019-07-08 14:49:52 +0200

    fix(Widgets3D): Add vtkRectangleWidget and vtkEllipseWidget to Widgets3D/index.js

--- Sources/Widgets/Widgets3D/index.js
+++ Sources/Widgets/Widgets3D/index.js
@@ -1,13 +1,17 @@
+import vtkEllipseWidget from './EllipseWidget';
 import vtkImageCroppingWidget from './ImageCroppingWidget';
 import vtkImplicitPlaneWidget from './ImplicitPlaneWidget';
 import vtkInteractiveOrientationWidget from './InteractiveOrientationWidget';
 import vtkPaintWidget from './PaintWidget';
 import vtkPolyLineWidget from './PolyLineWidget';
+import vtkRectangleWidget from './RectangleWidget';
 
 export default {
+  vtkEllipseWidget,
   vtkImageCroppingWidget,
   vtkImplicitPlaneWidget,
   vtkInteractiveOrientationWidget,
   vtkPaintWidget,
   vtkPolyLineWidget,
+  vtkRectangleWidget,
 };
