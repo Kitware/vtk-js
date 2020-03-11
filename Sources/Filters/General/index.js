afce05a44ee9f8241d6e62a676ebeac9db88e2dd
Forrest Li <forrest.li@kitware.com>
2020-03-11 09:49:37 -0400

    fix(Filters): Correctly export two filters

--- Sources/Filters/General/index.js
+++ Sources/Filters/General/index.js
@@ -4,6 +4,7 @@ import vtkClosedPolyLineToSurfaceFilter from './ClosedPolyLineToSurfaceFilter';
 import vtkImageCropFilter from './ImageCropFilter';
 import vtkImageMarchingCubes from './ImageMarchingCubes';
 import vtkImageMarchingSquares from './ImageMarchingSquares';
+import vtkImageOutlineFilter from './ImageOutlineFilter';
 import vtkImageSliceFilter from './ImageSliceFilter';
 import vtkImageStreamline from './ImageStreamline';
 import vtkMoleculeToRepresentation from './MoleculeToRepresentation';
@@ -12,6 +13,7 @@ import vtkPaintFilter from './PaintFilter';
 import vtkScalarToRGBA from './ScalarToRGBA';
 import vtkTubeFilter from './TubeFilter';
 import vtkWarpScalar from './WarpScalar';
+import vtkWindowedSincPolyDataFilter from './WindowedSincPolyDataFilter';
 
 export default {
   vtkAppendPolyData,
@@ -20,6 +22,7 @@ export default {
   vtkImageCropFilter,
   vtkImageMarchingCubes,
   vtkImageMarchingSquares,
+  vtkImageOutlineFilter,
   vtkImageSliceFilter,
   vtkImageStreamline,
   vtkMoleculeToRepresentation,
@@ -28,4 +31,5 @@ export default {
   vtkScalarToRGBA,
   vtkTubeFilter,
   vtkWarpScalar,
+  vtkWindowedSincPolyDataFilter,
 };
