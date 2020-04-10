3d1917e9c8b44ecf3a9d5770e3f8da820be1c0fb
Scott Wittenburg <scott.wittenburg@kitware.com>
2020-04-10 17:25:59 -0600

    fix(ImageMapper): Fix floating point pwf range

--- Sources/Rendering/OpenGL/ImageMapper/index.js
+++ Sources/Rendering/OpenGL/ImageMapper/index.js
@@ -731,20 +731,19 @@ function vtkOpenGLImageMapper(publicAPI, model) {
           pwfun = actorProperty.getPiecewiseFunction(c);
           if (pwfun === null) {
             // Piecewise constant max if no function supplied for this component
-            pwfFloatTable.fill(255.0);
+            pwfFloatTable.fill(1.0);
           } else {
             const pwfRange = pwfun.getRange();
             pwfun.getTable(pwfRange[0], pwfRange[1], pwfWidth, tmpTable, 1);
             // adjust for sample distance etc
             if (iComps) {
               for (let i = 0; i < pwfWidth; i++) {
-                pwfFloatTable[c * pwfWidth * 2 + i] = 255.0 * tmpTable[i];
-                pwfFloatTable[c * pwfWidth * 2 + i + pwfWidth] =
-                  255.0 * tmpTable[i];
+                pwfFloatTable[c * pwfWidth * 2 + i] = tmpTable[i];
+                pwfFloatTable[c * pwfWidth * 2 + i + pwfWidth] = tmpTable[i];
               }
             } else {
               for (let i = 0; i < pwfWidth; i++) {
-                pwfFloatTable[c * pwfWidth * 2 + i] = 255.0 * tmpTable[i];
+                pwfFloatTable[c * pwfWidth * 2 + i] = tmpTable[i];
               }
             }
           }
