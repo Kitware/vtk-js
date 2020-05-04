5c0b881d74fc1185458ad443d528583f623169a7
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2020-05-04 12:25:20 +0200

    feat(ThirdParty): Expose dependency so other can use them

--- Sources/index.js
+++ Sources/index.js
@@ -5,6 +5,7 @@ import Interaction from './Interaction';
 import IO from './IO';
 import Rendering from './Rendering';
 import VTKProxy from './Proxy';
+import ThirdParty from './ThirdParty';
 import Widgets from './Widgets';
 
 import macro from './macro';
@@ -18,6 +19,7 @@ vtk.Interaction = Interaction;
 vtk.IO = IO;
 vtk.Proxy = VTKProxy;
 vtk.Rendering = Rendering;
+vtk.ThirdParty = ThirdParty;
 vtk.Widgets = Widgets;
 
 vtk.mtime = macro.getCurrentGlobalMTime;
