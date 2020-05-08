29677ddb3fbeb0e1a9c4103c48b46d93a168cbc0
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2020-05-08 19:46:27 +0200

    attempt to register vtkDataArray

--- Sources/index.d.ts
+++ Sources/index.d.ts
@@ -1,3 +1,11 @@
 declare namespace vtk {
   export * as macro from './macro';
+  namespace Common {
+    namespace Core {
+      namespace vtkDataArray {
+        export * from './Common/Core/DataArray';
+      }
+    }
+  }
+
 }
