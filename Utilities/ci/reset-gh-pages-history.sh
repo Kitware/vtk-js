73e7a53b5b34839d7ffcb54359d481f40a24193b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-04-30 11:08:08 -0600

    chore(ghpages): finish script for ci

--- Utilities/ci/reset-gh-pages-history.sh
+++ Utilities/ci/reset-gh-pages-history.sh
@@ -1,7 +1,6 @@
 git checkout gh-pages
 git checkout --orphan tmp
-git add -A
-git commit -am "Reset website history"
+git commit -m "Reset website history"
 git branch -D gh-pages
 git branch -m gh-pages
 git push -f origin gh-pages
