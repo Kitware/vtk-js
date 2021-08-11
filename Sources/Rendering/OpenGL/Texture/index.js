11f1961e03802d48236941d45b55746111fe51de
Ken Martin <ken.martin@kitware.com>
2021-08-11 13:57:10 -0400

    fix(Rendering): support half float textures, fix line normal
    When float textures are not available fall back to half float
    if they are available.
    
    Also a potential fix for line normal rendering issues

--- Sources/Rendering/OpenGL/Texture/index.js
+++ Sources/Rendering/OpenGL/Texture/index.js
@@ -10,6 +10,68 @@ const { Wrap, Filter } = Constants;
 const { VtkDataTypes } = vtkDataArray;
 const { vtkDebugMacro, vtkErrorMacro, vtkWarningMacro } = macro;
 
+const floatView = new Float32Array(1);
+const int32View = new Int32Array(floatView.buffer);
+
+/* eslint-disable no-bitwise */
+/* This method is faster than the OpenEXR implementation (very often
+ * used, eg. in Ogre), with the additional benefit of rounding, inspired
+ * by James Tursa?s half-precision code. */
+function toHalf(val) {
+  floatView[0] = val;
+  const x = int32View[0];
+
+  let bits = (x >> 16) & 0x8000; /* Get the sign */
+  let m = (x >> 12) & 0x07ff; /* Keep one extra bit for rounding */
+  const e = (x >> 23) & 0xff; /* Using int is faster here */
+
+  /* If zero, or denormal, or exponent underflows too much for a denormal
+   * half, return signed zero. */
+  if (e < 103) {
+    return bits;
+  }
+
+  /* If NaN, return NaN. If Inf or exponent overflow, return Inf. */
+  if (e > 142) {
+    bits |= 0x7c00;
+    /* If exponent was 0xff and one mantissa bit was set, it means NaN,
+     * not Inf, so make sure we set one mantissa bit too. */
+    bits |= (e === 255 ? 0 : 1) && x & 0x007fffff;
+    return bits;
+  }
+
+  /* If exponent underflows but not too much, return a denormal */
+  if (e < 113) {
+    m |= 0x0800;
+    /* Extra rounding may overflow and set mantissa to 0 and exponent
+     * to 1, which is OK. */
+    bits |= (m >> (114 - e)) + ((m >> (113 - e)) & 1);
+    return bits;
+  }
+
+  bits |= ((e - 112) << 10) | (m >> 1);
+  /* Extra rounding. An overflow will set mantissa to 0 and increment
+   * the exponent, which is OK. */
+  bits += m & 1;
+  return bits;
+}
+
+function fromHalf(h) {
+  const s = (h & 0x8000) >> 15;
+  const e = (h & 0x7c00) >> 10;
+  const f = h & 0x03ff;
+
+  if (e === 0) {
+    return (s ? -1 : 1) * 2 ** -14 * (f / 2 ** 10);
+  }
+
+  if (e === 0x1f) {
+    return f ? NaN : (s ? -1 : 1) * Infinity;
+  }
+
+  return (s ? -1 : 1) * 2 ** (e - 15) * (1 + f / 2 ** 10);
+}
+
 // ----------------------------------------------------------------------------
 // vtkOpenGLTexture methods
 // ----------------------------------------------------------------------------
@@ -458,6 +520,17 @@ function vtkOpenGLTexture(publicAPI, model) {
         ) {
           return model.context.FLOAT;
         }
+        {
+          const halfFloat = model.context.getExtension(
+            'OES_texture_half_float'
+          );
+          if (
+            halfFloat &&
+            model.context.getExtension('OES_texture_half_float_linear')
+          ) {
+            return halfFloat.HALF_FLOAT_OES;
+          }
+        }
         return model.context.UNSIGNED_BYTE;
     }
   };
@@ -574,6 +647,20 @@ function vtkOpenGLTexture(publicAPI, model) {
       }
     }
 
+    // if the opengl data type is half float
+    // then the data array must be u16
+    const halfFloat = model.context.getExtension('OES_texture_half_float');
+    if (halfFloat && model.openGLDataType === halfFloat.HALF_FLOAT_OES) {
+      const pixCount = model.width * model.height * model.components;
+      for (let idx = 0; idx < data.length; idx++) {
+        const newArray = new Uint16Array(pixCount);
+        for (let i = 0; i < pixCount; i++) {
+          newArray[i] = toHalf(data[idx][i]);
+        }
+        pixData.push(newArray);
+      }
+    }
+
     // The output has to be filled
     if (pixData.length === 0) {
       for (let i = 0; i < data.length; i++) {
@@ -599,23 +686,27 @@ function vtkOpenGLTexture(publicAPI, model) {
       (!vtkMath.isPowerOfTwo(width) || !vtkMath.isPowerOfTwo(height))
     ) {
       // Scale up the texture to the next highest power of two dimensions.
+      const halfFloat = model.context.getExtension('OES_texture_half_float');
       const newWidth = vtkMath.nearestPowerOfTwo(width);
       const newHeight = vtkMath.nearestPowerOfTwo(height);
       const pixCount = newWidth * newHeight * model.components;
       for (let idx = 0; idx < data.length; idx++) {
         if (data[idx] !== null) {
           let newArray = null;
-          switch (model.openGLDataType) {
-            case model.context.FLOAT:
-              newArray = new Float32Array(pixCount);
-              break;
-            default:
-            case model.context.UNSIGNED_BYTE:
-              newArray = new Uint8Array(pixCount);
-              break;
-          }
           const jFactor = height / newHeight;
           const iFactor = width / newWidth;
+          let usingHalf = false;
+          if (model.openGLDataType === model.context.FLOAT) {
+            newArray = new Float32Array(pixCount);
+          } else if (
+            halfFloat &&
+            model.openGLDataType === halfFloat.HALF_FLOAT_OES
+          ) {
+            newArray = new Uint16Array(pixCount);
+            usingHalf = true;
+          } else {
+            newArray = new Uint8Array(pixCount);
+          }
           for (let j = 0; j < newHeight; j++) {
             const joff = j * newWidth * numComps;
             const jidx = j * jFactor;
@@ -640,11 +731,24 @@ function vtkOpenGLTexture(publicAPI, model) {
               ilow *= numComps;
               ihi *= numComps;
               for (let c = 0; c < numComps; c++) {
-                newArray[joff + ioff + c] =
-                  data[idx][jlow + ilow + c] * jmix1 * (1.0 - imix) +
-                  data[idx][jlow + ihi + c] * jmix1 * imix +
-                  data[idx][jhi + ilow + c] * jmix * (1.0 - imix) +
-                  data[idx][jhi + ihi + c] * jmix * imix;
+                if (usingHalf) {
+                  newArray[joff + ioff + c] = toHalf(
+                    fromHalf(data[idx][jlow + ilow + c]) *
+                      jmix1 *
+                      (1.0 - imix) +
+                      fromHalf(data[idx][jlow + ihi + c]) * jmix1 * imix +
+                      fromHalf(data[idx][jhi + ilow + c]) *
+                        jmix *
+                        (1.0 - imix) +
+                      fromHalf(data[idx][jhi + ihi + c]) * jmix * imix
+                  );
+                } else {
+                  newArray[joff + ioff + c] =
+                    data[idx][jlow + ilow + c] * jmix1 * (1.0 - imix) +
+                    data[idx][jlow + ihi + c] * jmix1 * imix +
+                    data[idx][jhi + ilow + c] * jmix * (1.0 - imix) +
+                    data[idx][jhi + ihi + c] * jmix * imix;
+                }
               }
             }
           }
