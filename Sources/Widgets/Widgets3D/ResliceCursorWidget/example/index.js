99a3e676033286f9bbaf2cadfa1c4816e45200f6
Julien Finet <julien.finet@kitware.com>
2021-08-13 00:31:20 +0200

    fix(reslicecursorwidget): take into account rotationHandlePosition specified in parameters

--- Sources/Widgets/Widgets3D/ResliceCursorWidget/example/index.js
+++ Sources/Widgets/Widgets3D/ResliceCursorWidget/example/index.js
@@ -143,6 +143,7 @@ for (let i = 0; i < 4; i++) {
     obj.interactor.setInteractorStyle(vtkInteractorStyleImage.newInstance());
     obj.widgetInstance = obj.widgetManager.addWidget(widget, xyzToViewType[i]);
     obj.widgetInstance.setScaleInPixels(true);
+    obj.widgetInstance.setRotationHandlePosition(0.75);
     obj.widgetManager.enablePicking();
     // Use to update all renderers buffer when actors are moved
     obj.widgetManager.setCaptureOn(CaptureOn.MOUSE_MOVE);
