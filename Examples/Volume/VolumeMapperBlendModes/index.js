c514683367f73ba23c0d9883c36517e927d68217
Alireza <ar.sedghi@gmail.com>
2022-03-01 18:29:25 -0500

    fix(example): revert back volumeMapperBlendModes

--- Examples/Volume/VolumeMapperBlendModes/index.js
+++ Examples/Volume/VolumeMapperBlendModes/index.js
@@ -1,76 +1,134 @@
 import 'vtk.js/Sources/favicon';
 
 // Load the rendering pieces we want to use (for both WebGL and WebGPU)
-import 'vtk.js/Sources/Rendering/Profiles/Geometry';
 import 'vtk.js/Sources/Rendering/Profiles/Volume';
 
+// Force DataAccessHelper to have access to various data source
 import 'vtk.js/Sources/IO/Core/DataAccessHelper/HtmlDataAccessHelper';
 import 'vtk.js/Sources/IO/Core/DataAccessHelper/HttpDataAccessHelper';
 import 'vtk.js/Sources/IO/Core/DataAccessHelper/JSZipDataAccessHelper';
 
 import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
-
-import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
-import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
+import vtkHttpDataSetReader from 'vtk.js/Sources/IO/Core/HttpDataSetReader';
+import vtkPiecewiseFunction from 'vtk.js/Sources/Common/DataModel/PiecewiseFunction';
+import vtkColorTransferFunction from 'vtk.js/Sources/Rendering/Core/ColorTransferFunction';
 import vtkVolume from 'vtk.js/Sources/Rendering/Core/Volume';
 import vtkVolumeMapper from 'vtk.js/Sources/Rendering/Core/VolumeMapper';
-import vtkHttpDataSetReader from 'vtk.js/Sources/IO/Core/HttpDataSetReader';
+import controlPanel from './controller.html';
 
-import vtkSphereSource from 'vtk.js/Sources/Filters/Sources/SphereSource';
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
 
-const fullScreenRenderWindow = vtkFullScreenRenderWindow.newInstance();
-const renderWindow = fullScreenRenderWindow.getRenderWindow();
-const renderer = fullScreenRenderWindow.getRenderer();
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+  background: [0.3, 0.3, 0.3],
+});
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
+
+fullScreenRenderer.addController(controlPanel);
+
+// ----------------------------------------------------------------------------
+// Example code
+// ----------------------------------------------------------------------------
+// Server is not sending the .gz and with the compress header
+// Need to fetch the true file name and uncompress it locally
+// ----------------------------------------------------------------------------
+
+const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true });
 
 const actor = vtkVolume.newInstance();
 const mapper = vtkVolumeMapper.newInstance();
-mapper.setSampleDistance(0.1);
+mapper.setSampleDistance(1.3);
+mapper.setPreferSizeOverAccuracy(true);
 actor.setMapper(mapper);
 
-const reader = vtkHttpDataSetReader.newInstance({
-  fetchGzip: true,
-});
+// create color and opacity transfer functions
+const ofun = vtkPiecewiseFunction.newInstance();
+ofun.addPoint(-3024, 0.1);
+ofun.addPoint(-637.62, 0.1);
+ofun.addPoint(700, 0.5);
+ofun.addPoint(3071, 0.9);
+
+const ctfun = vtkColorTransferFunction.newInstance();
+ctfun.addRGBPoint(-3024, 0, 0, 0);
+ctfun.addRGBPoint(-637.62, 0, 0, 0);
+ctfun.addRGBPoint(700, 1, 1, 1);
+ctfun.addRGBPoint(3071, 1, 1, 1);
+
+actor.getProperty().setRGBTransferFunction(0, ctfun);
+actor.getProperty().setScalarOpacity(0, ofun);
+actor.getProperty().setScalarOpacityUnitDistance(0, 3.0);
+actor.getProperty().setInterpolationTypeToLinear();
+actor.getProperty().setShade(true);
+actor.getProperty().setAmbient(0.1);
+actor.getProperty().setDiffuse(0.9);
+actor.getProperty().setSpecular(0.2);
+actor.getProperty().setSpecularPower(10.0);
 
 mapper.setInputConnection(reader.getOutputPort());
 
-// Sphere sources
-const sphereSource1 = vtkSphereSource.newInstance();
-const sphereActor1 = vtkActor.newInstance();
-const sphereMapper1 = vtkMapper.newInstance();
-const sphereSource2 = vtkSphereSource.newInstance();
-const sphereActor2 = vtkActor.newInstance();
-const sphereMapper2 = vtkMapper.newInstance();
+function updateBlendMode(event) {
+  const blendMode = parseInt(event.target.value, 10);
+  const ipScalarEls = document.querySelectorAll('.ipScalar');
 
-sphereMapper1.setInputConnection(sphereSource1.getOutputPort());
-sphereActor1.setMapper(sphereMapper1);
-sphereMapper2.setInputConnection(sphereSource2.getOutputPort());
-sphereActor2.setMapper(sphereMapper2);
+  mapper.setBlendMode(blendMode);
+  mapper.setIpScalarRange(0.0, 1.0);
 
-reader
-  .setUrl(
-    'https://sedghi.github.io/dicom-test/src/data/synthetic_dicom_11Slices_1mm.vti'
-  )
-  .then(() => {
-    reader.loadData().then(() => {
-      renderer.addVolume(actor);
-      actor.getProperty().setInterpolationTypeToNearest();
+  // if average or additive blend mode
+  if (blendMode === 3 || blendMode === 4) {
+    // Show scalar ui
+    for (let i = 0; i < ipScalarEls.length; i += 1) {
+      const el = ipScalarEls[i];
+      el.style.display = 'table-row';
+    }
+  } else {
+    // Hide scalar ui
+    for (let i = 0; i < ipScalarEls.length; i += 1) {
+      const el = ipScalarEls[i];
+      el.style.display = 'none';
+    }
+  }
 
-      renderer.resetCamera();
+  renderWindow.render();
+}
 
-      const activeCamera = renderer.getActiveCamera();
-      activeCamera.setParallelProjection(true);
+function updateScalarMin(event) {
+  mapper.setIpScalarRange(event.target.value, mapper.getIpScalarRange()[1]);
+  renderWindow.render();
+}
 
-      sphereSource1.setCenter(0, 0, 0);
-      sphereSource1.setRadius(0.2);
+function updateScalarMax(event) {
+  mapper.setIpScalarRange(mapper.getIpScalarRange()[0], event.target.value);
+  renderWindow.render();
+}
 
-      sphereSource2.setCenter(10, 10, 10);
-      sphereSource2.setRadius(0.2);
+reader.setUrl(`${__BASE_PATH__}/data/volume/headsq.vti`).then(() => {
+  reader.loadData().then(() => {
+    renderer.addVolume(actor);
+    const interactor = renderWindow.getInteractor();
+    interactor.setDesiredUpdateRate(15.0);
+    renderer.resetCamera();
+    renderer.getActiveCamera().elevation(-70);
+    renderWindow.render();
 
-      renderer.addVolume(sphereActor1);
-      renderer.addVolume(sphereActor2);
-
-      renderer.resetCamera();
-
-      renderWindow.render();
-    });
+    const el = document.querySelector('.blendMode');
+    el.addEventListener('change', updateBlendMode);
+    const scalarMinEl = document.querySelector('.scalarMin');
+    scalarMinEl.addEventListener('input', updateScalarMin);
+    const scalarMaxEl = document.querySelector('.scalarMax');
+    scalarMaxEl.addEventListener('input', updateScalarMax);
   });
+});
+
+// -----------------------------------------------------------
+// Make some variables global so that you can inspect and
+// modify objects in your browser's developer console:
+// -----------------------------------------------------------
+
+global.source = reader;
+global.mapper = mapper;
+global.actor = actor;
+global.ofun = ofun;
+global.renderer = renderer;
+global.renderWindow = renderWindow;
