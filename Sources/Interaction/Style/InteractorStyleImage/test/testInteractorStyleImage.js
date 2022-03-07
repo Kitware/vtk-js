c5e1c95d57f6ec2919f8a863297712e45b9f8837
Alireza <ar.sedghi@gmail.com>
2022-03-07 12:44:10 -0500

    fix: add release resources to interactorStyle test

--- Sources/Interaction/Style/InteractorStyleImage/test/testInteractorStyleImage.js
+++ Sources/Interaction/Style/InteractorStyleImage/test/testInteractorStyleImage.js
@@ -1,4 +1,5 @@
 import test from 'tape-catch';
+import testUtils from 'vtk.js/Sources/Testing/testUtils';
 
 import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
 import vtkGenericRenderWindow from 'vtk.js/Sources/Rendering/Misc/GenericRenderWindow';
@@ -9,10 +10,13 @@ import vtkSplineWidget from 'vtk.js/Sources/Widgets/Widgets3D/SplineWidget';
 import vtkWidgetManager from 'vtk.js/Sources/Widgets/Core/WidgetManager';
 
 test('Test vtkInteractorStyleImage.setCurrentImageNumber', (t) => {
+  const gc = testUtils.createGarbageCollector(t);
   const container = document.querySelector('body');
-  const renderWindowContainer = document.createElement('div');
+  const renderWindowContainer = gc.registerDOMElement(
+    document.createElement('div')
+  );
   container.appendChild(renderWindowContainer);
-  const grw = vtkGenericRenderWindow.newInstance();
+  const grw = gc.registerResource(vtkGenericRenderWindow.newInstance());
   grw.setContainer(renderWindowContainer);
   grw.resize();
 
@@ -20,40 +24,42 @@ test('Test vtkInteractorStyleImage.setCurrentImageNumber', (t) => {
   const interactor = grw.getInteractor();
   interactor.setEnabled(false);
 
-  const interactorStyle = vtkInteractorStyleImage.newInstance();
+  const interactorStyle = gc.registerResource(
+    vtkInteractorStyleImage.newInstance()
+  );
   interactor.setInteractorStyle(interactorStyle);
 
   const imageSlices = [];
   for (let i = 0; i < 5; ++i) {
-    const imageMapper = vtkImageMapper.newInstance();
-    const imageActor = vtkImageSlice.newInstance();
+    const imageMapper = gc.registerResource(vtkImageMapper.newInstance());
+    const imageActor = gc.registerResource(vtkImageSlice.newInstance());
     imageActor.setMapper(imageMapper);
     imageSlices.push(imageActor);
   }
-  const widgetManager = vtkWidgetManager.newInstance();
+  const widgetManager = gc.registerResource(vtkWidgetManager.newInstance());
   widgetManager.setRenderer(renderer);
 
   // Populate renderer with props other than image slice.
-  renderer.addActor(vtkActor.newInstance());
-  const widgetFactory = vtkSplineWidget.newInstance();
+  renderer.addActor(gc.registerResource(vtkActor.newInstance()));
+  const widgetFactory = gc.registerResource(vtkSplineWidget.newInstance());
 
   widgetManager.addWidget(widgetFactory); // Adds a widget in the renderer
 
   renderer.addActor(imageSlices[0]);
   widgetManager.addWidget(widgetFactory);
-  renderer.addActor(vtkActor.newInstance());
+  renderer.addActor(gc.registerResource(vtkActor.newInstance()));
   renderer.addActor(imageSlices[1]);
   widgetManager.addWidget(widgetFactory);
-  renderer.addActor(vtkActor.newInstance());
+  renderer.addActor(gc.registerResource(vtkActor.newInstance()));
   renderer.addActor(imageSlices[2]);
   widgetManager.addWidget(widgetFactory);
-  renderer.addActor(vtkActor.newInstance());
+  renderer.addActor(gc.registerResource(vtkActor.newInstance()));
   renderer.addActor(imageSlices[3]);
   widgetManager.addWidget(widgetFactory);
-  renderer.addActor(vtkActor.newInstance());
+  renderer.addActor(gc.registerResource(vtkActor.newInstance()));
   renderer.addActor(imageSlices[4]);
   widgetManager.addWidget(widgetFactory);
-  renderer.addActor(vtkActor.newInstance());
+  renderer.addActor(gc.registerResource(vtkActor.newInstance()));
 
   // Test setCurrentImageNumber()
   interactorStyle.setCurrentImageNumber(0);
@@ -81,5 +87,7 @@ test('Test vtkInteractorStyleImage.setCurrentImageNumber', (t) => {
     interactorStyle.getCurrentImageProperty(),
     imageSlices[3].getProperty()
   );
-  t.end();
+
+  // release resources and end test
+  gc.releaseResources();
 });
