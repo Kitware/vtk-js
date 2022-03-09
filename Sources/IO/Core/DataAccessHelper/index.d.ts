688edd472f54f179d2f16d672b269f3b618265ba
Adnane Belmadiaf <daker@users.noreply.github.com>
2022-03-09 01:19:43 +0100

    docs(ts): fix typescript definitions

---
+++ Sources/IO/Core/DataAccessHelper/index.d.ts
@@ -0,0 +1,32 @@
+import HtmlDataAccessHelper from "./HtmlDataAccessHelper";
+import HttpDataAccessHelper from "./HttpDataAccessHelper";
+import JSZipDataAccessHelper from "./JSZipDataAccessHelper";
+import LiteHttpDataAccessHelper from "./LiteHttpDataAccessHelper";
+
+export function has(type: string): boolean;
+export function get(type?: string, options?: object): HtmlDataAccessHelper | HttpDataAccessHelper | JSZipDataAccessHelper | LiteHttpDataAccessHelper
+export function registerType(type: string, fn: any): void;
+
+export interface DataAccessHelper {
+	/**
+	 * 
+	 * @param {String} type 
+	 */
+	has(type: string): boolean;
+
+	/**
+	 * 
+	 * @param {String} type 
+	 * @param options 
+	 */
+	get(type?: string, options?: object): HtmlDataAccessHelper | HttpDataAccessHelper | JSZipDataAccessHelper | LiteHttpDataAccessHelper
+
+	/**
+	 * 
+	 * @param {String} type 
+	 * @param fn 
+	 */
+	registerType(type: string, fn: any): void;
+}
+
+export default DataAccessHelper;
