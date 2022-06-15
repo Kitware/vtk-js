7f15397b5ae33352d06ea4e0a730a2ad1d0ea49f
joannacirillo <cirillojoanna@gmail.com>
2022-06-15 18:40:01 +0200

    refactor(remoteview): refactor constructor
    refactor constructor to call setters at instanciation

--- Sources/Rendering/Misc/RemoteView/index.js
+++ Sources/Rendering/Misc/RemoteView/index.js
@@ -187,21 +187,24 @@ function vtkRemoteView(publicAPI, model) {
 // Object factory
 // ----------------------------------------------------------------------------
 
-const DEFAULT_VALUES = {
-  viewId: '-1',
-  interactiveQuality: 60,
-  interactiveRatio: 1 / window.devicePixelRatio,
-  stillQuality: 100,
-  stillRatio: 1,
-  rpcMouseEvent: 'viewport.mouse.interaction',
-  rpcGestureEvent: null,
-  rpcWheelEvent: null,
-};
+function defaultValues(initialValues) {
+  return {
+    viewId: '-1',
+    interactiveQuality: 60,
+    interactiveRatio: 1 / window.devicePixelRatio,
+    stillQuality: 100,
+    stillRatio: 1,
+    rpcMouseEvent: 'viewport.mouse.interaction',
+    rpcGestureEvent: null,
+    rpcWheelEvent: null,
+    ...initialValues,
+  };
+}
 
 // ----------------------------------------------------------------------------
 
 export function extend(publicAPI, model, initialValues = {}) {
-  Object.assign(model, DEFAULT_VALUES, initialValues);
+  Object.assign(initialValues, defaultValues(initialValues));
 
   macro.obj(publicAPI, model, initialValues);
   macro.get(publicAPI, model, [
