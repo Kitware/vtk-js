3718d8fe51d552a5ebf2ab6e26fb8dfe070a2f74
hx <hxhxhx88@gmail.com>
2022-12-05 20:12:32 +0100

    Update vtkImageMapper interface

--- Sources/Rendering/Core/ImageMapper/index.d.ts
+++ Sources/Rendering/Core/ImageMapper/index.d.ts
@@ -237,7 +237,7 @@ export interface vtkImageMapper extends vtkAbstractMapper {
 	 * Set the slice from a given focal point. 
 	 * @param {Boolean} sliceAtFocalPoint
 	 */
-	sliceAtFocalPoint(sliceAtFocalPoint: boolean): boolean;
+	setSliceAtFocalPoint(sliceAtFocalPoint: boolean): boolean;
 
 	/**
 	 * 
