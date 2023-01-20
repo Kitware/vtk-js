Kitware/vtk-js

commit 2dc7244409a175042750b4437ce522a56d210d22

refactor(reslicecursorwidget): split representation into handle representations
This allows simpler overriding of sub-widget states. Each component (center, translation line,
rotation handle) can be tweaked by changing representation.

BREAKING CHANGE: vtkResliceCursorContextRepresentation is deprecated and
removed. Instead, a vtkSphereHandleRepresentation is used for rotation
and center handles, and a vtkLineHandleRepresenttion is used for the
axes.
rotateLineInView() now takes an axis name (string, e.g. 'XinY') instead of a substate.

fix(reslicecursorwidget): to be merged

Sources/Widgets/Widgets3D/ResliceCursorWidget/behavior.js
@@ -6,19 +6,78 @@ import * as vtkMath from 'vtk.js/Sources/Common/Core/Math';
 
 import {
   boundPointOnPlane,
-  getAssociatedLinesName,
   rotateVector,
   updateState,
+  getOtherLineName,
+  getLinePlaneName,
+  getLineInPlaneName,
 } from 'vtk.js/Sources/Widgets/Widgets3D/ResliceCursorWidget/helpers';
 
 import {
   ScrollingMethods,
   InteractionMethodsName,
+  planeNameToViewType,
+  lineNames,
 } from 'vtk.js/Sources/Widgets/Widgets3D/ResliceCursorWidget/Constants';
 
 export default function widgetBehavior(publicAPI, model) {
   model._isDragging = false;
   let isScrolling = false;
+
+  // FIXME: label information should be accessible from activeState instead of parent state.
+  publicAPI.getActiveInteraction = () => {
+    if (
+      model.widgetState
+        .getStatesWithLabel('rotation')
+        .includes(model.activeState)
+    ) {
+      return InteractionMethodsName.RotateLine;
+    }
+    if (
+      model.widgetState.getStatesWithLabel('line').includes(model.activeState)
+    ) {
+      return InteractionMethodsName.TranslateAxis;
+    }
+    if (
+      model.widgetState.getStatesWithLabel('center').includes(model.activeState)
+    ) {
+      return InteractionMethodsName.TranslateCenter;
+    }
+    return null;
+  };
+
+  /**
+   * ActiveState can be RotationHandle or a LineHandle
+   * @returns 'YinX', 'ZinX', 'XinY', 'ZinY', 'XinZ' or 'YinZ'
+   */
+  publicAPI.getActiveLineName = () =>
+    lineNames.find((lineName) =>
+      model.widgetState.getStatesWithLabel(lineName).includes(model.activeState)
+    );
+
+  // FIXME: label information should be accessible from activeState instead of parent state.
+  publicAPI.getActiveLineHandle = () =>
+    model.widgetState[`getAxis${publicAPI.getActiveLineName()}`]?.();
+
+  /**
+   * Return the line handle of the other line in the same view.
+   * @param {string} lineName name of the line (YinX, ZinX, XinY, ZinY, XinZ, YinZ)
+   * @returns ZinX if lineName == YinX, YinX if lineName == ZinX, ZinY if lineName == XinY...
+   */
+  publicAPI.getOtherLineHandle = (lineName) =>
+    model.widgetState[`getAxis${getOtherLineName(lineName)}`]?.();
+
+  // FIXME: label information should be accessible from activeState instead of parent state.
+  /**
+   * There are 2 rotation handles per axis: 'point0' and 'point1'.
+   * This function returns which rotation handle (point0 or point1) is currently active.
+   * ActiveState must be a RotationHandle.
+   * @returns 'point0' or 'point1'
+   */
+  publicAPI.getActiveRotationPointName = () =>
+    model.widgetState.getStatesWithLabel('point0').includes(model.activeState)
+      ? 'point0'
+      : 'point1';
 
   // Reset "updateMethodName" attribute when no actors are selected
   // Useful to update 'updateMethodeName' to the correct name which
@@ -44,7 +103,7 @@ export default function widgetBehavior(publicAPI, model) {
   };
 
   publicAPI.updateCursor = () => {
-    switch (model.activeState.getUpdateMethodName()) {
+    switch (publicAPI.getActiveInteraction()) {
       case InteractionMethodsName.TranslateCenter:
         model._apiSpecificRenderWindow.setCursor('move');
         break;
@@ -63,7 +122,7 @@ export default function widgetBehavior(publicAPI, model) {
   publicAPI.handleLeftButtonPress = (callData) => {
     if (model.activeState && model.activeState.getActive()) {
       model._isDragging = true;
-      const viewType = model.widgetState.getActiveViewType();
+      const viewType = model.viewType;
       const currentPlaneNormal = model.widgetState.getPlanes()[viewType].normal;
       model.planeManipulator.setWidgetOrigin(model.widgetState.getCenter());
       model.planeManipulator.setWidgetNormal(currentPlaneNormal);
@@ -163,7 +222,7 @@ export default function widgetBehavior(publicAPI, model) {
 
   publicAPI.handleEvent = (callData) => {
     if (model.activeState.getActive()) {
-      publicAPI[model.activeState.getUpdateMethodName()](callData);
+      publicAPI[publicAPI.getActiveInteraction()](callData);
       publicAPI.invokeInternalInteractionEvent();
       return macro.EVENT_ABORT;
     }
@@ -171,9 +230,7 @@ export default function widgetBehavior(publicAPI, model) {
   };
 
   publicAPI.invokeInternalInteractionEvent = () => {
-    const methodName = model.activeState
-      ? model.activeState.getUpdateMethodName()
-      : '';
+    const methodName = publicAPI.getActiveInteraction();
     const computeFocalPointOffset =
       methodName !== InteractionMethodsName.RotateLine;
     const canUpdateFocalPoint =
@@ -222,32 +279,29 @@ export default function widgetBehavior(publicAPI, model) {
     newCenter = publicAPI.getBoundedCenter(newCenter);
 
     model.widgetState.setCenter(newCenter);
-    updateState(model.widgetState);
+    updateState(
+      model.widgetState,
+      model._factory.getDisplayScaleParams(),
+      model._factory.getRotationHandlePosition()
+    );
   };
 
   publicAPI[InteractionMethodsName.TranslateAxis] = (calldata) => {
-    const stateLine = model.widgetState.getActiveLineState();
-    const worldCoords = model.planeManipulator.handleEvent(
-      calldata,
-      model._apiSpecificRenderWindow
-    );
-
-    const point1 = stateLine.getPoint1();
-    const point2 = stateLine.getPoint2();
-
-    // Translate the current line along the other line
-    const otherLineName = getAssociatedLinesName(stateLine.getName());
-    const otherLine = model.widgetState[`get${otherLineName}`]();
-    const otherLineVector = vtkMath.subtract(
-      otherLine.getPoint2(),
-      otherLine.getPoint1(),
+    const lineHandle = publicAPI.getActiveLineHandle();
+    const lineName = publicAPI.getActiveLineName();
+    const pointOnLine = vtkMath.add(
+      lineHandle.getOrigin(),
+      lineHandle.getDirection(),
       []
     );
+    const currentLineVector = lineHandle.getDirection();
+    vtkMath.normalize(currentLineVector);
+
+    // Translate the current line along the other line
+    const otherLineHandle = publicAPI.getOtherLineHandle(lineName);
+    const otherLineVector = otherLineHandle.getDirection();
     vtkMath.normalize(otherLineVector);
     const axisTranslation = otherLineVector;
-
-    const currentLineVector = vtkMath.subtract(point2, point1, [0, 0, 0]);
-    vtkMath.normalize(currentLineVector);
 
     const dot = vtkMath.dot(currentLineVector, otherLineVector);
     // lines are colinear, translate along perpendicular axis from current line
@@ -260,7 +314,16 @@ export default function widgetBehavior(publicAPI, model) {
     }
 
     const closestPoint = [];
-    vtkLine.distanceToLine(worldCoords, point1, point2, closestPoint);
+    const worldCoords = model.planeManipulator.handleEvent(
+      calldata,
+      model._apiSpecificRenderWindow
+    );
+    vtkLine.distanceToLine(
+      worldCoords,
+      lineHandle.getOrigin(),
+      pointOnLine,
+      closestPoint
+    );
 
     const translationVector = vtkMath.subtract(worldCoords, closestPoint, []);
     const translationDistance = vtkMath.dot(translationVector, axisTranslation);
@@ -274,7 +337,11 @@ export default function widgetBehavior(publicAPI, model) {
     );
     newOrigin = publicAPI.getBoundedCenter(newOrigin);
     model.widgetState.setCenter(newOrigin);
-    updateState(model.widgetState);
+    updateState(
+      model.widgetState,
+      model._factory.getDisplayScaleParams(),
+      model._factory.getRotationHandlePosition()
+    );
   };
 
   publicAPI.getBoundedCenter = (newCenter) => {
@@ -294,12 +361,16 @@ export default function widgetBehavior(publicAPI, model) {
       model._apiSpecificRenderWindow
     );
     worldCoords = publicAPI.getBoundedCenter(worldCoords);
-    model.activeState.setCenter(worldCoords);
-    updateState(model.widgetState);
+    model.widgetState.setCenter(worldCoords);
+    updateState(
+      model.widgetState,
+      model._factory.getDisplayScaleParams(),
+      model._factory.getRotationHandlePosition()
+    );
   };
 
   publicAPI[InteractionMethodsName.RotateLine] = (calldata) => {
-    const activeLine = model.widgetState.getActiveLineState();
+    const activeLineHandle = publicAPI.getActiveLineHandle();
     const planeNormal = model.planeManipulator.getWidgetNormal();
     const worldCoords = model.planeManipulator.handleEvent(
       calldata,
@@ -307,13 +378,9 @@ export default function widgetBehavior(publicAPI, model) {
     );
 
     const center = model.widgetState.getCenter();
-    const previousLineDirection = vtkMath.subtract(
-      activeLine.getPoint1(),
-      activeLine.getPoint2(),
-      []
-    );
+    const previousLineDirection = activeLineHandle.getDirection();
     vtkMath.normalize(previousLineDirection);
-    if (model.widgetState.getActiveRotationPointName() === 'point1') {
+    if (publicAPI.getActiveRotationPointName() === 'point0') {
       vtkMath.multiplyScalar(previousLineDirection, -1);
     }
 
@@ -327,27 +394,35 @@ export default function widgetBehavior(publicAPI, model) {
       planeNormal
     );
 
-    publicAPI.rotateLineInView(activeLine, radianAngle);
+    console.log('Rotate', publicAPI.getActiveLineName());
+    publicAPI.rotateLineInView(publicAPI.getActiveLineName(), radianAngle);
   };
 
   /**
    * Rotate a line by a specified angle
-   * @param {Line} line The line to rotate (e.g. getActiveLineState())
+   * @param {string} lineName The line name to rotate (e.g. YinX, ZinX, XinY, ZinY, XinZ, YinZ)
    * @param {Number} radianAngle Applied angle in radian
    */
-  publicAPI.rotateLineInView = (line, radianAngle) => {
-    const viewType = line.getViewType();
-    const inViewType = line.getInViewType();
+  publicAPI.rotateLineInView = (lineName, radianAngle) => {
+    const viewType = planeNameToViewType[getLinePlaneName(lineName)];
+    const inViewType = planeNameToViewType[getLineInPlaneName(lineName)];
     const planeNormal = model.widgetState.getPlanes()[inViewType].normal;
     publicAPI.rotatePlane(viewType, radianAngle, planeNormal);
 
     if (model.widgetState.getKeepOrthogonality()) {
-      const associatedLineName = getAssociatedLinesName(line.getName());
-      const associatedLine = model.widgetState[`get${associatedLineName}`]();
-      const associatedViewType = associatedLine.getViewType();
-      publicAPI.rotatePlane(associatedViewType, radianAngle, planeNormal);
+      const otherLineName = getOtherLineName(lineName);
+      const otherPlaneName = getLinePlaneName(otherLineName);
+      publicAPI.rotatePlane(
+        planeNameToViewType[otherPlaneName],
+        radianAngle,
+        planeNormal
+      );
     }
-    updateState(model.widgetState);
+    updateState(
+      model.widgetState,
+      model._factory.getDisplayScaleParams(),
+      model._factory.getRotationHandlePosition()
+    );
   };
 
   /**

Sources/Widgets/Core/AbstractWidgetFactory/index.js
@@ -49,6 +49,7 @@ function vtkAbstractWidgetFactory(publicAPI, model) {
         'apiSpecificRenderWindow',
         'factory',
       ]);
+      macro.get(widgetPublicAPI, widgetModel, ['viewType']);
       macro.safeArrays(widgetModel);
       vtkAbstractWidget.extend(widgetPublicAPI, widgetModel, initialValues);
 

Sources/Widgets/Representations/LineHandleRepresentation/index.js
@@ -1,0 +1,224 @@
+import macro from 'vtk.js/Sources/macros';
+import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
+import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+import vtkGlyph3DMapper from 'vtk.js/Sources/Rendering/Core/Glyph3DMapper';
+import { OrientationModes } from 'vtk.js/Sources/Rendering/Core/Glyph3DMapper/Constants';
+import vtkHandleRepresentation from 'vtk.js/Sources/Widgets/Representations/HandleRepresentation';
+import vtkPixelSpaceCallbackMapper from 'vtk.js/Sources/Rendering/Core/PixelSpaceCallbackMapper';
+import vtkPolyData from 'vtk.js/Sources/Common/DataModel/PolyData';
+import vtkCylinderSource from 'vtk.js/Sources/Filters/Sources/CylinderSource';
+
+import { ScalarMode } from 'vtk.js/Sources/Rendering/Core/Mapper/Constants';
+import { getPixelWorldHeightAtCoord } from 'vtk.js/Sources/Widgets/Representations/WidgetRepresentation';
+
+// ----------------------------------------------------------------------------
+// vtkLineHandleRepresentation methods
+// ----------------------------------------------------------------------------
+
+function vtkLineHandleRepresentation(publicAPI, model) {
+  // Set our className
+  model.classHierarchy.push('vtkLineHandleRepresentation');
+
+  // --------------------------------------------------------------------------
+  // Internal polydata dataset
+  // --------------------------------------------------------------------------
+
+  model.internalPolyData = vtkPolyData.newInstance({ mtime: 0 });
+  model.internalArrays = {
+    points: model.internalPolyData.getPoints(),
+    scale: vtkDataArray.newInstance({
+      name: 'scale',
+      numberOfComponents: 3,
+      empty: true,
+    }),
+    color: vtkDataArray.newInstance({
+      name: 'color',
+      numberOfComponents: 1,
+      empty: true,
+    }),
+    direction: vtkDataArray.newInstance({
+      name: 'direction',
+      numberOfComponents: 9,
+      empty: true,
+    }),
+  };
+
+  model.internalPolyData.getPointData().addArray(model.internalArrays.scale);
+  model.internalPolyData.getPointData().addArray(model.internalArrays.color);
+  model.internalPolyData
+    .getPointData()
+    .addArray(model.internalArrays.direction);
+
+  // --------------------------------------------------------------------------
+  // Generic rendering pipeline
+  // --------------------------------------------------------------------------
+
+  /*
+   * displayActors and displayMappers are used to render objects in HTML, allowing objects
+   * to be 'rendered' internally in a VTK scene without being visible on the final output
+   */
+
+  model.displayMapper = vtkPixelSpaceCallbackMapper.newInstance();
+  model.displayActor = vtkActor.newInstance({ parentProp: publicAPI });
+  // model.displayActor.getProperty().setOpacity(0); // don't show in 3D
+  model.displayActor.setMapper(model.displayMapper);
+  model.displayMapper.setInputConnection(publicAPI.getOutputPort());
+  publicAPI.addActor(model.displayActor);
+  model.alwaysVisibleActors = [model.displayActor];
+
+  model.mapper = vtkGlyph3DMapper.newInstance({
+    scaleArray: 'scale',
+    scaleMode: vtkGlyph3DMapper.ScaleModes.SCALE_BY_COMPONENTS,
+    colorByArrayName: 'color',
+    orientationArray: 'direction',
+    scalarMode: ScalarMode.USE_POINT_FIELD_DATA,
+    orientationMode: OrientationModes.MATRIX,
+  });
+  model.actor = vtkActor.newInstance({ parentProp: publicAPI });
+  model.glyph = vtkCylinderSource.newInstance({
+    resolution: model.glyphResolution,
+    direction: [0, 0, 1],
+  });
+
+  model.mapper.setInputConnection(publicAPI.getOutputPort(), 0);
+  model.mapper.setInputConnection(model.glyph.getOutputPort(), 1);
+  model.actor.setMapper(model.mapper);
+
+  publicAPI.addActor(model.actor);
+
+  // --------------------------------------------------------------------------
+
+  publicAPI.setGlyphResolution = macro.chain(
+    publicAPI.setGlyphResolution,
+    (r) => model.glyph.setResolution(r)
+  );
+
+  // --------------------------------------------------------------------------
+
+  function callbackProxy(coords) {
+    if (model.displayCallback) {
+      const filteredList = [];
+      const states = publicAPI.getRepresentationStates();
+      for (let i = 0; i < states.length; i++) {
+        if (states[i].getActive()) {
+          filteredList.push(coords[i]);
+        }
+      }
+      if (filteredList.length) {
+        model.displayCallback(filteredList);
+        return;
+      }
+    }
+    model.displayCallback();
+  }
+
+  publicAPI.setDisplayCallback = (callback) => {
+    model.displayCallback = callback;
+    model.displayMapper.setCallback(callback ? callbackProxy : null);
+  };
+
+  const superGetRepresentationStates = publicAPI.getRepresentationStates;
+  publicAPI.getRepresentationStates = (input = model.inputData[0]) =>
+    superGetRepresentationStates(input).filter(
+      (state) => state.getOrigin?.() && state.isVisible?.()
+    );
+
+  // --------------------------------------------------------------------------
+
+  publicAPI.requestData = (inData, outData) => {
+    const { points, scale, color, direction } = model.internalArrays;
+    const list = publicAPI.getRepresentationStates(inData[0]);
+    const totalCount = list.length;
+
+    if (color.getNumberOfValues() !== totalCount) {
+      // Need to resize dataset
+      points.setData(new Float64Array(3 * totalCount), 3);
+      scale.setData(new Float32Array(3 * totalCount), 3);
+      color.setData(new Float32Array(totalCount));
+      direction.setData(new Float32Array(9 * totalCount), 9);
+    }
+    const typedArray = {
+      points: points.getData(),
+      scale: scale.getData(),
+      color: color.getData(),
+      direction: direction.getData(),
+    };
+
+    for (let i = 0; i < totalCount; i++) {
+      const state = list[i];
+      const isActive = state.getActive();
+      let scaleFactor = isActive ? model.activeScaleFactor : 1;
+
+      const coord = state.getOrigin();
+      typedArray.points[i * 3 + 0] = coord[0];
+      typedArray.points[i * 3 + 1] = coord[1];
+      typedArray.points[i * 3 + 2] = coord[2];
+
+      const right = state.getRight ? state.getRight() : [1, 0, 0];
+      const up = state.getUp ? state.getUp() : [0, 1, 0];
+      const dir = state.getDirection ? state.getDirection() : [0, 0, 1];
+      const rotation = [...right, ...up, ...dir];
+      typedArray.direction.set(rotation, 9 * i);
+
+      let scale3 = state.getScale3 ? state.getScale3() : [1, 1, 1];
+      scale3 = scale3.map((x) => (x === 0 ? model.defaultScale : x));
+      if (model.infiniteLine) {
+        scale3[2] = 100000;
+      }
+
+      const scale1 = state.getScale1 ? state.getScale1() : model.defaultScale;
+      if (publicAPI.getScaleInPixels()) {
+        scaleFactor *= getPixelWorldHeightAtCoord(
+          coord,
+          model.displayScaleParams
+        );
+      }
+
+      typedArray.scale[i * 3 + 0] = scale1 * scaleFactor * scale3[0];
+      typedArray.scale[i * 3 + 1] = scale1 * scaleFactor * scale3[1];
+      typedArray.scale[i * 3 + 2] = scale1 * scaleFactor * scale3[2];
+
+      typedArray.color[i] =
+        model.useActiveColor && isActive ? model.activeColor : state.getColor();
+    }
+
+    model.internalPolyData.modified();
+    outData[0] = model.internalPolyData;
+  };
+}
+
+// ----------------------------------------------------------------------------
+// Object factory
+// ----------------------------------------------------------------------------
+
+const DEFAULT_VALUES = {
+  glyphResolution: 8,
+  defaultScale: 1,
+  useActiveColor: true,
+  infiniteLine: true,
+};
+let id = 0;
+
+// ----------------------------------------------------------------------------
+
+export function extend(publicAPI, model, initialValues = {}) {
+  Object.assign(model, DEFAULT_VALUES, initialValues);
+  model.id = id++;
+
+  vtkHandleRepresentation.extend(publicAPI, model, initialValues);
+  macro.get(publicAPI, model, ['glyph', 'mapper', 'actor']);
+
+  // Object specific methods
+  vtkLineHandleRepresentation(publicAPI, model);
+}
+
+// ----------------------------------------------------------------------------
+
+export const newInstance = macro.newInstance(
+  extend,
+  'vtkLineHandleRepresentation'
+);
+
+// ----------------------------------------------------------------------------
+
+export default { newInstance, extend };

Sources/Widgets/Representations/ResliceCursorContextRepresentation/Constants.js
@@ -1,11 +1,0 @@
-export const PlaneNormal = {
-  X: [1, 0, 0],
-  Y: [0, -1, 0],
-  Z: [0, 0, 1],
-};
-
-export const PlaneOrientation = {
-  X: 0,
-  Y: 1,
-  Z: 2,
-};

Sources/Widgets/Representations/ResliceCursorContextRepresentation/index.d.ts
@@ -1,54 +1,0 @@
-import {vtkWidgetRepresentation, IWidgetRepresentationInitialValues} from "../WidgetRepresentation";
-
-export interface IResliceCursorContextRepresentationInitialValues extends IWidgetRepresentationInitialValues {
-	axis1Name?: string,
-	axis2Name?: string,
-	rotationEnabled?: boolean,
-	rotationHandlePosition?: number,
-	scaleInPixels?: boolean,
-	viewType: any,
-	coincidentTopologyParameters: object,
-}
-
-export interface vtkResliceCursorContextRepresentation extends vtkWidgetRepresentation {
-
-	/**
-	 * Returns the ratio applied on the smallest view side (width or height)
-	 * used to compute the position of the rotation handles.
-	 * 1 to place the handles on the edge of the smallest view side, 0.5 to
-	 * place the handles half way between the center of the reslice cursor
-	 * and the edge of the smallest view side.
-	 * 0.5 by default
-	 */
-	getRotationHandlePosition(): number;
-
-	/**
-	 * Sets the ratio applied on the smallest view side (width or height) used to
-	 * compute the position of the rotation handles
-	 * @param {number} ratio a positive floating percent of the smallest view side
-	 * @returns true if modified, false otherwise
-	 * @see getRotationHandlePosition()
-	 */
-	 setRotationHandlePosition(ratio: number): boolean;
-}
-
-/**
- * Method use to decorate a given object (publicAPI+model) with vtkResliceCursorContextRepresentation characteristics.
- *
- * @param publicAPI object on which methods will be bounds (public)
- * @param model object on which data structure will be bounds (protected)
- * @param {IResliceCursorContextRepresentationInitialValues} [initialValues] (default: {})
- */
-export function extend(publicAPI: object, model: object, initialValues?: IResliceCursorContextRepresentationInitialValues): void;
-
-/**
- * Method use to create a new instance of vtkResliceCursorContextRepresentation
- * @param {IResliceCursorContextRepresentationInitialValues} [initialValues] for pre-setting some of its content
- */
-export function newInstance(initialValues?: IResliceCursorContextRepresentationInitialValues): vtkResliceCursorContextRepresentation;
-
-export declare const vtkResliceCursorContextRepresentation: {
-	newInstance: typeof newInstance;
-	extend: typeof extend;
-}
-export default vtkResliceCursorContextRepresentation;

commit ff50d0555fa60db2f209569174192058dd6ac831

fix(reslicecursorwidget): fix line widget representation error when scrolling
Regression from 2dc7244409a175042750b4437ce522a56d210d22

Sources/Widgets/Widgets3D/ResliceCursorWidget/behavior.js
@@ -80,21 +80,11 @@ export default function widgetBehavior(publicAPI, model) {
       ? 'point0'
       : 'point1';
 
-  // Reset "updateMethodName" attribute when no actors are selected
-  // Useful to update 'updateMethodeName' to the correct name which
-  // will be TranslateCenter by default
-  publicAPI.resetUpdateMethod = () => {
-    if (model.representations.length !== 0) {
-      model.representations[0].getSelectedState();
-    }
-  };
-
   publicAPI.startScrolling = (newPosition) => {
     if (newPosition) {
       previousPosition = newPosition;
     }
     isScrolling = true;
-    publicAPI.resetUpdateMethod();
     publicAPI.startInteraction();
   };
 
@@ -189,7 +179,6 @@ export default function widgetBehavior(publicAPI, model) {
   };
 
   publicAPI.handleStartMouseWheel = () => {
-    publicAPI.resetUpdateMethod();
     publicAPI.startInteraction();
   };
 