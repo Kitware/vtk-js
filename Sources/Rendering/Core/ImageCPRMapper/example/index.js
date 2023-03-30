ba38ce7de32c1629ae706cb3aee288f5eac9ddf9
Julien Finet <julien.finet@kitware.com>
2023-03-30 09:42:24 +0200

    feat(ImageCPRMapper): Add reslice cursor to example

--- Sources/Rendering/Core/ImageCPRMapper/example/index.js
+++ Sources/Rendering/Core/ImageCPRMapper/example/index.js
@@ -1,20 +1,26 @@
 import '@kitware/vtk.js/favicon';
 
 // Load the rendering pieces we want to use (for both WebGL and WebGPU)
-import '@kitware/vtk.js/Rendering/Profiles/Volume';
-
-import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
-import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
-import vtkPolyData from '@kitware/vtk.js/Common/DataModel/PolyData';
-import vtkDataArray from '@kitware/vtk.js/Common/Core/DataArray';
-import vtkImageSlice from '@kitware/vtk.js/Rendering/Core/ImageSlice';
-import vtkImageCPRMapper from '@kitware/vtk.js/Rendering/Core/ImageCPRMapper';
+import '@kitware/vtk.js/Rendering/Profiles/All';
 
 // Force the loading of HttpDataAccessHelper to support gzip decompression
 import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
 
-import vtkTransform from '@kitware/vtk.js/Common/Transform/Transform';
+import vtkCPRManipulator from '@kitware/vtk.js/Widgets/Manipulators/CPRManipulator';
+import vtkDataArray from '@kitware/vtk.js/Common/Core/DataArray';
+import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
+import vtkImageSlice from '@kitware/vtk.js/Rendering/Core/ImageSlice';
+import vtkImageCPRMapper from '@kitware/vtk.js/Rendering/Core/ImageCPRMapper';
+import vtkImageMapper from '@kitware/vtk.js/Rendering/Core/ImageMapper';
+import vtkImageReslice from '@kitware/vtk.js/Imaging/Core/ImageReslice';
 import vtkMatrixBuilder from '@kitware/vtk.js/Common/Core/MatrixBuilder';
+import vtkPolyData from '@kitware/vtk.js/Common/DataModel/PolyData';
+import vtkResliceCursorWidget from '@kitware/vtk.js/Widgets/Widgets3D/ResliceCursorWidget';
+import vtkRenderer from '@kitware/vtk.js/Rendering/Core/Renderer';
+import vtkTransform from '@kitware/vtk.js/Common/Transform/Transform';
+import vtkWidgetManager from '@kitware/vtk.js/Widgets/Core/WidgetManager';
+import { ViewTypes } from '@kitware/vtk.js/Widgets/Core/WidgetManager/Constants';
 
 import controlPanel from './controller.html';
 
@@ -25,12 +31,12 @@ const centerlinePaths = [`${__BASE_PATH__}/data/volume/centerline.json`];
 // Standard rendering code setup
 // ----------------------------------------------------------------------------
 
-const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
-  background: [0, 0, 0],
-});
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
 const renderer = fullScreenRenderer.getRenderer();
 const renderWindow = fullScreenRenderer.getRenderWindow();
 fullScreenRenderer.addController(controlPanel);
+const interactor = renderWindow.getInteractor();
+interactor.setDesiredUpdateRate(15.0);
 
 // ----------------------------------------------------------------------------
 // Example code
@@ -131,15 +137,75 @@ angleEl.addEventListener('input', (e) =>
   setCenterlineAngle(Number(e.target.value))
 );
 
+// Reslice Cursor Widget
+const widget = vtkResliceCursorWidget.newInstance({ planes: ['X', 'Y'] });
+const widgetManager = vtkWidgetManager.newInstance();
+widgetManager.setRenderer(renderer);
+widgetManager.addWidget(widget, ViewTypes.YZ_PLANE);
+const widgetState = widget.getWidgetState();
+// Set size in CSS pixel space because scaleInPixels defaults to true
+widgetState
+  .getStatesWithLabel('sphere')
+  .forEach((handle) => handle.setScale1(20));
+widgetState.getCenterHandle().setVisible(false);
+const crossRenderer = vtkRenderer.newInstance();
+crossRenderer.setViewport(0.7, 0, 1, 0.3);
+renderWindow.addRenderer(crossRenderer);
+renderWindow.setNumberOfLayers(2);
+crossRenderer.setLayer(1);
+const crossWidgetManager = vtkWidgetManager.newInstance();
+crossWidgetManager.setRenderer(crossRenderer);
+crossWidgetManager.addWidget(widget, ViewTypes.XZ_PLANE);
+
+const reslice = vtkImageReslice.newInstance();
+reslice.setTransformInputSampling(false);
+reslice.setAutoCropOutput(true);
+reslice.setOutputDimensionality(2);
+const resliceMapper = vtkImageMapper.newInstance();
+resliceMapper.setInputConnection(reslice.getOutputPort());
+const resliceActor = vtkImageSlice.newInstance();
+resliceActor.setMapper(resliceMapper);
+
+// FIXME: place widget center in middle of centerline
+// widget.setCenter(middleOfCenterLine)
+// FIXME: set cross view normal + viewUp of middle point of centerline
+widget.getWidgetState().getPlanes()[ViewTypes.XZ_PLANE].normal = [0, 0, 1];
+widget.getWidgetState().getPlanes()[ViewTypes.XZ_PLANE].viewUp = [0, 1, 0];
+const cprManipulator = vtkCPRManipulator.newInstance();
+// FIXME: pass centerline to CPR manipulator
+widget.getWidgetState().getAxisXinY().setManipulator(cprManipulator);
+widget.getWidgetState().getAxisYinX().setManipulator(cprManipulator);
+
+// Read image
 reader.setUrl(volumePath).then(() => {
   reader.loadData().then(() => {
-    renderer.addVolume(actor);
-    const interactor = renderWindow.getInteractor();
-    interactor.setDesiredUpdateRate(15.0);
-    renderer.resetCamera();
-    renderWindow.render();
+    const image = reader.getOutputData();
+    widget.setImage(image);
 
-    global.imageData = reader.getOutputData();
+    // FIXME: place the center of actor on the center of centerline
+    // actor.setOrigin(widget.getWidgetState().getCenter());
+    // actor.setOrientation(image.getOrigin());
+    actor.setUserMatrix(widget.getResliceAxes(ViewTypes.YZ_PLANE));
+    renderer.addVolume(actor);
+    widget.updateCameraPoints(renderer, ViewTypes.YZ_PLANE, true, false, true);
+
+    reslice.setInputData(image);
+    crossRenderer.addActor(resliceActor);
+    widget.updateReslicePlane(reslice, ViewTypes.XZ_PLANE);
+    resliceActor.setUserMatrix(reslice.getResliceAxes());
+    widget.updateCameraPoints(
+      crossRenderer,
+      ViewTypes.XZ_PLANE,
+      true,
+      false,
+      true
+    );
+
+    renderer.resetCamera();
+    crossRenderer.resetCamera();
+    interactor.render();
+
+    global.imageData = image;
   });
 });
 
