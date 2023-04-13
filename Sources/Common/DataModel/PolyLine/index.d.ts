270d9e8516a981d15a0a19a5aef208b46d2572ab
Thibault Bruyère <thibault.bruyere@kitware.com>
2023-04-13 14:01:48 +0200

    feat(vtkPolyline): add orientation to lines and polylines

--- Sources/Common/DataModel/PolyLine/index.d.ts
+++ Sources/Common/DataModel/PolyLine/index.d.ts
@@ -1,3 +1,4 @@
+import { quat } from 'gl-matrix';
 import { Vector2, Vector3 } from '../../../types';
 import vtkCell, { ICellInitialValues } from '../Cell';
 import { IIntersectWithLine } from '../Line';
@@ -24,8 +25,8 @@ export interface vtkPolyLine extends vtkCell {
 
   /**
    * Determine global coordinate (x[3]) from subId and parametric coordinates.
-   * Also returns interpolation weights. (The number of weights is equal to
-   * the number of points in the cell.)
+   * Also set interpolation weights. (There are two weights for the two
+   * points of the line segment specified by subId)
    * 
    * @param {number} subId
    * @param {Vector3} pcoords The parametric coordinates
@@ -33,6 +34,39 @@ export interface vtkPolyLine extends vtkCell {
    * @param {Vector2} weights The interpolation weights
    */
   evaluateLocation(subId: number, pcoords: Vector3, x: Vector3, weights: Vector2): void
+
+  /**
+   * Determine global orientation (q[3]) from subId and parametric coordinates.
+   * This uses the same algorithm as vtkLine (interpolates using slerp).
+   * Also set interpolation weights. (There are two weights for the two
+   * points of the line segment specified by subId)
+   * 
+   * @param {number} subId
+   * @param {Vector3} pcoords The parametric coordinates
+   * @param {Vector3} q The global orientation
+   * @param {Vector2} weights The interpolation weights
+   * @returns {boolean} Wether the orientation has been set in `q'
+   */
+  evaluateOrientation(subId: number, pcoords: Vector3, q: quat, weights: Vector2): boolean
+
+  /**
+   * Returns an array containing for each pointIdx between 0 (included) and
+   * numberOfPoints (excluded) the distance from the first point of the
+   * polyline to the point at pointIdx
+   * In particular if tda = publicAPI.getTotalDistanceArray(), then tda[0] = 0
+   * and tda[tda.length - 1] is the total length of the polyline
+   */
+  getTotalDistanceArray(): number[]
+
+  /**
+   * Returns the subId of the segment at the given distance from the first
+   * point of the polyline
+   * If the distance is negative or greater than the total length of the
+   * polyline, returns -1
+   * 
+   * @param distance The distance from the first point of the polyline
+   */
+  findSubId(distance: number): number;
 }
 
 /**
