5cf7ed6f61d7d2070261772ea5f910e9a2736eff
Thibault Bruyère <thibault.bruyere@kitware.com>
2023-04-26 14:00:21 +0200

    feat(cprManipulator): add a cpr manipulator
    Edit cpr manipulator and create a cpr behavior in the reslice cursor to be able
    to use the reslice cursor with the ImageCPRMapper
    Update the example to use this manipulator and this behavior
    
    See: ImageCPRMapper example to know how to use the manipulator and bhevior with cpr

---
+++ Sources/Widgets/Widgets3D/ResliceCursorWidget/cprBehavior.js
@@ -0,0 +1,102 @@
+import resliceCursorBehavior from 'vtk.js/Sources/Widgets/Widgets3D/ResliceCursorWidget/behavior';
+import { InteractionMethodsName } from 'vtk.js/Sources/Widgets/Widgets3D/ResliceCursorWidget/Constants';
+import { updateState } from 'vtk.js/Sources/Widgets/Widgets3D/ResliceCursorWidget/helpers';
+import { ViewTypes } from 'vtk.js/Sources/Widgets/Core/WidgetManager/Constants';
+import { vec3 } from 'gl-matrix';
+// import macro from 'vtk.js/Sources/macros';
+
+export default function widgetBehavior(publicAPI, model) {
+  // We inherit resliceCursorBehavior
+  resliceCursorBehavior(publicAPI, model);
+
+  const stretchPlaneName = 'Y';
+  const crossPlaneName = 'Z';
+
+  publicAPI.getActiveInteraction = () => {
+    if (
+      model.widgetState
+        .getStatesWithLabel(`lineIn${stretchPlaneName}`)
+        .includes(model.activeState)
+    ) {
+      return InteractionMethodsName.TranslateCenterAndUpdatePlanes;
+    }
+    if (
+      model.widgetState
+        .getStatesWithLabel(`lineIn${crossPlaneName}`)
+        .includes(model.activeState) ||
+      model.widgetState
+        .getStatesWithLabel(`rotationIn${crossPlaneName}`)
+        .includes(model.activeState)
+    ) {
+      return InteractionMethodsName.RotateLine;
+    }
+    return null;
+  };
+
+  publicAPI[InteractionMethodsName.TranslateCenterAndUpdatePlanes] = (
+    calldata
+  ) => {
+    const manipulator =
+      model.activeState?.getManipulator?.() ?? model.manipulator;
+
+    const { worldCoords, worldDirection } = manipulator.handleEvent(
+      calldata,
+      model._apiSpecificRenderWindow
+    );
+    publicAPI.updateCenterAndPlanes(worldCoords, worldDirection);
+  };
+
+  publicAPI.updateCenterAndPlanes = (worldCoords, worldDirection) => {
+    // Update center
+    const newBoundedCenter = publicAPI.getBoundedCenter(worldCoords);
+    model.widgetState.setCenter(newBoundedCenter);
+
+    // Update planes if axes are given
+    if (worldDirection) {
+      const getAxis = (idx) =>
+        vec3.normalize([], worldDirection.slice(3 * idx, 3 * idx + 3));
+      const planes = model.widgetState.getPlanes();
+      Object.keys(planes).forEach((viewType) => {
+        switch (Number.parseInt(viewType, 10)) {
+          case ViewTypes.YZ_PLANE:
+            planes[viewType] = {
+              normal: getAxis(0),
+              viewUp: getAxis(2),
+            };
+            break;
+          case ViewTypes.XZ_PLANE:
+            planes[viewType] = {
+              normal: getAxis(1),
+              viewUp: getAxis(2),
+            };
+            break;
+          case ViewTypes.XY_PLANE:
+            planes[viewType] = {
+              normal: getAxis(2),
+              viewUp: getAxis(1),
+            };
+            break;
+          default:
+            break;
+        }
+      });
+    }
+
+    updateState(
+      model.widgetState,
+      model._factory.getScaleInPixels(),
+      model._factory.getRotationHandlePosition()
+    );
+  };
+
+  publicAPI.translateCenterOnPlaneDirection = (nbSteps) => {
+    const stretchState = model.widgetState.getStatesWithLabel(
+      `lineIn${stretchPlaneName}`
+    )?.[0];
+    const handleScroll = stretchState?.getManipulator?.()?.handleScroll;
+    if (handleScroll) {
+      const { worldCoords, worldDirection } = handleScroll(nbSteps);
+      publicAPI.updateCenterAndPlanes(worldCoords, worldDirection);
+    }
+  };
+}
