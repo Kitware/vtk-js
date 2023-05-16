Kitware/vtk-js

commit 9c7525ef5ee081cbca620e0552c80d3876648f97

refactor(manipulator): change handleEvent signature
BREAKING CHANGE:
handleEvent(callData, glRenderWindow): vec3
becomes
handleEvent(callData, glRenderWindow): {
  worldCoords: Nullable<vec3>,
  worldDirection?: mat3,
}

It is now possible to change the orientation of reslice cursor from
a cprManipulator using an oriented polyline, not only the position

Sources/Widgets/Widgets3D/ImplicitPlaneWidget/index.js
@@ -100,7 +100,7 @@ function widgetBehavior(publicAPI, model) {
 
   publicAPI.updateFromOrigin = (callData) => {
     model.planeManipulator.setWidgetNormal(model.widgetState.getNormal());
-    const worldCoords = model.planeManipulator.handleEvent(
+    const { worldCoords } = model.planeManipulator.handleEvent(
       callData,
       model._apiSpecificRenderWindow
     );

Sources/Widgets/Manipulators/CPRManipulator/index.js
@@ -25,15 +25,16 @@ function vtkCPRManipulator(publicAPI, model) {
   // Set our className
   model.classHierarchy.push('vtkCPRManipulator');
 
-  publicAPI.handleEvent = (callData, glRenderWindow) =>
-    intersectDisplayWithPlane(
+  publicAPI.handleEvent = (callData, glRenderWindow) => ({
+    worldCoords: intersectDisplayWithPlane(
       callData.position.x,
       callData.position.y,
       publicAPI.getOrigin(callData),
       publicAPI.getNormal(callData),
       callData.pokedRenderer,
       glRenderWindow
-    );
+    ),
+  });
 }
 
 // ----------------------------------------------------------------------------
@@ -42,6 +43,9 @@ function vtkCPRManipulator(publicAPI, model) {
 
 function defaultValues(initialValues) {
   return {
+    angle: 0,
+    distance: 0,
+    polyline: null,
     ...initialValues,
   };
 }
@@ -50,6 +54,8 @@ function defaultValues(initialValues) {
 
 export function extend(publicAPI, model, initialValues = {}) {
   vtkAbstractManipulator.extend(publicAPI, model, defaultValues(initialValues));
+
+  macro.setGet(publicAPI, model, ['angle', 'distance', 'polyline']);
 
   vtkCPRManipulator(publicAPI, model);
 }

Sources/Widgets/Manipulators/LineManipulator/index.js
@@ -41,15 +41,16 @@ function vtkLineManipulator(publicAPI, model) {
   // Set our className
   model.classHierarchy.push('vtkLineManipulator');
 
-  publicAPI.handleEvent = (callData, glRenderWindow) =>
-    projectDisplayToLine(
+  publicAPI.handleEvent = (callData, glRenderWindow) => ({
+    worldCoords: projectDisplayToLine(
       callData.position.x,
       callData.position.y,
       publicAPI.getOrigin(callData),
       publicAPI.getNormal(callData),
       callData.pokedRenderer,
       glRenderWindow
-    );
+    ),
+  });
 }
 
 // ----------------------------------------------------------------------------

Sources/Widgets/Manipulators/PlaneManipulator/index.js
@@ -25,15 +25,16 @@ function vtkPlaneManipulator(publicAPI, model) {
   // Set our className
   model.classHierarchy.push('vtkPlaneManipulator');
 
-  publicAPI.handleEvent = (callData, glRenderWindow) =>
-    intersectDisplayWithPlane(
+  publicAPI.handleEvent = (callData, glRenderWindow) => ({
+    worldCoords: intersectDisplayWithPlane(
       callData.position.x,
       callData.position.y,
       publicAPI.getOrigin(callData),
       publicAPI.getNormal(callData),
       callData.pokedRenderer,
       glRenderWindow
-    );
+    ),
+  });
 }
 
 // ----------------------------------------------------------------------------

Sources/Widgets/Manipulators/TrackballManipulator/index.js
@@ -71,7 +71,7 @@ function vtkTrackballManipulator(publicAPI, model) {
     );
     prevX = callData.position.x;
     prevY = callData.position.y;
-    return newDirection;
+    return { worldCoords: newDirection };
   };
 
   publicAPI.reset = (callData) => {

Examples/Widgets/Box/BoxWidget.js
@@ -53,7 +53,7 @@ function widgetBehavior(publicAPI, model) {
       model.activeState.getActive()
     ) {
       // model.manipulator.setNormal(model.camera.getDirectionOfProjection());
-      const worldCoords = model.manipulator.handleEvent(
+      const { worldCoords } = model.manipulator.handleEvent(
         callData,
         model._apiSpecificRenderWindow
       );

Sources/Widgets/Manipulators/AbstractManipulator/index.d.ts
@@ -1,6 +1,6 @@
 import { vtkObject } from "../../../interfaces";
 import { vtkOpenGLRenderWindow } from "../../../Rendering/OpenGL/RenderWindow"
-import { Vector3 } from "../../../types";
+import { Matrix3x3, Nullable, Vector3 } from "../../../types";
 
 /**
  *
@@ -53,7 +53,7 @@ export interface vtkAbstractManipulator extends vtkObject {
      * @param callData 
      * @param glRenderWindow 
      */
-    handleEvent(callData: any, glRenderWindow: vtkOpenGLRenderWindow): Vector3;
+    handleEvent(callData: any, glRenderWindow: vtkOpenGLRenderWindow): { worldCoords: Nullable<Vector3>, worldDirection?: Matrix3x3 };
 
     /* ------------------------------------------------------------------- */
 

Sources/Widgets/Widgets3D/AngleWidget/behavior.js
@@ -48,7 +48,7 @@ export default function widgetBehavior(publicAPI, model) {
       model.widgetState.getHandleList().length < MAX_POINTS &&
       manipulator
     ) {
-      const worldCoords = manipulator.handleEvent(
+      const { worldCoords } = manipulator.handleEvent(
         e,
         model._apiSpecificRenderWindow
       );
@@ -85,7 +85,7 @@ export default function widgetBehavior(publicAPI, model) {
       model.activeState.getActive() &&
       !ignoreKey(callData)
     ) {
-      const worldCoords = manipulator.handleEvent(
+      const { worldCoords } = manipulator.handleEvent(
         callData,
         model._apiSpecificRenderWindow
       );

commit 5931f6218686bb2147831a38443dbc9eb17c131c

fix(ImplicitPlaneWidget): regression due to handleEvent
Fix regression introduced by 9c7525e
due to change in return type of trackballManipulator.handleEvent()

Sources/Widgets/Widgets3D/ImplicitPlaneWidget/index.js
@@ -130,7 +130,7 @@ function widgetBehavior(publicAPI, model) {
   publicAPI.updateFromNormal = (callData) => {
     model.trackballManipulator.setWidgetNormal(model.activeState.getNormal());
 
-    const newNormal = model.trackballManipulator.handleEvent(
+    const { worldCoords: newNormal } = model.trackballManipulator.handleEvent(
       callData,
       model._apiSpecificRenderWindow
     );