950ea678206cdb036b3f61c40b915c1fbae5688c
Julien Finet <julien.finet@kitware.com>
2023-05-18 16:53:04 +0200

    feat(labelwidget): expose vtkLabelWidget and vtkSphereWidget to module

--- Sources/Widgets/Widgets3D/index.js
+++ Sources/Widgets/Widgets3D/index.js
@@ -4,12 +4,14 @@ import vtkEllipseWidget from './EllipseWidget';
 import vtkImageCroppingWidget from './ImageCroppingWidget';
 import vtkImplicitPlaneWidget from './ImplicitPlaneWidget';
 import vtkInteractiveOrientationWidget from './InteractiveOrientationWidget';
+import vtkLabelWidget from './LabelWidget';
 import vtkLineWidget from './LineWidget';
 import vtkPaintWidget from './PaintWidget';
 import vtkPolyLineWidget from './PolyLineWidget';
 import vtkRectangleWidget from './RectangleWidget';
 import vtkResliceCursorWidget from './ResliceCursorWidget';
 import vtkShapeWidget from './ShapeWidget';
+import vtkSphereWidget from './SphereWidget';
 import vtkSplineWidget from './SplineWidget';
 
 export default {
@@ -19,11 +21,13 @@ export default {
   vtkImageCroppingWidget,
   vtkImplicitPlaneWidget,
   vtkInteractiveOrientationWidget,
+  vtkLabelWidget,
   vtkLineWidget,
   vtkPaintWidget,
   vtkPolyLineWidget,
   vtkRectangleWidget,
   vtkResliceCursorWidget,
   vtkShapeWidget,
+  vtkSphereWidget,
   vtkSplineWidget,
 };
