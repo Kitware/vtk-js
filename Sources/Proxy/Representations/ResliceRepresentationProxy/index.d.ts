ea5d996656adc8c0e116636a53464c7388dec6f5
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2023-08-31 18:08:06 -0400

    feat(ResliceRepresentationProxy): add outline to slice geometry
    Some applications may want to highlight a the slicing polygon geometry
    with a colored outline (specially to identify a slice across multiple
    views). Upgrade the representation proxy to allow for optional
    rendering of such an outline. Default visibility is off.
    Exposed properties include `outlineColor`, `outlineVisibility`, and
    `outlineLineWidth`.

--- Sources/Proxy/Representations/ResliceRepresentationProxy/index.d.ts
+++ Sources/Proxy/Representations/ResliceRepresentationProxy/index.d.ts
@@ -1,6 +1,7 @@
 import type vtkPolyData from '../../../Common/DataModel/PolyData';
 import type vtkPlane from '../../../Common/DataModel/Plane';
 import vtkAbstractRepresentationProxy from '../../Core/AbstractRepresentationProxy';
+import { RGBColor } from '../../../types';
 
 export interface vtkResliceRepresentationProxy
   extends vtkAbstractRepresentationProxy {
@@ -15,6 +16,12 @@ export interface vtkResliceRepresentationProxy
   getWindowLevel(): number;
   setInterpolationType(type: number): boolean;
   getInterpolationType(): number;
+  setOutlineLineWidth(lineWidth: number): boolean;
+  getOutlineLineWidth(): number;
+  setOutlineColor(color: RGBColor): boolean;
+  getOutlineColor(): RGBColor;
+  setOutlineVisibility(visibility: boolean): boolean;
+  getOutlineVisibility(): boolean;
   setSlabType(type: number): boolean;
   getSlabtype(): number;
   setSlicePlane(plane: vtkPlane): boolean;
