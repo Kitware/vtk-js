bd8d45abe8346bf7c089c54d494d05ba4cb0d81a
Anne Haley <anne.haley@kitware.com>
2023-09-05 13:01:09 -0400

    refactor(PickerManipulator): Rename ActorPickManipulator -> PickerManipulator

---
+++ Sources/Widgets/Manipulators/PickerManipulator/index.d.ts
@@ -0,0 +1,37 @@
+import { IAbstractManipulatorInitialValues, vtkAbstractManipulator } from "../AbstractManipulator";
+
+/**
+ *
+ */
+export interface IPickerManipulatorInitialValues extends IAbstractManipulatorInitialValues {
+
+}
+
+export interface vtkPickerManipulator extends vtkAbstractManipulator {
+
+}
+
+
+/**
+ * Method use to decorate a given object (publicAPI+model) with vtkPickerManipulator characteristics.
+ *
+ * @param publicAPI object on which methods will be bounds (public)
+ * @param model object on which data structure will be bounds (protected)
+ * @param {IPickerManipulatorInitialValues} [initialValues] (default: {})
+ */
+export function extend(publicAPI: object, model: object, initialValues?: IPickerManipulatorInitialValues): void;
+
+/**
+ * Method use to create a new instance of vtkPickerManipulator
+ */
+export function newInstance(initialValues?: IPickerManipulatorInitialValues): vtkPickerManipulator;
+
+
+/**
+ * vtkPickerManipulator.
+ */
+export declare const vtkPickerManipulator: {
+	newInstance: typeof newInstance,
+	extend: typeof extend,
+};
+export default vtkPickerManipulator;
