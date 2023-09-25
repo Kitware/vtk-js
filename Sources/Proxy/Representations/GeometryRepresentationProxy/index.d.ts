4723a645de9a32ac885eaf0d17b31ff6a7a64359
Shreeraj Jadhav <shreerajjadhav@gmail.com>
2023-09-25 15:42:05 -0400

    fix(GeometryRepresentationProxy): setOpacity types and intends
    Fix parameter type for getOpacity/setOpacity.
    Replace all indentation spaces with tabspaces.

--- Sources/Proxy/Representations/GeometryRepresentationProxy/index.d.ts
+++ Sources/Proxy/Representations/GeometryRepresentationProxy/index.d.ts
@@ -2,34 +2,34 @@ import { RGBColor } from "../../../types";
 import vtkAbstractRepresentationProxy from '../../Core/AbstractRepresentationProxy';
 
 export interface vtkGeometryRepresentationProxy
-  extends vtkAbstractRepresentationProxy {
+	extends vtkAbstractRepresentationProxy {
 
-  /**
-   * 
-   * @param representation a string that describes what representation to use for the explicit geometry.
-   *                       possible values are 'Surface with edges', 'Surface' (default), 'Wireframe',
-   *                       and 'Points'. 
-   */
-  setRepresentation(representation: string): boolean;
-  getRepresentation(): string;
+	/**
+	*
+	* @param representation a string that describes what representation to use for the explicit geometry.
+	*                       possible values are 'Surface with edges', 'Surface' (default), 'Wireframe',
+	*                       and 'Points'.
+	*/
+	setRepresentation(representation: string): boolean;
+	getRepresentation(): string;
 
-  // proxy property mappings
-  getColor(): RGBColor;
-  setColor(color: RGBColor): boolean;
+	// proxy property mappings
+	getColor(): RGBColor;
+	setColor(color: RGBColor): boolean;
 	getInterpolateScalarsBeforeMapping(): boolean;
 	setInterpolateScalarsBeforeMapping(interpolateScalarsBeforeMapping: boolean): boolean;
-  setOpacity(opacity: boolean): boolean;
-  getOpacity(): boolean;
-  setVisibility(visible: boolean): boolean;
-  getVisibility(): boolean;
+	getOpacity(): number;
+	setOpacity(opacity: number): boolean;
+	getVisibility(): boolean;
+	setVisibility(visible: boolean): boolean;
 	getPointSize(): number;
 	setPointSize(pointSize: number): boolean;
-  getUseShadow(): boolean;
-  setUseShadow(lighting: boolean): boolean;
-  getUseBounds(): boolean;
-  setUseBounds(useBounds: boolean): boolean;
-  getLineWidth(): number;
-  setLineWidth(width: number): boolean;
+	getUseShadow(): boolean;
+	setUseShadow(lighting: boolean): boolean;
+	getUseBounds(): boolean;
+	setUseBounds(useBounds: boolean): boolean;
+	getLineWidth(): number;
+	setLineWidth(width: number): boolean;
 }
 
 export default vtkGeometryRepresentationProxy;
