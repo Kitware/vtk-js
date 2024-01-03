f61f646c1bc79aff11f2bf68ec37625feacbc93d
Julien Finet <julien.finet@kitware.com>
2024-01-03 21:27:14 +0100

    docs(examples): fix render window sizing of CubeAxes and SimpleCone examples
    vtkFullScreenRenderWindow or vtkGenericRenderWindow must be used for simplicity
    
    issue #2973

--- Examples/Geometry/SimpleCone/index.js
+++ Examples/Geometry/SimpleCone/index.js
@@ -6,19 +6,16 @@ import '@kitware/vtk.js/Rendering/Profiles/Geometry';
 import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
 import vtkConeSource from '@kitware/vtk.js/Filters/Sources/ConeSource';
 import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkOpenGLRenderWindow from '@kitware/vtk.js/Rendering/OpenGL/RenderWindow';
-import vtkRenderWindow from '@kitware/vtk.js/Rendering/Core/RenderWindow';
-import vtkRenderWindowInteractor from '@kitware/vtk.js/Rendering/Core/RenderWindowInteractor';
-import vtkRenderer from '@kitware/vtk.js/Rendering/Core/Renderer';
-import vtkInteractorStyleTrackballCamera from '@kitware/vtk.js/Interaction/Style/InteractorStyleTrackballCamera';
+import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
 
 // ----------------------------------------------------------------------------
 // Standard rendering code setup
 // ----------------------------------------------------------------------------
 
-const renderWindow = vtkRenderWindow.newInstance();
-const renderer = vtkRenderer.newInstance({ background: [0.2, 0.3, 0.4] });
-renderWindow.addRenderer(renderer);
+const renderWindow = vtkFullScreenRenderWindow.newInstance({
+  background: [0.2, 0.3, 0.4],
+});
+const renderer = renderWindow.getRenderer();
 
 // ----------------------------------------------------------------------------
 // Simple pipeline ConeSource --> Mapper --> Actor
@@ -38,40 +35,3 @@ actor.setMapper(mapper);
 
 renderer.addActor(actor);
 renderer.resetCamera();
-
-// ----------------------------------------------------------------------------
-// Use OpenGL as the backend to view the all this
-// ----------------------------------------------------------------------------
-
-const openGLRenderWindow = vtkOpenGLRenderWindow.newInstance();
-renderWindow.addView(openGLRenderWindow);
-
-// ----------------------------------------------------------------------------
-// Create a div section to put this into
-// ----------------------------------------------------------------------------
-
-const container = document.createElement('div');
-document.querySelector('body').appendChild(container);
-openGLRenderWindow.setContainer(container);
-
-// ----------------------------------------------------------------------------
-// Capture size of the container and set it to the renderWindow
-// ----------------------------------------------------------------------------
-
-const { width, height } = container.getBoundingClientRect();
-openGLRenderWindow.setSize(width, height);
-
-// ----------------------------------------------------------------------------
-// Setup an interactor to handle mouse events
-// ----------------------------------------------------------------------------
-
-const interactor = vtkRenderWindowInteractor.newInstance();
-interactor.setView(openGLRenderWindow);
-interactor.initialize();
-interactor.bindEvents(container);
-
-// ----------------------------------------------------------------------------
-// Setup interactor style to use
-// ----------------------------------------------------------------------------
-
-interactor.setInteractorStyle(vtkInteractorStyleTrackballCamera.newInstance());
