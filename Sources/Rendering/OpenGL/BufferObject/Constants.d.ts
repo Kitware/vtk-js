81a8399ffbb862d0bd1067f5d5c05b01eea72246
hjunqq <qhjun@live.com>
2024-01-11 16:51:02 +0800

    docs(ts): Add typescript definitions

---
+++ Sources/Rendering/OpenGL/BufferObject/Constants.d.ts
@@ -0,0 +1,11 @@
+export declare enum ObjectType {
+  ARRAY_BUFFER = 0,
+  ELEMENT_ARRAY_BUFFER = 1,
+  TEXTURE_BUFFER = 2,
+}
+
+declare const _default: {
+  ObjectType: typeof ObjectType;
+};
+
+export default _default;
--- Sources/Rendering/OpenGL/BufferObject/index.d.ts
+++ Sources/Rendering/OpenGL/BufferObject/index.d.ts
@@ -1,8 +1,87 @@
+import { ObjectType } from './Constants';
+import { vtkAlgorithm, vtkObject } from '../../../interfaces';
 
-export interface BufferObject {
-    objectType?:null;
-    context?:null;
-    allocatedGPUMemoryInBytes?:0;
+/**
+ * Interface for initial values of BufferObject
+ */
+export interface IBufferObjectInitialValues {
+  objectType: ObjectType;
+  context?: WebGLRenderingContext | WebGL2RenderingContext;
+  allocatedGPUMemoryInBytes: number;
 }
 
-export default BufferObject;
+/**
+ * Base type for vtkOpenGLBufferObject excluding the 'set' method from vtkObject
+ */
+type vtkOpenGLBufferObjectBase = Omit<vtkObject, 'set'> & vtkAlgorithm;
+
+/**
+ * Interface for OpenGL Buffer Object
+ */
+export interface vtkOpenGLBufferObject extends vtkOpenGLBufferObjectBase {
+  /**
+   * Uploads data to the buffer object.
+   * @param data The data to be uploaded.
+   * @param type The type of the data.
+   * @returns {boolean} Whether the upload was successful.
+   */
+  upload(data: any, type: any): boolean;
+
+  /**
+   * Binds the buffer object.
+   * @returns {boolean} Whether the binding was successful.
+   */
+  bind(): boolean;
+
+  /**
+   * Releases the buffer object.
+   * @returns {boolean} Whether the release was successful.
+   */
+  release(): boolean;
+
+  /**
+   * Releases graphics resources associated with the buffer object.
+   */
+  releaseGraphicsResources(): void;
+
+  /**
+   * Sets the OpenGL render window.
+   * @param renWin The render window to set.
+   */
+  setOpenGLRenderWindow(renWin: any): void;
+
+  /**
+   * Retrieves the error message, if any.
+   * @returns {string} The error message.
+   */
+  getError(): string;
+
+}
+
+/**
+ * Extends the given object with the properties and methods of vtkOpenGLBufferObject.
+ * @param publicAPI The public API to extend.
+ * @param model The model to extend.
+ * @param initialValues The initial values to apply.
+ */
+export function extend(
+  publicAPI: object,
+  model: object,
+  initialValues?: IBufferObjectInitialValues
+): void;
+
+/**
+ * Creates a new instance of vtkOpenGLBufferObject with the given initial values.
+ * @param initialValues The initial values to use.
+ * @returns {vtkOpenGLBufferObject} The new instance.
+ */
+export function newInstance(initialValues?: IBufferObjectInitialValues): vtkOpenGLBufferObject;
+
+/**
+ * Object containing the newInstance and extend functions for vtkOpenGLBufferObject.
+ */
+export declare const vtkOpenGLBufferObject: {
+  newInstance: typeof newInstance;
+  extend: typeof extend;
+};
+export default vtkOpenGLBufferObject;
