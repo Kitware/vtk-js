5f98f5581e94cc20e784f6ea215dcaa439d2b559
Forrest <forrest.li@kitware.com>
2024-02-06 17:39:58 -0500

    chore(rewrite-imports): update regex lastIndex

--- Utilities/build/rewrite-imports.js
+++ Utilities/build/rewrite-imports.js
@@ -1,7 +1,7 @@
 module.exports = (code, replaceFunc) => {
-  const importRegex = /(?:import|from) ['"]([^'"]*)['"]/g;
+  const importRegex = /(?:import|from)\s+['"]([^'"]*)['"]/g;
   let m;
-  while ((m = importRegex.exec(code)) !== null) {
+  while ((m = importRegex.exec(code)) != null) {
     // This is necessary to avoid infinite loops with zero-width matches
     if (m.index === importRegex.lastIndex) {
       importRegex.lastIndex++;
@@ -9,7 +9,10 @@ module.exports = (code, replaceFunc) => {
 
     if (m[1].startsWith('../') || m[1].startsWith('./')) {
       const importPath = replaceFunc(m[1]);
+      const origLen = code.length;
       code = code.replace(m[0], `from '${importPath}'`);
+      const lenDiff = code.length - origLen;
+      importRegex.lastIndex += lenDiff;
     }
   }
   return code;
