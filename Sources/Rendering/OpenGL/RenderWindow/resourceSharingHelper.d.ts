70703a1513dea92a7011c38ec6c0bd012276d142
Thibault Bruyère <thibault.bruyere@kitware.com>
2024-08-07 16:08:31 +0200

    chore: Reformat typescript header

--- Sources/Rendering/OpenGL/RenderWindow/resourceSharingHelper.d.ts
+++ Sources/Rendering/OpenGL/RenderWindow/resourceSharingHelper.d.ts
@@ -1,6 +1,6 @@
 import vtkImageData from '../../../Common/DataModel/ImageData';
 import vtkDataArray from '../../../Common/Core/DataArray';
-import { vtkObject } from "../../../interfaces";
+import { vtkObject } from '../../../interfaces';
 
 /**
  * Compute and return a hash than can be used for caching resources
@@ -9,7 +9,11 @@ import { vtkObject } from "../../../interfaces";
  * @param useIndependentComponents A boolean taken from the image or volume property, using getIndependentComponents()
  * @param numberOfComponents Taken from the data array, using getNumberOfComponents()
  */
-export function getTransferFunctionHash(transferFunction: vtkObject | undefined, useIndependentComponents: boolean, numberOfComponents: number): string;
+export function getTransferFunctionHash(
+  transferFunction: vtkObject | undefined,
+  useIndependentComponents: boolean,
+  numberOfComponents: number
+): string;
 
 /**
  * Compute and return a hash than can be used for caching resources
@@ -17,10 +21,14 @@ export function getTransferFunctionHash(transferFunction: vtkObject | undefined,
  * @param image The image data that contains the scalars
  * @param scalars The scalars used for rendering
  */
-export function getImageDataHash(image: vtkImageData, scalars: vtkDataArray): string;
+export function getImageDataHash(
+  image: vtkImageData,
+  scalars: vtkDataArray
+): string;
 
 declare const defaultExport: {
   getTransferFunctionHash: typeof getTransferFunctionHash;
+  getImageDataHash: typeof getImageDataHash;
 };
 
 export default defaultExport;
