be98c23560d7366ca13aca2d8e5379134e3803a2
Thibault Bruyère <thibault.bruyere@kitware.com>
2024-09-17 10:25:12 +0200

    feat: Add WebXR to default Rendering export
    WebXR wasn't exposed in the file sent by CDNs

--- Sources/Rendering/index.js
+++ Sources/Rendering/index.js
@@ -3,6 +3,7 @@ import Misc from './Misc';
 import OpenGL from './OpenGL';
 import SceneGraph from './SceneGraph';
 import WebGPU from './WebGPU';
+import WebXR from './WebXR';
 
 export default {
   Core,
@@ -10,4 +11,5 @@ export default {
   OpenGL,
   SceneGraph,
   WebGPU,
+  WebXR,
 };
