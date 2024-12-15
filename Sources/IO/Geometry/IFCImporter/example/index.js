ff1cf8c9d424f7269acd1cfa777147c06a834282
Adnane Belmadiaf <adnane002@gmail.com>
2024-12-15 16:38:00 +0100

    fix(IFCImporter): fix normals
    fix #3189

--- Sources/IO/Geometry/IFCImporter/example/index.js
+++ Sources/IO/Geometry/IFCImporter/example/index.js
@@ -6,18 +6,33 @@ import '@kitware/vtk.js/Rendering/Profiles/Geometry';
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
 import vtkResourceLoader from '@kitware/vtk.js/IO/Core/ResourceLoader';
 import vtkIFCImporter from '@kitware/vtk.js/IO/Geometry/IFCImporter';
+import vtkURLExtract from '@kitware/vtk.js/Common/Core/URLExtract';
+
+import controlPanel from './controller.html';
 
 // ----------------------------------------------------------------------------
 // Example code
 // ----------------------------------------------------------------------------
 
+const userParams = vtkURLExtract.extractURLParameters();
+const url =
+  userParams.fileURL ||
+  'https://raw.githubusercontent.com/ThatOpen/engine_web-ifc/refs/heads/main/tests/ifcfiles/public/duplex.ifc';
+const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
+fullScreenRenderer.addController(controlPanel);
+
+if (userParams.mergeGeometries === undefined) {
+  userParams.mergeGeometries = true;
+}
+const mergeGeometriesCheckbox = document.querySelector('#mergeGeometries');
+mergeGeometriesCheckbox.checked = Boolean(userParams.mergeGeometries);
+
 const importer = vtkIFCImporter.newInstance({
-  mergeGeometries: true,
+  mergeGeometries: mergeGeometriesCheckbox.checked,
 });
 
 // ----------------------------------------------------------------------------
 function update() {
-  const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance();
   const renderer = fullScreenRenderer.getRenderer();
   const renderWindow = fullScreenRenderer.getRenderWindow();
 
@@ -41,9 +56,9 @@ vtkResourceLoader
     vtkIFCImporter.setIFCAPI(window.WebIFC);
 
     // Trigger data download
-    importer
-      .setUrl(
-        'https://raw.githubusercontent.com/ThatOpen/engine_web-ifc/refs/heads/main/tests/ifcfiles/public/duplex.ifc'
-      )
-      .then(update);
+    importer.setUrl(url).then(update);
   });
+
+mergeGeometriesCheckbox.addEventListener('change', (evt) => {
+  window.location = `?mergeGeometries=${evt.target.checked}`;
+});
