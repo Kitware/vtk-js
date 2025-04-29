beb5520eee444a2171171f1e456796a1bf1180a4
Wenqi He <galmungral@gmail.com>
2025-04-29 15:46:27 -0500

    fix: add missing default export

--- Sources/Common/Core/URLExtract/index.d.ts
+++ Sources/Common/Core/URLExtract/index.d.ts
@@ -13,3 +13,12 @@ export function extractURLParameters(
  * @param {String} str The type value as string.
  */
 export function toNativeType(str: string): void;
+
+declare const _default: {
+  toNativeType(str: string): any;
+  extractURLParameters(
+    castToNativeType?: boolean,
+    query?: string
+  ): Record<string, any>;
+};
+export default _default;
