9ab32e5eeba9c703eb205d78cd32c15bfad883ea
Julien Finet <julien.finet@kitware.com>
2025-08-25 16:13:06 +0200

    fix(prop3d): fix vtkProp3D bounds
    vtkProp3D expects mapper GetBounds() - hence vtkPolyData::GetBounds() - to return a unique array
    each time GetBounds() is called. If a reference array is expected, then GetBoundsByReference()
    should be used instead.

--- Sources/Common/Core/Points/index.js
+++ Sources/Common/Core/Points/index.js
@@ -1,16 +1,17 @@
 import macro from 'vtk.js/Sources/macros';
 import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
 import { VtkDataTypes } from 'vtk.js/Sources/Common/Core/DataArray/Constants';
+import * as vtkMath from 'vtk.js/Sources/Common/Core/Math';
 
 const { vtkErrorMacro } = macro;
-
-const INVALID_BOUNDS = [1, -1, 1, -1, 1, -1];
-
 // ----------------------------------------------------------------------------
 // vtkPoints methods
 // ----------------------------------------------------------------------------
 
 function vtkPoints(publicAPI, model) {
+  // Keep track of modified time for bounds computation
+  let boundMTime = 0;
+
   // Set our className
   model.classHierarchy.push('vtkPoints');
 
@@ -37,7 +38,24 @@ function vtkPoints(publicAPI, model) {
 
   publicAPI.insertPoint = (ptId, point) => publicAPI.insertTuple(ptId, point);
 
+  const superGetBounds = publicAPI.getBounds;
   publicAPI.getBounds = () => {
+    if (boundMTime < model.mtime) {
+      publicAPI.computeBounds();
+    }
+    return superGetBounds();
+  };
+
+  const superGetBoundsByReference = publicAPI.getBoundsByReference;
+  publicAPI.getBoundsByReference = () => {
+    if (boundMTime < model.mtime) {
+      publicAPI.computeBounds();
+    }
+    return superGetBoundsByReference();
+  };
+
+  // Trigger the computation of bounds
+  publicAPI.computeBounds = () => {
     if (publicAPI.getNumberOfComponents() === 3) {
       const xRange = publicAPI.getRange(0);
       model.bounds[0] = xRange[0];
@@ -48,30 +66,23 @@ function vtkPoints(publicAPI, model) {
       const zRange = publicAPI.getRange(2);
       model.bounds[4] = zRange[0];
       model.bounds[5] = zRange[1];
-      return model.bounds;
-    }
-
-    if (publicAPI.getNumberOfComponents() !== 2) {
+    } else if (publicAPI.getNumberOfComponents() === 2) {
+      const xRange = publicAPI.getRange(0);
+      model.bounds[0] = xRange[0];
+      model.bounds[1] = xRange[1];
+      const yRange = publicAPI.getRange(1);
+      model.bounds[2] = yRange[0];
+      model.bounds[3] = yRange[1];
+      model.bounds[4] = 0;
+      model.bounds[5] = 0;
+    } else {
       vtkErrorMacro(
         `getBounds called on an array with components of ${publicAPI.getNumberOfComponents()}`
       );
-      return INVALID_BOUNDS;
+      vtkMath.uninitializeBounds(model.bounds);
     }
-
-    const xRange = publicAPI.getRange(0);
-    model.bounds[0] = xRange[0];
-    model.bounds[1] = xRange[1];
-    const yRange = publicAPI.getRange(1);
-    model.bounds[2] = yRange[0];
-    model.bounds[3] = yRange[1];
-    model.bounds[4] = 0;
-    model.bounds[5] = 0;
-
-    return model.bounds;
+    boundMTime = macro.getCurrentGlobalMTime();
   };
-
-  // Trigger the computation of bounds
-  publicAPI.computeBounds = publicAPI.getBounds;
 
   // Initialize
   publicAPI.setNumberOfComponents(
@@ -96,6 +107,8 @@ export function extend(publicAPI, model, initialValues = {}) {
   Object.assign(model, DEFAULT_VALUES, initialValues);
 
   vtkDataArray.extend(publicAPI, model, initialValues);
+
+  macro.getArray(publicAPI, model, ['bounds'], 6);
   vtkPoints(publicAPI, model);
 }
 
