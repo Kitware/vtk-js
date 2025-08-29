604fa00ee50736ef12376418ef19b488f227bd46
Adnane Belmadiaf <adnane002@gmail.com>
2025-08-30 00:57:16 +0200

    feat(CleanPolyData): add vtkCleanPolyData

--- Sources/Filters/Core/index.js
+++ Sources/Filters/Core/index.js
@@ -1,7 +1,11 @@
+import vtkCleanPolyData from './CleanPolyData';
 import vtkCutter from './Cutter';
 import vtkPolyDataNormals from './PolyDataNormals';
+import vtkThresholdPoints from './ThresholdPoints';
 
 export default {
+  vtkCleanPolyData,
   vtkCutter,
   vtkPolyDataNormals,
+  vtkThresholdPoints,
 };
