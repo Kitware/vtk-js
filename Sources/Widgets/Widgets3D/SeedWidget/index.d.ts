75f9c7320f9753dc0f717e9bc465647f08d9f1c1
Julien Finet <julien.finet@kitware.com>
2025-11-09 22:12:24 +0100

    refactor(seedwidget): expose start and end interact convenient functions
    Demonstrate how to programmatically place a seed widget and to make the widget interactive.

--- Sources/Widgets/Widgets3D/SeedWidget/index.d.ts
+++ Sources/Widgets/Widgets3D/SeedWidget/index.d.ts
@@ -20,9 +20,26 @@ export interface vtkSeedWidgetState {
   getMoveHandle(): ISeedWidgetHandleState;
 }
 
-// The type of object returned by vtkWidgetManager.addWidget()
+// Object returned by vtkWidgetManager.addWidget().
+// One instance per view.
 export interface vtkSeedWidgetHandle {
+  /**
+   * Place the seed position.
+   * @param center Vector3 3D position
+   */
   setCenter(center: Vector3): void;
+
+  /**
+   * Turn the seed widget as interactive.
+   * @see vtkSeedWidgetHandle.endInteract
+   */
+  startInteract(): void;
+
+  /**
+   * Stop the seed widget to be interactive.
+   * @see vtkSeedWidgetHandle.endInteract
+   */
+  endInteract(): void;
 }
 
 export interface vtkSeedWidget {
