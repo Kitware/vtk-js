ae69223b26a567a4ed7e25cd7a5ea5fdc514eb81
Adnane Belmadiaf <adnane002@gmail.com>
2026-03-24 11:55:21 +0100

    feat(ClipPolyData): add vtkClipPolyData

---
+++ Sources/Common/DataModel/IncrementalPointLocator/index.d.ts
@@ -0,0 +1,45 @@
+import vtkAbstractPointLocator, {
+  IAbstractPointLocatorInitialValues,
+} from '../AbstractPointLocator';
+
+/**
+ * Initial values for vtkIncrementalPointLocator.
+ */
+export interface IIncrementalPointLocatorInitialValues
+  extends IAbstractPointLocatorInitialValues {}
+
+export interface vtkIncrementalPointLocator extends vtkAbstractPointLocator {}
+
+/**
+ * Method used to decorate a given object (publicAPI+model) with
+ * vtkIncrementalPointLocator characteristics.
+ *
+ * @param publicAPI object on which methods will be bounds (public)
+ * @param model object on which data structure will be bounds (protected)
+ * @param {IIncrementalPointLocatorInitialValues} [initialValues] (default: {})
+ */
+export function extend(
+  publicAPI: object,
+  model: object,
+  initialValues?: IIncrementalPointLocatorInitialValues
+): void;
+
+/**
+ * Method used to create a new instance of vtkIncrementalPointLocator.
+ *
+ * @param {IIncrementalPointLocatorInitialValues} [initialValues] for pre-setting some of its content
+ */
+export function newInstance(
+  initialValues?: IIncrementalPointLocatorInitialValues
+): vtkIncrementalPointLocator;
+
+/**
+ * vtkIncrementalPointLocator is the abstract incremental insertion variant of
+ * vtkAbstractPointLocator.
+ */
+export declare const vtkIncrementalPointLocator: {
+  newInstance: typeof newInstance;
+  extend: typeof extend;
+};
+
+export default vtkIncrementalPointLocator;
