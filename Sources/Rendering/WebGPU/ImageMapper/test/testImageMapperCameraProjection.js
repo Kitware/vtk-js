Kitware/vtk-js

commit 7fbc87a98155f1ad1301bf5ddab753070a4de81f

feat(WebGPU): add clipping plane support for polydata, volume and image
Implement WebGPU clipping planes in CellArrayMapper, VolumePassFSQ
and ImageMapper.

Sources/Rendering/WebGPU/ImageMapper/index.js
@@ -4,6 +4,12 @@ import * as macro from 'vtk.js/Sources/macros';
 import vtkWebGPUShaderCache from 'vtk.js/Sources/Rendering/WebGPU/ShaderCache';
 import vtkWebGPUFullScreenQuad from 'vtk.js/Sources/Rendering/WebGPU/FullScreenQuad';
 import vtkWebGPUUniformBuffer from 'vtk.js/Sources/Rendering/WebGPU/UniformBuffer';
+import {
+  addClipPlaneEntries,
+  getClippingPlaneEquationsInCoords,
+  getClipPlaneShaderChecks,
+  MAX_CLIPPING_PLANES,
+} from 'vtk.js/Sources/Rendering/WebGPU/Helpers/ClippingPlanes';
 
 import { Resolve } from 'vtk.js/Sources/Rendering/Core/Mapper/CoincidentTopologyHelper';
 import { InterpolationType } from 'vtk.js/Sources/Rendering/Core/ImageProperty/Constants';
@@ -13,9 +19,8 @@ import {
 } from 'vtk.js/Sources/Rendering/WebGPU/ImageMapper/Constants';
 import { registerOverride } from 'vtk.js/Sources/Rendering/WebGPU/ViewNodeFactory';
 
-// const { vtkErrorMacro } = macro;
+const { vtkErrorMacro } = macro;
 const { SlicingMode } = Constants;
-
 const imgFragTemplate = `
 //VTK::Renderer::Dec
 
@@ -24,6 +29,8 @@ const imgFragTemplate = `
 //VTK::TCoord::Dec
 
 //VTK::Image::Dec
+
+//VTK::Clip::Dec
 
 //VTK::RenderEncoder::Dec
 
@@ -36,6 +43,8 @@ fn main(
 //VTK::IOStructs::Output
 {
   var output: fragmentOutput;
+
+  //VTK::Clip::Impl
 
   //VTK::Image::Sample
 
@@ -215,7 +224,11 @@ function vtkWebGPUImageMapper(publicAPI, model) {
   publicAPI.render = () => {
     model.renderable.update();
 
-    model.currentInput = model.renderable.getInputData();
+    model.currentInput = model.renderable.getCurrentImage();
+    if (!model.currentInput) {
+      vtkErrorMacro('No input!');
+      return;
+    }
 
     publicAPI.prepareToDraw(model.WebGPURenderer.getRenderEncoder());
     model.renderEncoder.registerDrawCallback(model.pipeline, publicAPI.draw);
@@ -257,13 +270,15 @@ function vtkWebGPUImageMapper(publicAPI, model) {
     const utime = model.UBO.getSendTime();
     const actor = model.WebGPUImageSlice.getRenderable();
     const volMapr = actor.getMapper();
+    const clippingPlanesMTime = model.renderable.getClippingPlanesMTime();
     if (
       publicAPI.getMTime() > utime ||
       model.renderable.getMTime() > utime ||
-      actor.getProperty().getMTime() > utime
+      actor.getProperty().getMTime() > utime ||
+      clippingPlanesMTime > utime
     ) {
       // compute the SCTCMatrix
-      const image = volMapr.getInputData();
+      const image = volMapr.getCurrentImage();
       const center = model.WebGPURenderer.getStabilizedCenterByReference();
 
       mat4.identity(tmpMat4);
@@ -422,6 +437,23 @@ function vtkWebGPUImageMapper(publicAPI, model) {
       const cp = publicAPI.getCoincidentParameters();
       model.UBO.setValue('CoincidentFactor', cp.factor);
       model.UBO.setValue('CoincidentOffset', cp.offset);
+      model.UBO.setValue('NumClipPlanes', 0);
+
+      const numClipPlanes = model.renderable.getClippingPlanes().length;
+      model.UBO.setValue('NumClipPlanes', numClipPlanes);
+
+      if (numClipPlanes > 0) {
+        mat4.fromTranslation(tmp2Mat4, [-center[0], -center[1], -center[2]]);
+        getClippingPlaneEquationsInCoords(
+          model.renderable,
+          tmp2Mat4,
+          model.clipPlanes
+        );
+        for (let i = 0; i < numClipPlanes; i++) {
+          model.UBO.setArray(`ClipPlane${i}`, model.clipPlanes[i]);
+        }
+      }
+
       model.UBO.sendIfNeeded(model.device);
     }
   };
@@ -686,6 +718,7 @@ function vtkWebGPUImageMapper(publicAPI, model) {
   publicAPI.replaceShaderPosition = (hash, pipeline, vertexInput) => {
     const vDesc = pipeline.getShaderDescription('vertex');
     vDesc.addBuiltinOutput('vec4<f32>', '@builtin(position) Position');
+    vDesc.addOutput('vec4<f32>', 'vertexSC');
     let code = vDesc.getCode();
     const lines = [
       'var pos: vec4<f32> = mapperUBO.Origin +',
@@ -702,7 +735,8 @@ function vtkWebGPUImageMapper(publicAPI, model) {
       // Match the OpenGL coincident topology constant offset scale (~1 / 2^16 = 0.000016)
       'pos.z = clamp(pos.z - 0.000016 * mapperUBO.CoincidentOffset * pos.w, 0.0, pos.w);',
       'output.tcoordVS = tcoord;',
-      'output.Position = pos;'
+      'output.vertexSC = pos;',
+      'output.Position = rendererUBO.SCPCMatrix * pos;'
     );
     code = vtkWebGPUShaderCache.substitute(
       code,
@@ -1059,6 +1093,24 @@ function vtkWebGPUImageMapper(publicAPI, model) {
   };
   sr.set('replaceShaderImage', publicAPI.replaceShaderImage);
 
+  publicAPI.replaceShaderClip = (hash, pipeline, vertexInput) => {
+    const fDesc = pipeline.getShaderDescription('fragment');
+    let code = fDesc.getCode();
+    const clipPlaneChecks = getClipPlaneShaderChecks({
+      countName: 'mapperUBO.NumClipPlanes',
+      planePrefix: 'mapperUBO.ClipPlane',
+      positionName: 'input.vertexSC',
+    });
+
+    code = vtkWebGPUShaderCache.substitute(code, '//VTK::Clip::Impl', [
+      ...clipPlaneChecks,
+      '//VTK::Clip::Impl',
+    ]).result;
+
+    fDesc.setCode(code);
+  };
+  sr.set('replaceShaderClip', publicAPI.replaceShaderClip);
+
   publicAPI.replaceShaderCoincidentOffset = (hash, pipeline, vertexInput) => {
     const fDesc = pipeline.getShaderDescription('fragment');
     if (!fDesc) {
@@ -1117,6 +1169,8 @@ export function extend(publicAPI, model, initialValues = {}) {
   model.UBO.addEntry('Opacity', 'f32');
   model.UBO.addEntry('CoincidentFactor', 'f32');
   model.UBO.addEntry('CoincidentOffset', 'f32');
+  addClipPlaneEntries(model.UBO, 'ClipPlane');
+  model.UBO.addEntry('NumClipPlanes', 'u32');
 
   model.lutBuildTime = {};
   macro.obj(model.lutBuildTime, { mtime: 0 });
@@ -1132,6 +1186,9 @@ export function extend(publicAPI, model, initialValues = {}) {
   model.opacityTmpTable = new Float32Array(model.rowLength);
   model.colorLUTArray = null;
   model.opacityLUTArray = null;
+  model.clipPlanes = Array.from({ length: MAX_CLIPPING_PLANES }, () => [
+    0.0, 0.0, 0.0, 0.0,
+  ]);
 
   model.VBOBuildTime = {};
   macro.obj(model.VBOBuildTime);
@@ -1149,4 +1206,4 @@ export const newInstance = macro.newInstance(extend, 'vtkWebGPUImageMapper');
 export default { newInstance, extend };
 
 // Register ourself to WebGPU backend if imported
-registerOverride('vtkImageMapper', newInstance);
+registerOverride('vtkAbstractImageMapper', newInstance);

Sources/Rendering/Core/AbstractMapper/index.js
@@ -1,8 +1,30 @@
+import { mat4, vec4 } from 'gl-matrix';
 import macro from 'vtk.js/Sources/macros';
+
+const { vtkErrorMacro } = macro;
 
 // ----------------------------------------------------------------------------
 // vtkAbstractMapper methods
 // ----------------------------------------------------------------------------
+
+const tmpClipMatrix = new Float64Array(16);
+const tmpClipWorldPlane = new Float64Array(4);
+
+function getClipPlaneEquation(plane, out) {
+  const normal = plane.getNormalByReference();
+  const origin = plane.getOriginByReference();
+
+  out[0] = normal[0];
+  out[1] = normal[1];
+  out[2] = normal[2];
+  out[3] = -(
+    normal[0] * origin[0] +
+    normal[1] * origin[1] +
+    normal[2] * origin[2]
+  );
+
+  return out;
+}
 
 function vtkAbstractMapper(publicAPI, model) {
   model.classHierarchy.push('vtkAbstractMapper');
@@ -45,6 +67,14 @@ function vtkAbstractMapper(publicAPI, model) {
 
   publicAPI.getClippingPlanes = () => model.clippingPlanes;
 
+  publicAPI.getClippingPlanesMTime = () => {
+    let mtime = 0;
+    for (let i = 0; i < model.clippingPlanes.length; i++) {
+      mtime = Math.max(mtime, model.clippingPlanes[i].getMTime());
+    }
+    return mtime;
+  };
+
   publicAPI.setClippingPlanes = (planes) => {
     if (!planes) {
       return;
@@ -59,34 +89,32 @@ function vtkAbstractMapper(publicAPI, model) {
     }
   };
 
-  publicAPI.getClippingPlaneInDataCoords = (propMatrix, i, hnormal) => {
+  publicAPI.getClippingPlaneInCoords = (worldToCoords, i, hnormal) => {
+    if (i < 0 || i >= model.clippingPlanes?.length) {
+      vtkErrorMacro(`Clipping plane index ${i} is out of range.`);
+      return undefined;
+    }
+    const outHNormal = hnormal || new Float64Array(4);
+    getClipPlaneEquation(model.clippingPlanes[i], tmpClipWorldPlane);
+    mat4.invert(tmpClipMatrix, worldToCoords);
+    mat4.transpose(tmpClipMatrix, tmpClipMatrix);
+    vec4.transformMat4(outHNormal, tmpClipWorldPlane, tmpClipMatrix);
+    return outHNormal;
+  };
+
+  publicAPI.getClippingPlaneInDataCoords = (coordsToWorld, i, hnormal) => {
     const clipPlanes = model.clippingPlanes;
-    const mat = propMatrix;
 
     if (clipPlanes) {
       const n = clipPlanes.length;
       if (i >= 0 && i < n) {
-        // Get the plane
-        const plane = clipPlanes[i];
-        const normal = plane.getNormal();
-        const origin = plane.getOrigin();
-
-        // Compute the plane equation
-        const v1 = normal[0];
-        const v2 = normal[1];
-        const v3 = normal[2];
-        const v4 = -(v1 * origin[0] + v2 * origin[1] + v3 * origin[2]);
-
-        // Transform normal from world to data coords
-        hnormal[0] = v1 * mat[0] + v2 * mat[4] + v3 * mat[8] + v4 * mat[12];
-        hnormal[1] = v1 * mat[1] + v2 * mat[5] + v3 * mat[9] + v4 * mat[13];
-        hnormal[2] = v1 * mat[2] + v2 * mat[6] + v3 * mat[10] + v4 * mat[14];
-        hnormal[3] = v1 * mat[3] + v2 * mat[7] + v3 * mat[11] + v4 * mat[15];
+        getClipPlaneEquation(clipPlanes[i], tmpClipWorldPlane);
+        vec4.transformMat4(hnormal, tmpClipWorldPlane, coordsToWorld);
 
         return;
       }
     }
-    macro.vtkErrorMacro(`Clipping plane index ${i} is out of range.`);
+    vtkErrorMacro(`Clipping plane index ${i} is out of range.`);
   };
 }
 

Sources/Rendering/WebGPU/CellArrayMapper/index.js
@@ -13,6 +13,12 @@ import vtkWebGPUShaderCache from 'vtk.js/Sources/Rendering/WebGPU/ShaderCache';
 import vtkWebGPUUniformBuffer from 'vtk.js/Sources/Rendering/WebGPU/UniformBuffer';
 import vtkWebGPUSimpleMapper from 'vtk.js/Sources/Rendering/WebGPU/SimpleMapper';
 import vtkWebGPUTypes from 'vtk.js/Sources/Rendering/WebGPU/Types';
+import {
+  addClipPlaneEntries,
+  getClippingPlaneEquationsInCoords,
+  getClipPlaneShaderChecks,
+  MAX_CLIPPING_PLANES,
+} from 'vtk.js/Sources/Rendering/WebGPU/Helpers/ClippingPlanes';
 
 const { Resolve } = CoincidentTopologyHelper;
 const { FieldAssociations } = vtkDataSet;
@@ -21,7 +27,6 @@ const { Representation } = vtkProperty;
 const { ScalarMode } = vtkMapper;
 const { CoordinateSystem } = vtkProp;
 const { DisplayLocation } = vtkProperty2D;
-
 const vtkWebGPUPolyDataVS = `
 //VTK::Renderer::Dec
 
@@ -384,6 +389,8 @@ fn main(
 }
 `;
 
+const tmp2Mat4 = new Float64Array(16);
+
 function isEdges(hash) {
   // edge pipelines have "edge" in them
   return hash.indexOf('edge') >= 0;
@@ -436,13 +443,15 @@ function vtkWebGPUCellArrayMapper(publicAPI, model) {
   publicAPI.updateUBO = () => {
     const actor = model.WebGPUActor.getRenderable();
     const ppty = actor.getProperty();
+    const clippingPlanesMTime = model.renderable.getClippingPlanesMTime();
     const backfaceProperty = actor.getBackfaceProperty?.() ?? ppty;
     const utime = model.UBO.getSendTime();
     if (
       publicAPI.getMTime() <= utime &&
       ppty.getMTime() <= utime &&
       backfaceProperty.getMTime() <= utime &&
-      model.renderable.getMTime() <= utime
+      model.renderable.getMTime() <= utime &&
+      clippingPlanesMTime <= utime
     ) {
       return;
     }
@@ -555,6 +564,24 @@ function vtkWebGPUCellArrayMapper(publicAPI, model) {
     const cp = publicAPI.getCoincidentParameters();
     model.UBO.setValue('CoincidentFactor', cp.factor);
     model.UBO.setValue('CoincidentOffset', cp.offset);
+    model.UBO.setValue('NumClipPlanes', 0);
+
+    if (!model.is2D && model.useRendererMatrix) {
+      const center = model.WebGPURenderer.getStabilizedCenterByReference();
+      mat4.fromTranslation(tmp2Mat4, [-center[0], -center[1], -center[2]]);
+      const numClipPlanes = getClippingPlaneEquationsInCoords(
+        model.renderable,
+        tmp2Mat4,
+        model.clipPlanes
+      );
+      model.UBO.setValue('NumClipPlanes', numClipPlanes);
+
+      if (numClipPlanes > 0) {
+        for (let i = 0; i < numClipPlanes; i++) {
+          model.UBO.setArray(`ClipPlane${i}`, model.clipPlanes[i]);
+        }
+      }
+    }
 
     // Only send if needed
     model.UBO.sendIfNeeded(model.WebGPURenderWindow.getDevice());
@@ -661,9 +688,11 @@ function vtkWebGPUCellArrayMapper(publicAPI, model) {
     const vDesc = pipeline.getShaderDescription('vertex');
     vDesc.addBuiltinOutput('vec4<f32>', '@builtin(position) Position');
     if (!vDesc.hasOutput('vertexVC')) vDesc.addOutput('vec4<f32>', 'vertexVC');
+    if (!vDesc.hasOutput('vertexSC')) vDesc.addOutput('vec4<f32>', 'vertexSC');
     let code = vDesc.getCode();
     if (model.useRendererMatrix) {
       code = vtkWebGPUShaderCache.substitute(code, '//VTK::Position::Impl', [
+        '    output.vertexSC = mapperUBO.BCSCMatrix * vec4<f32>(vertexBC.xyz, 1.0);',
         '    var pCoord: vec4<f32> = rendererUBO.SCPCMatrix*mapperUBO.BCSCMatrix*vertexBC;',
         '    output.vertexVC = rendererUBO.SCVCMatrix * mapperUBO.BCSCMatrix * vec4<f32>(vertexBC.xyz, 1.0);',
         '//VTK::Position::Impl',
@@ -711,6 +740,19 @@ function vtkWebGPUCellArrayMapper(publicAPI, model) {
       '    output.Position = pCoord;',
     ]).result;
     vDesc.setCode(code);
+
+    const fDesc = pipeline.getShaderDescription('fragment');
+    code = fDesc.getCode();
+    const clipPlaneChecks = getClipPlaneShaderChecks({
+      countName: 'mapperUBO.NumClipPlanes',
+      planePrefix: 'mapperUBO.ClipPlane',
+      positionName: 'input.vertexSC',
+    });
+    code = vtkWebGPUShaderCache.substitute(code, '//VTK::Position::Impl', [
+      ...clipPlaneChecks,
+      '//VTK::Position::Impl',
+    ]).result;
+    fDesc.setCode(code);
   };
   model.shaderReplacements.set(
     'replaceShaderPosition',
@@ -1560,7 +1602,6 @@ export function extend(publicAPI, model, initiaLalues = {}) {
   model.vertexShaderTemplate = vtkWebGPUPolyDataVS;
 
   model._tmpMat3 = mat3.identity(new Float64Array(9));
-  model._tmpMat4 = mat4.identity(new Float64Array(16));
 
   // UBO
   model.UBO = vtkWebGPUUniformBuffer.newInstance({ label: 'mapperUBO' });
@@ -1602,6 +1643,8 @@ export function extend(publicAPI, model, initiaLalues = {}) {
   model.UBO.addEntry('ClipNear', 'f32');
   model.UBO.addEntry('ClipFar', 'f32');
   model.UBO.addEntry('Time', 'u32');
+  addClipPlaneEntries(model.UBO, 'ClipPlane');
+  model.UBO.addEntry('NumClipPlanes', 'u32');
 
   // Build VTK API
   macro.setGet(publicAPI, model, [
@@ -1614,6 +1657,9 @@ export function extend(publicAPI, model, initiaLalues = {}) {
   ]);
 
   model.textures = [];
+  model.clipPlanes = Array.from({ length: MAX_CLIPPING_PLANES }, () => [
+    0.0, 0.0, 0.0, 0.0,
+  ]);
 
   // Object methods
   vtkWebGPUCellArrayMapper(publicAPI, model);

commit 56c856803ec9e611753fae88ba697c9cf9c344e0

fix(WebGPU): apply the view-projection matrix once in ImageMapper
The generated vertex shader multiplied by rendererUBO.SCPCMatrix twice
(regression from 7fbc87a98), pushing the doubly-projected slice quad
outside the clip volume for close-in cameras, and stored a clip-space
position in vertexSC, which clip planes expect in stabilized
coordinates. Capture vertexSC before projecting and project once.

Adds a WebGPU regression test and enables the WebGPU adapter for
headless Chromium test runs.

Sources/Rendering/WebGPU/ImageMapper/index.js
@@ -665,12 +665,14 @@ function vtkWebGPUImageMapper(publicAPI, model) {
       lines.push('var tcoord : vec3<f32> = (mapperUBO.SCTCMatrix * pos).xyz;');
     }
     lines.push(
+      // Clip planes are evaluated in stabilized coordinates, so capture the
+      // position before it is projected.
+      'output.vertexSC = pos;',
       'pos = rendererUBO.SCPCMatrix * pos;',
       // Match the OpenGL coincident topology constant offset scale (~1 / 2^16 = 0.000016)
       'pos.z = clamp(pos.z - 0.000016 * mapperUBO.CoincidentOffset * pos.w, 0.0, pos.w);',
       'output.tcoordVS = tcoord;',
-      'output.vertexSC = pos;',
-      'output.Position = rendererUBO.SCPCMatrix * pos;'
+      'output.Position = pos;'
     );
     code = vtkWebGPUShaderCache.substitute(
       code,

vitest.config.js
@@ -19,6 +19,15 @@ const chromiumArgs = ci
   ? ['--no-sandbox', '--enable-unsafe-swiftshader', '--use-angle=swiftshader']
   : [];
 
+if (webGPU) {
+  // Headless Chromium does not expose a WebGPU adapter by default.
+  chromiumArgs.push('--enable-unsafe-webgpu');
+  if (ci) {
+    // No real GPU on CI: use Dawn's software (SwiftShader) WebGPU adapter.
+    chromiumArgs.push('--use-webgpu-adapter=swiftshader');
+  }
+}
+
 const firefox = {
   browser: 'firefox',
   headless: false, // supported Vitest Browser option; xvfb-run supplies the display on CI

Sources/Rendering/WebGPU/ImageMapper/test/testImageMapperCameraProjection.js
@@ -1,0 +1,106 @@
+import { it, expect } from 'vitest';
+
+import testUtils from 'vtk.js/Sources/Testing/testUtils';
+import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
+import vtkImageMapper from 'vtk.js/Sources/Rendering/Core/ImageMapper';
+import vtkImageSlice from 'vtk.js/Sources/Rendering/Core/ImageSlice';
+import vtkRenderer from 'vtk.js/Sources/Rendering/Core/Renderer';
+import vtkRenderWindow from 'vtk.js/Sources/Rendering/Core/RenderWindow';
+import 'vtk.js/Sources/Rendering/Misc/RenderingAPIs';
+
+// Fraction of pixels in a captured data URL brighter than the given cutoff.
+async function computeBrightFraction(dataURL, cutoff = 100) {
+  const image = new Image();
+  image.src = dataURL;
+  await image.decode();
+
+  const canvas = document.createElement('canvas');
+  canvas.width = image.width;
+  canvas.height = image.height;
+  const context = canvas.getContext('2d');
+  context.drawImage(image, 0, 0);
+
+  const { data } = context.getImageData(0, 0, canvas.width, canvas.height);
+  let bright = 0;
+  const pixelCount = data.length / 4;
+  for (let i = 0; i < data.length; i += 4) {
+    if ((data[i] + data[i + 1] + data[i + 2]) / 3 > cutoff) {
+      bright++;
+    }
+  }
+  return bright / pixelCount;
+}
+
+it.skipIf(!__VTK_TEST_WEBGPU__)(
+  'Test WebGPU ImageMapper applies the view-projection matrix exactly once',
+  async () => {
+    const gc = testUtils.createGarbageCollector();
+
+    const container = document.querySelector('body');
+    const renderWindowContainer = gc.registerDOMElement(
+      document.createElement('div')
+    );
+    container.appendChild(renderWindowContainer);
+
+    const renderWindow = gc.registerResource(vtkRenderWindow.newInstance());
+    const renderer = gc.registerResource(vtkRenderer.newInstance());
+    renderWindow.addRenderer(renderer);
+    renderer.setBackground(0, 0, 0);
+
+    // A constant bright slice positioned away from the world origin, the way
+    // medical viewers place slices in patient space.
+    const dims = 64;
+    const sliceZ = 50;
+    const values = new Uint8Array(dims * dims).fill(255);
+    const scalars = vtkDataArray.newInstance({
+      name: 'Scalars',
+      values,
+      numberOfComponents: 1,
+    });
+    const imageData = gc.registerResource(vtkImageData.newInstance());
+    imageData.setDimensions(dims, dims, 1);
+    imageData.setOrigin(0, 0, sliceZ);
+    imageData.getPointData().setScalars(scalars);
+
+    const mapper = gc.registerResource(vtkImageMapper.newInstance());
+    mapper.setInputData(imageData);
+
+    const actor = gc.registerResource(vtkImageSlice.newInstance());
+    actor.getProperty().setColorWindow(255);
+    actor.getProperty().setColorLevel(127);
+    actor.setMapper(mapper);
+    renderer.addActor(actor);
+
+    // Parallel camera one unit in front of the slice, looking down +z. If
+    // the vertex shader applies the SCPC (view-projection) matrix more than
+    // once, the doubly-projected quad falls outside the clip volume and the
+    // slice silently disappears; applied once, it fills the viewport.
+    const center = (dims - 1) / 2;
+    const camera = renderer.getActiveCamera();
+    camera.setParallelProjection(true);
+    camera.setFocalPoint(center, center, sliceZ);
+    camera.setPosition(center, center, sliceZ - 1);
+    camera.setViewUp(0, -1, 0);
+    camera.setParallelScale(dims / 2);
+    camera.setClippingRange(0.01, 1000.01);
+
+    const view = gc.registerResource(renderWindow.newAPISpecificView('WebGPU'));
+    view.setContainer(renderWindowContainer);
+    renderWindow.addView(view);
+    view.setSize(128, 128);
+
+    const promise = view
+      .captureNextImage()
+      .then(async (image) => {
+        const brightFraction = await computeBrightFraction(image);
+        expect(
+          brightFraction,
+          'the slice must cover the viewport (a doubly-projected quad is clipped away and renders nothing)'
+        ).toBeGreaterThan(0.5);
+      })
+      .finally(gc.releaseResources);
+    renderWindow.render();
+    return promise;
+  }
+);