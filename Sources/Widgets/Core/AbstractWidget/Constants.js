d3fb0da8f925533ed9b008db4392ad84c8dadfa3
Julien Finet <julien.finet@kitware.com>
2026-07-16 18:23:01 +0200

    docs(Widgets): clarify priority value meaning

--- Sources/Widgets/Core/AbstractWidget/Constants.js
+++ Sources/Widgets/Core/AbstractWidget/Constants.js
@@ -1,4 +1,6 @@
-/** @type {number} The priority used for widget interaction in VTK.js. */
+/** @type {number} The priority used for widget interaction in VTK.js.
+ * High value for high priority (default priority is 0.0)
+ */
 export const WIDGET_PRIORITY = 0.5;
 
 export default { WIDGET_PRIORITY };
